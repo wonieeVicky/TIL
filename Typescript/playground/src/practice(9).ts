d0d7b2af6949757b1d07ef534afd101d2c417a03
Vicky <hwfongfing@gmail.com>
2020-12-26 22:47:58 +0900

    chore: add test code for ts-9(overloads)

--- Typescript/playground/src/practice(9).ts
+++ Typescript/playground/src/practice(9).ts
@@ -10,7 +10,7 @@
       console.log(this.a); // global.a
     }
   },
-};*/
+};
 
 // 객체 데이터 obj에서 b 메서드는 a를 this를 통해 참조한다.
 const obj = {
@@ -21,7 +21,7 @@ const obj = {
 };
 
 // this가 유효한 콘텍스트를 상실하여 a를 참조할 수 없게되는 경우 (콜백함수들이 여기에 해당함)
-/*obj.b(); // Hello!
+obj.b(); // Hello!
 const b = obj.b;
 b(); // Cannot read property 'a' of undefined
 
@@ -100,4 +100,40 @@ function someFn(this: ICat, greeting: string) {
   console.log(`${greeting} ${this.name}`);
 }
 someFn.call(cat, 'Hello'); // Hello Nana
+
+
+// 2) overloads
+// 매개변수 타입과 반환 타입이 다른 여러 함수를 가질 수 있는 함수 오버로드
+function add(a: string, b: string): string; // 함수 선언
+function add(a: number, b: number): number; // 함수 선언
+function add(a: any, b: any): any {
+  // 함수 구현
+  return a + b;
+}
+add('Hello ', 'world!');
+add(1, 2);
+add('Hello', 2); // Error - No overload matches this call.
+
+// 인터페이스나 타입 별칭 등의 메소드 정의에서도 활용 가능
+// 타입 단언이나 타입 가드를 통해 함수 선언부의 동적인 매개변수와 반환값을 정의
+interface IUser {
+  name: string;
+  age: number;
+  getData(x: string): string[];
+  getData(x: number): string;
+}
+let user: IUser = {
+  name: 'Vicky',
+  age: 31,
+  getData: (data: any) => {
+    if (typeof data === 'string') {
+      return data.split('');
+    } else {
+      return data.toString();
+    }
+  },
+};
+
+user.getData('Hello'); // ['H','e','l','l','o']
+user.getData(123); // '123'
 */
