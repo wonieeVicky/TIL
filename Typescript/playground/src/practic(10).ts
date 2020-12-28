3e8efc209d2ff35034972ffea5ff768c8498a4ba
Vicky <hwfongfing@gmail.com>
2020-12-28 23:24:24 +0900

    chore: add test code ts-10(class modifiers)

--- Typescript/playground/src/practic(10).ts
+++ Typescript/playground/src/practic(10).ts
@@ -1,5 +1,6 @@
 // 10. 클래스
-//클래스 바디(Class body)에 별도로 타입을 선언하는 클래스 속성(properties)
+//2. 클래스 바디(Class body)
+// 클래스 바디(Class body)에 별도로 타입을 선언하는 클래스 속성(properties)
 // {} 중괄호에 묶여있는 영역을 의미함
 
 class Animal {
@@ -16,3 +17,66 @@ class Cat extends Animal {
 let cat: Cat;
 cat = new Cat('Nana');
 console.log(cat.getName()); // Cat naem is Nana
+ 
+// 1) 클래스 수식어(Modifiers)
+// 접근 제어자가 public인 경우
+class Animal {
+    public name: string;
+    constructor(name: string) {
+        this.name = name;
+    }
+}
+class Cat extends Animal {
+    getName(): string {
+        return `Cat name is ${this.name}`;
+    }
+}
+
+let cat = new Cat('Nana')
+console.log(cat.getName()); // Cat name is Nana
+cat.name = ''Lucy
+console.log(cat.getName()); // Cat name is Lucy
+
+// 접근 제어자가 private 경우
+class Animal {
+    private name: string;
+    constructor(name: string) {
+        this.name = name;
+    }
+}
+class Cat extends Animal {
+    getName(): string {
+        return `Cat name is ${this.name}`; // Error - TS2341: Property 'name' is private and only accessible within class 'Animal'
+    }
+}
+let cat = new Cat("Nana");
+console.log(cat.getName()); // Cat name is Nana
+console.log(cat.name); // Error - TS2341: Property 'name' is private and only accessible within class 'Animal'.
+
+cat.name = "Lucy"; // Error - TS2341: Property 'name' is private and only accessible within class 'Animal'.
+console.log(cat.getName());
+
+// 접근 제어자가 protected인 경우
+class Animal {
+    name: string;
+    protected constructor(name: string) {
+        this.name = name;
+    }
+}
+const cat = new Animal('Dog'); // Error - TS2674: Constructor of class 'Animal' is protected and only accessible within the class declaration.
+
+
+// 생성자 메소드에서 인수 타입 선언과 동시에 접근 제어자 사용시 속성멤버로 정의가능
+class Cat {
+    constructor(public name: string, protected age: number) {}
+    getName() {
+        return this.name;
+    }
+    getAge() {
+        return this.age;
+    }
+}
+
+const cat = new Cat("Nana", 2);
+console.log(cat.getName()); // Nana
+console.log(cat.getAge()); // 2
