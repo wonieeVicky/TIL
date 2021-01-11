0aaf2384d85a29f754a3254acb1887de49b7c8ef
Vicky <hwfongfing@gmail.com>
2021-01-11 21:50:44 +0900

    feat: add test code for TDD

---
+++ TDD/playground/ClickCounter/ClickCounter.js
@@ -0,0 +1,14 @@
+var App = App || {};
+
+App.ClickCounter = () => {
+    let value = 0;
+
+    return {
+        getValue() {
+            return value;
+        },
+        increase() {
+            value++;
+        },
+    };
+};
---
+++ TDD/playground/ClickCounter/ClickCounter.spec.js
@@ -0,0 +1,19 @@
+describe("App.ClickCounter", () => {
+    let counter;
+    beforeEach(() => {
+        counter = App.ClickCounter();
+    });
+    describe("getValue()", () => {
+        it("초기값이 0인 카운터 값을 반환한다", () => {
+            expect(counter.getValue()).toBe(0);
+        });
+    });
+
+    describe("increase()", () => {
+        it("카운터를 1 올린다", () => {
+            const initialValue = counter.getValue();
+            counter.increase();
+            expect(counter.getValue()).toBe(initialValue + 1);
+        });
+    });
+});
