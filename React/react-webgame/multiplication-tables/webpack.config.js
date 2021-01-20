f9026ba59d25347881c753a60785475ecdd32dad
Vicky <hwfongfing@gmail.com>
2021-01-20 23:19:07 +0900

    feat: add test code for web-game-2(webpack preset-env setting)

--- React/react-webgame/multiplication-tables/webpack.config.js
+++ React/react-webgame/multiplication-tables/webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
-  devtool: "eval", // production: hidden-source-map
+  devtool: "eval",
   resolve: {
     extensions: [".jsx", ".js"],
   },
@@ -17,14 +17,26 @@ module.exports = {
         test: /\.jsx?$/,
         loader: "babel-loader",
         options: {
-          presets: ["@babel/preset-env", "@babel/preset-react"],
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                targets: {
+                  browsers: ["> 5% in KR"], // 브라우저별 설정이 가능하다. (참조 //github.com/browserslist/browserslist)
+                },
+                debug: true,
+              },
+            ],
+            "@babel/preset-react",
+          ],
           plugins: ["@babel/plugin-proposal-class-properties"],
         },
       },
     ],
   },
+  plugins: [new webpack.LoaderOptionsPlugin({ debug: true })],
   output: {
-    path: path.join(__dirname, "dist"), // C:\users\vicky\TIL\.. 이런거 안적어도 된다.
+    path: path.join(__dirname, "dist"),
     filename: "app.js",
   },
 };
