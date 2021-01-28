e24c2265ecff4ac1916794b979da159c1caa4b6d
Vicky <hwfongfing@gmail.com>
2021-01-28 23:13:17 +0900

    feat: ResponseCheck 컴포넌트 구현(react-webgame-4)

--- React/react-webgame/response-check/client.jsx
+++ React/react-webgame/response-check/client.jsx
@@ -1,4 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// ReactDOM.render(<NumberBaseball />, document.querySelector("#root"));
+import ResponseCheck from "./ResponseCheck";
+
+ReactDOM.render(<ResponseCheck />, document.querySelector("#root"));
