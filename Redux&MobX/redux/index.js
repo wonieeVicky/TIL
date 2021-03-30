2f6993ef82ef2313671c9bc6743c7d9c39301d93
Vicky <hwfongfing@gmail.com>
2021-03-30 23:31:04 +0900

    feat: add redux test code

---
+++ Redux&MobX/redux/index.js
@@ -0,0 +1,48 @@
+﻿const { createStore } = require("redux");
+
+const initialState = {
+  compA: "a",
+  compB: 12,
+  compC: null,
+};
+
+const reducer = (prevState, action) => {
+  switch (action.type) {
+    case "CHANGE_COMP_A":
+      return {
+        ...prevState,
+        compA: action.data,
+      };
+    case "CHANGE_COMP_B":
+      return {
+        ...prevState,
+        compB: action.data,
+      };
+    case "CHANGE_COMP_C":
+      return {
+        ...prevState,
+        compC: action.data,
+      };
+    default:
+      return prevState;
+  }
+};
+
+const store = createStore(reducer, initialState);
+
+store.subscribe(() => {
+  // react-redux 안에 기본적으로 들어있다.
+  console.log("changed!"); // 화면 바꿔주는 코드는 여기서 실행한다.
+});
+
+console.log(store.getState()); // { compA: 'a', compB: 12, compC: null }
+
+// action
+const changeCompA = (data) => ({
+  type: "CHANGE_COMP_A",
+  data,
+});
+
+store.dispatch(changeCompA("b"));
+
+console.log(store.getState()); // { compA: 'b', compB: 12, compC: null }
---
+++ Redux&MobX/redux/index2.js
@@ -0,0 +1,50 @@
+﻿const { createStore } = require('redux');
+
+const initialState = {
+  user: null,
+  posts: [],
+};
+
+const reducer = (prevState, action) => {
+  switch (action.type) {
+    case 'LOG_IN':
+      return {
+        ...prevState,
+        user: action.data,
+      };
+    case 'LOG_OUT':
+      return {
+        ...prevState,
+        user: null,
+      };
+    case 'ADD_POST':
+      return {
+        ...prevState,
+        posts: [...prevState.posts, action.data],
+      };
+    default:
+      return prevState;
+  }
+};
+
+const store = createStore(reducer, initialState);
+
+store.subscribe(() => {
+  console.log('changed!');
+});
+
+console.log(store.getState()); // { user: null, posts: [] }
+
+// -------------------------------------------------------
+
+// action
+const logIn = (data) => ({ type: 'LOG_IN', data });
+const logOut = () => ({ type: 'LOG_OUT' });
+const addPost = (data) => ({ type: 'ADD_POST', data });
+
+store.dispatch(logIn({ id: 1, name: 'vicky', admin: true }));
+store.dispatch(addPost({ userId: 1, id: 1, content: 'hi redux!' }));
+store.dispatch(addPost({ userId: 1, id: 2, content: 'seconds redux :)' }));
+store.dispatch(logOut());
+
+console.log('3:', store.getState()); // { user: null, posts: [ { userId: 1, id: 1, content: 'hi redux!' } ] }
