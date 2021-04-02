d945d41ee075b65b66961aeec9c4e7eae3afa425
Vicky <hwfongfing@gmail.com>
2021-04-02 13:32:01 +0900

    feat: add typescript(todo app) test code

--- Typescript/learn-typescript/quiz/1_todo/src/index.ts
+++ Typescript/learn-typescript/quiz/1_todo/src/index.ts
@@ -1,50 +1,56 @@
-let todoItems;
+interface Todo {
+  id: number;
+  title: string;
+  done: boolean;
+}
+
+let todoItems: Todo[];
 
 // api
-function fetchTodoItems() {
+function fetchTodoItems(): Todo[] {
   const todos = [
-    { id: 1, title: '안녕', done: false },
-    { id: 2, title: '타입', done: false },
-    { id: 3, title: '스크립트', done: false },
+    { id: 1, title: "안녕", done: false },
+    { id: 2, title: "타입", done: false },
+    { id: 3, title: "스크립트", done: false },
   ];
   return todos;
 }
 
 // crud methods
-function fetchTodos() {
+function fetchTodos(): object[] {
   const todos = fetchTodoItems();
   return todos;
 }
 
-function addTodo(todo) {
+function addTodo(todo: Todo): void {
   todoItems.push(todo);
 }
 
-function deleteTodo(index) {
+function deleteTodo(index: number): void {
   todoItems.splice(index, 1);
 }
 
-function completeTodo(index, todo) {
+function completeTodo(index: number, todo: Todo): void {
   todo.done = true;
   todoItems.splice(index, 1, todo);
 }
 
 // business logic
-function logFirstTodo() {
+function logFirstTodo(): object {
   return todoItems[0];
 }
 
-function showCompleted() {
-  return todoItems.filter(item => item.done);
+function showCompleted(): object[] {
+  return todoItems.filter((item) => item.done);
 }
 
-// TODO: 아래 함수의 내용을 채워보세요. 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수입니다.
-function addTwoTodoItems() {
-  // addTodo() 함수를 두 번 호출하여 todoItems에 새 할 일이 2개 추가되어야 합니다.
+function addTwoTodoItems(): void {
+  addTodo({ id: 4, title: "호", done: false });
+  addTodo({ id: 5, title: "몰랑", done: false });
 }
 
 // NOTE: 유틸 함수
-function log() {
+function log(): void {
   console.log(todoItems);
 }
 
