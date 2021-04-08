686ff0e39972d418f9441b5d3f97039bd5006435
Vicky <hwfongfing@gmail.com>
2021-04-08 23:01:35 +0900

    feat: add genericType for Typescript test code

--- Typescript/learn-typescript/example/dropdown-generic.ts
+++ Typescript/learn-typescript/example/dropdown-generic.ts
@@ -1,26 +1,36 @@
-const emails = [
-  { value: 'naver.com', selected: true },
-  { value: 'gmail.com', selected: false },
-  { value: 'hanmail.net', selected: false },
+interface DropdownItem<T> {
+  value: T;
+  selected: boolean;
+}
+
+const emails: DropdownItem<string>[] = [
+  { value: "naver.com", selected: true },
+  { value: "gmail.com", selected: false },
+  { value: "hanmail.net", selected: false },
 ];
 
-const numberOfProducts = [
+const numberOfProducts: DropdownItem<number>[] = [
   { value: 1, selected: true },
   { value: 2, selected: false },
   { value: 3, selected: false },
 ];
 
-function createDropdownItem(item) {
-  const option = document.createElement('option');
+function createDropdownItem<T>(item: DropdownItem<T>) {
+  const option = document.createElement("option");
   option.value = item.value.toString();
   option.innerText = item.value.toString();
   option.selected = item.selected;
   return option;
 }
 
-// NOTE: 이메일 드롭 다운 아이템 추가
 emails.forEach(function (email) {
-  const item = createDropdownItem(email);
-  const selectTag = document.querySelector('#email-dropdown');
+  const item = createDropdownItem<string>(email);
+  const selectTag = document.querySelector("#email-dropdown");
+  selectTag.appendChild(item);
+});
+
+numberOfProducts.forEach((product) => {
+  const item = createDropdownItem<number>(product);
+  const selectTag = document.querySelector("#email-dropdown");
   selectTag.appendChild(item);
 });
