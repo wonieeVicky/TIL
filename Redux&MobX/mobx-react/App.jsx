f4df6edb02459ad2e3b2f35458183620103d0b40
Vicky <hwfongfing@gmail.com>
2021-04-08 23:35:49 +0900

    feat: mobx-react 프로젝트 내 mobX용 store 및 App 컴포넌트 구현

---
+++ Redux&MobX/mobx-react/App.jsx
@@ -0,0 +1,35 @@
+﻿import React, { Component } from "react";
+import { observer } from "mobx-react";
+import { userStore, postStore } from "./store";
+
+@observer
+class App extends Component {
+  onLogIn = () => {};
+  onLogOut = () => {};
+  onChangeName = (e) => (this.state.name = e.target.value);
+  onChangePassword = (e) => (this.state.password = e.target.value);
+
+  render() {
+    <div>
+      {userStore.isLoggingIn ? (
+        <div>로그인 중입니다.</div>
+      ) : userStore.data ? (
+        <div>{userStore.data.nickname}</div>
+      ) : (
+        "로그인 해주세요"
+      )}
+      {!userStore.data ? (
+        <button onClick={this.onClick}>로그인</button>
+      ) : (
+        <button onClick={this.onLogout}>로그아웃</button>
+      )}
+      <div>{postStore.data.length}</div>
+      <div>
+        <input value={this.state.name} onChange={this.onChangeName} />
+        <input value={this.state.password} type="password" onChange={this.onChangePassword} />
+      </div>
+    </div>;
+  }
+}
+
+export default App;
---
+++ Redux&MobX/mobx-react/client.jsx
@@ -0,0 +1,5 @@
+﻿import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+ReactDOM.render(<App />, document.querySelector("#root"));
