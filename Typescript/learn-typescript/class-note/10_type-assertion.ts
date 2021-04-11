a90ccdd6229a81e6b6b6f283e8a2dad8c96cc9f9
Vicky <hwfongfing@gmail.com>
2021-04-11 22:34:10 +0900

    feat: add test code for type assersion & type guard

---
+++ Typescript/learn-typescript/class-note/10_type-assertion.ts
@@ -0,0 +1,11 @@
+﻿// 타입 단언(type assertion)
+var a; // 타입 추론방식에 의해 a의 타입은 any로 추론
+// var b = 10; // 타입 추론방식에 의해 b의 타입은 number로 추론
+var b = a; // b의 타입은 맨처음에 선언된 a의 타입에 따라 any로 추론된다.
+
+var c = a as string; // a를 string으로 할당되어 c의 타입은 string으로 선언된다.
+
+// DOM API 조작할 때 타입 단언을 가장 많이 사용한다.
+// <div id="app">hi</div>에 접근한다고 했을 때..
+var div = document.querySelector('div') as HTMLDivElement; // 특정 tag에 대한 정보를 접근할 수 있음, 타입 HTMLDivElement
+div.innerText = 'abc';
---
+++ Typescript/learn-typescript/class-note/11_type-guard.ts
@@ -0,0 +1,37 @@
+﻿interface Developer {
+  name: string;
+  skill: string;
+}
+
+interface Person {
+  name: string;
+  age: number;
+}
+
+function introduce(): Developer | Person {
+  return { name: 'Vicky', age: 32, skill: 'React' };
+}
+
+var vicky = introduce();
+console.log(vicky.name); // Vicky
+console.log(vicky.skill); // Type error! Union 타입을 썻을 때에는 공통된 속성만 접근이 가능하므로 SKILL 접근 불가
+
+// 이를 타입 단언으로 해결할 수 있다.
+if ((vicky as Developer).skill) {
+  var skill = (vicky as Developer).skill;
+  console.log(skill); // React
+} else if ((vicky as Person).age) {
+  var age = (vicky as Person).age;
+  console.log(age); // 32
+}
+
+// 타입 가드 정의
+function isDeveloper(target: Developer | Person): target is Developer {
+  return (target as Developer).skill !== undefined;
+}
+
+if (isDeveloper(vicky)) {
+  console.log(vicky.skill);
+} else {
+  console.log(vicky.age);
+}
