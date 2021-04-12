4f1c1a734d9bd68fb8f6408c5838051fe2fe63a4
Vicky <hwfongfing@gmail.com>
2021-04-12 23:42:52 +0900

    feat: mobx-react class컴포넌트로 로그인, 로그아웃 이벤트 구현(userStore, postStore)

--- Redux&MobX/mobx-react/App.jsx
+++ Redux&MobX/mobx-react/App.jsx
@@ -1,34 +1,53 @@
 ﻿import React, { Component } from "react";
+import { observable } from "mobx";
 import { observer } from "mobx-react";
 import { userStore, postStore } from "./store";
 
-@observer
+// observer는 컴포넌트 안에서 쓰이는 observable이 바뀌면 자동으로 컴포넌트를 리렌더링해준다.
+@observer // decorator는 hoc이다. 주로 클래스는 decorator로 초기에 선언하여 HOC를 대신할 수 있다.
 class App extends Component {
-  onLogIn = () => {};
-  onLogOut = () => {};
-  onChangeName = (e) => (this.state.name = e.target.value);
-  onChangePassword = (e) => (this.state.password = e.target.value);
+  // 컴포넌트 안에서만 쓰는 state
+  state = observable({
+    name: "",
+    password: "",
+  });
+  onLogIn = () =>
+    userStore.logIn({
+      nickname: "vicky",
+      password: "123",
+    });
+
+  onLogOut = () => userStore.logOut();
+
+  onChangeName = e => (this.state.name = e.target.value);
+  onChangePassword = e => (this.state.password = e.target.value);
 
   render() {
-    <div>
-      {userStore.isLoggingIn ? (
-        <div>로그인 중입니다.</div>
-      ) : userStore.data ? (
-        <div>{userStore.data.nickname}</div>
-      ) : (
-        "로그인 해주세요"
-      )}
-      {!userStore.data ? (
-        <button onClick={this.onClick}>로그인</button>
-      ) : (
-        <button onClick={this.onLogout}>로그아웃</button>
-      )}
-      <div>{postStore.data.length}</div>
+    return (
       <div>
-        <input value={this.state.name} onChange={this.onChangeName} />
-        <input value={this.state.password} type="password" onChange={this.onChangePassword} />
+        {userStore.isLoggingIn ? (
+          <div>로그인 중입니다.</div>
+        ) : userStore.data ? (
+          <div>{userStore.data.nickname}</div>
+        ) : (
+          "로그인 해주세요"
+        )}
+        {!userStore.data ? (
+          <button onClick={this.onLogIn}>로그인</button>
+        ) : (
+          <button onClick={this.onLogOut}>로그아웃</button>
+        )}
+        <div>{postStore.data.length}</div>
+        <div>
+          <input value={this.state.name} onChange={this.onChangeName} />
+          <input
+            value={this.state.password}
+            type="password"
+            onChange={this.onChangePassword}
+          />
+        </div>
       </div>
-    </div>;
+    );
   }
 }
 
--- Redux&MobX/mobx-react/store.js
+++ Redux&MobX/mobx-react/store.js
@@ -1,5 +1,6 @@
 ﻿const { observable } = require("mobx");
 
+// 일반 객체 리터럴은 decorator로 observable을 붙일 수 없다. hoc로 감싸줘야 함
 const userStore = observable({
   isLoggingIn: false,
   data: null,
@@ -8,7 +9,7 @@ const userStore = observable({
     setTimeout(() => {
       this.data = data;
       this.isLoggingIn = false;
-      postStore.data.push(1); // redux에 비해 편한 점
+      postStore.data.push(1); // redux에 비해 편한 점 : userStore에서 postStore의 데이터를 바꿀 수 있음
     }, 2000);
   },
   logOut() {
