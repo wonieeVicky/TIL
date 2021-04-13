aea175d3a6b27a4fa7fc8c2f4090a04f8c19dc3c
Vicky <hwfongfing@gmail.com>
2021-04-13 23:34:19 +0900

    feat: Mobx store를 Provider로 분리하기 위한 코드 작성(client.jsx에서 적용 필요)
    - Context API를 사용해 Provider 생성(Context.jsx)
    - 필요한 store를 가져올 수 있도록 커스텀 훅 생성(useStore)

---
+++ Redux&MobX/mobx-react/Context.jsx
@@ -0,0 +1,13 @@
+﻿import React from "react";
+import { userStore, postStore } from "./store";
+
+export const storeContext = React.createContext({
+  userStore,
+  postStore,
+});
+
+export const StoreProvider = ({ children }) => {
+  return <storeContext.Provider>{children}</storeContext.Provider>;
+};
+
+export default StoreProvider;
---
+++ Redux&MobX/mobx-react/useStore.js
@@ -0,0 +1,12 @@
+﻿import React from "react";
+import { storeContext } from "./Context";
+function useStore() {
+  const { userStore, postStore } = React.useContext(storeContext);
+
+  return {
+    userStore,
+    postStore,
+  };
+}
+
+export default useStore;
