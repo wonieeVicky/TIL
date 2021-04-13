ee81b8e11f1814883251ef837ace14cebd77e950
Vicky <hwfongfing@gmail.com>
2021-04-13 23:34:46 +0900

    feat: 함수형 Mobx 컴포넌트 생성

--- Redux&MobX/mobx-react/App.jsx
+++ Redux&MobX/mobx-react/App.jsx
@@ -1,54 +1,44 @@
-﻿import React, { Component } from "react";
-import { observable } from "mobx";
-import { observer } from "mobx-react";
+﻿import React, { useCallback } from "react";
+import { useLocalObservable, observer } from "mobx-react";
 import { userStore, postStore } from "./store";
+import { action, runInAction } from "mobx";
 
-// observer는 컴포넌트 안에서 쓰이는 observable이 바뀌면 자동으로 컴포넌트를 리렌더링해준다.
-@observer // decorator는 hoc이다. 주로 클래스는 decorator로 초기에 선언하여 HOC를 대신할 수 있다.
-class App extends Component {
-  // 컴포넌트 안에서만 쓰는 state
-  state = observable({
+const App = () => {
+  const state = useLocalObservable(() => ({
     name: "",
     password: "",
-  });
-  onLogIn = () =>
-    userStore.logIn({
-      nickname: "vicky",
-      password: "123",
-    });
+    onChangeName: (e) => runInAction(() => (state.name = e.target.value)),
+    onChangePassword: action((e) => (state.password = e.target.value)),
+  }));
 
-  onLogOut = () => userStore.logOut();
+  const onLogIn = useCallback(
+    () =>
+      userStore.logIn({
+        nickname: "vicky",
+        password: 123,
+      }),
+    []
+  );
 
-  onChangeName = e => (this.state.name = e.target.value);
-  onChangePassword = e => (this.state.password = e.target.value);
+  const onLogOut = useCallback(() => userStore.logOut(), []);
 
-  render() {
-    return (
+  return (
+    <div>
+      {userStore.isLoggingIn ? (
+        <div>로그인 중입니다.</div>
+      ) : userStore.data ? (
+        <div>{userStore.data.nickname}</div>
+      ) : (
+        "로그인 해주세요"
+      )}
+      {!userStore.data ? <button onClick={onLogIn}>로그인</button> : <button onClick={onLogOut}>로그아웃</button>}
+      <div>{postStore.postLength}</div>
       <div>
-        {userStore.isLoggingIn ? (
-          <div>로그인 중입니다.</div>
-        ) : userStore.data ? (
-          <div>{userStore.data.nickname}</div>
-        ) : (
-          "로그인 해주세요"
-        )}
-        {!userStore.data ? (
-          <button onClick={this.onLogIn}>로그인</button>
-        ) : (
-          <button onClick={this.onLogOut}>로그아웃</button>
-        )}
-        <div>{postStore.data.length}</div>
-        <div>
-          <input value={this.state.name} onChange={this.onChangeName} />
-          <input
-            value={this.state.password}
-            type="password"
-            onChange={this.onChangePassword}
-          />
-        </div>
+        <input value={state.name} onChange={state.onChangeName} />
+        <input value={state.password} type="password" onChange={state.onChangePassword} />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
-export default App;
+export default observer(App);
