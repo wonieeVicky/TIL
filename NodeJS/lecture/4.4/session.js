8c381900c3ff5b075867a3a53c7469a7221e86c0
Vicky <hwfongfing@gmail.com>
2021-04-14 23:06:09 +0900

    feat: add test code(session) in node.js

---
+++ NodeJS/lecture/4.4/session.js
@@ -0,0 +1,54 @@
+﻿const http = require("http");
+const fs = require("fs").promises;
+const url = require("url");
+const qs = require("querystring");
+
+const parseCookies = (cookie = "") =>
+  cookie
+    .split(";")
+    .map((v) => v.split("="))
+    .reduce((acc, [k, v]) => {
+      acc[k.trim()] = decodeURIComponent(v);
+      return acc;
+    }, {});
+
+const session = {}; // 세션을 담는 객체
+
+http
+  .createServer(async (req, res) => {
+    const cookies = parseCookies(req.headers.cookie);
+    if (req.url.startsWith("/login")) {
+      const { query } = url.parse(req.url);
+      const { name } = qs.parse(query);
+      const expires = new Date();
+      expires.setMinutes(expires.getMinutes() + 5);
+      const uniqueInt = Date.now(); // unique한 키
+      session[uniqueInt] = {
+        name,
+        expires,
+      };
+      // 고유한 키 값(uniqueInt)만 보낸다.
+      res.writeHead(302, {
+        Location: "/",
+        "Set-Cookie": `session=${uniqueInt}; Expires=${expires.toGMTString()}; HttpOnly; Path=/`,
+      });
+      res.end();
+      // 세션 쿠키가 존재하고, 만료 기간이 지나지 않았다면
+      // 브라우저는 의미를 알 수 없는 키를 가지고 있고, 그 키를 통해 서버의 데이터에 접근한다.
+    } else if (cookies.session && session[cookies.session].expires > new Date()) {
+      res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+      res.end(`${session[cookies.session].name}님 안녕하세요`);
+    } else {
+      try {
+        const data = await fs.readFile("./cookie2.html");
+        res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+        res.end(data);
+      } catch (err) {
+        res.writeHead(500, { "Content-Type": "text/plain; charset=utf-8" });
+        res.end(err.message);
+      }
+    }
+  })
+  .listen(8085, () => {
+    console.log("8085번 포트에서 서버 대기 중입니다!");
+  });
