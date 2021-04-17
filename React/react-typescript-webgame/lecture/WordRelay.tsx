1d17a6282398d1324079ebfb8fbe2091515fabd5
Vicky <hwfongfing@gmail.com>
2021-04-18 00:15:38 +0900

    feat: react-ts 내 useCallback 타이핑 구현(WordRelay)

---
+++ React/react-typescript-webgame/lecture/WordRelay.tsx
@@ -0,0 +1,48 @@
+﻿import * as React from "react";
+import { useState, useRef, useCallback } from "react";
+
+const WordRelay = () => {
+  const [word, setWord] = useState("비키");
+  const [value, setValue] = useState("");
+  const [result, setResult] = useState("");
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  // useCallback으로 감싸주면 매개변수 e의 타입추론이 안됨.
+  // useCallback에 타입추론하는 방법 1
+  const onSubmitForm = useCallback<(e: React.FormEvent<Element>) => void>(
+    (e) => {
+      e.preventDefault();
+      const input = inputRef.current;
+      if (word[word.length - 1] === value[0]) {
+        setResult("딩동댕!");
+        setWord(value);
+        setValue("");
+      } else {
+        setResult("땡!");
+        setValue("");
+      }
+      if (input) {
+        input.focus();
+      }
+    },
+    [value]
+  );
+
+  // useCallback에 타입추론하는 방법 1
+  const onChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => setValue(e.currentTarget.value), []);
+
+  return (
+    <>
+      <div>{word}</div>
+      <form onSubmit={onSubmitForm}>
+        <label htmlFor="wordInput">글자를 입력하세요.</label>
+        <input id="wordInput" className="wordInput" ref={inputRef} onChange={onChange} value={value} />
+        <button>입력</button>
+      </form>
+      <div>{result}</div>
+    </>
+  );
+};
+
+export default WordRelay;
--- React/react-typescript-webgame/lecture/client.tsx
+++ React/react-typescript-webgame/lecture/client.tsx
@@ -1,6 +1,6 @@
 ﻿import * as React from "react"; // index.d.ts에서 export default를 지원하지 않으므로 * as를 사용한다.
 import * as ReactDOM from "react-dom";
 
-import GuGuDan from "./GuGuDan";
+import WordRelay from "./WordRelay";
 
-ReactDOM.render(<GuGuDan />, document.querySelector("#root"));
+ReactDOM.render(<WordRelay />, document.querySelector("#root"));
