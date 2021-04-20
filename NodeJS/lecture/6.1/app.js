8f4c9f2d967192af0268f3ca37ed2889b022c81e
Vicky <hwfongfing@gmail.com>
2021-04-20 17:14:05 +0900

    feat: html-serving with express(node.js)

--- NodeJS/lecture/6.1/app.js
+++ NodeJS/lecture/6.1/app.js
@@ -1,18 +1,42 @@
 ﻿const express = require("express");
-
-// 전역변수 느낌적인 느낌
+const path = require("path");
 const app = express();
+
 app.set("port", process.env.PORT || 3000);
 
-// app에 메서드를 구현하는 방식으로 코드를 작성할 수 있다.
-app.get("/", (req, res) => {
-  res.send("Hello Express!");
+// 1. app.use를 통한 전 라우터 코드 실행 처리
+app.use((req, res, next) => {
+  console.log("모든 요청에 실행하고 싶다!");
+  next(); // 반드시 next를 넣어준다.
 });
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// 2. 라우터 매개변수를 이용한 라우터
+app.get("/category/:name", (req, res) => {
+  res.send(`hello ${req.params.name}`);
+});
+
+// 3. 정적 라우트 코드가 아래에 있으면 안된다.
+app.get("/category/javascript", (req, res) => {
+  res.send(`hello Test!`);
+});
+
+app.get("/", (req, res) => {
+  res.send("hello express!");
+});
+
 app.get("/about", (req, res) => {
   res.send("about Express!");
 });
 
-// 별도 포트를 지정하지 않고, app의 포트번호를 가져온다.
+// 4. 특히 모든 요청에 처리해야하는 라우터(*)의 경우 최하단에 위치시켜줘야한다.
+app.get("*", (req, res) => {
+  res.send("about global!");
+});
+
 app.listen(app.get("port"), () => {
   console.log(app.get("port"), "번 포트에서 대기 중");
 });
