758545029eb4c7c71b4fb0903fe402b84a8b3b23
Vicky <hwfongfing@gmail.com>
2021-05-07 23:42:23 +0900

    feat: 라우터 처리 컴포넌트 Games, GameMatcher 구현

---
+++ React/react-typescript-webgame/lecture/GameMatcher.tsx
@@ -0,0 +1,21 @@
+﻿import * as React from "react";
+import { Component } from "react";
+import NumberBaseball from "./src/NumberBaseballClass";
+import RSP from "./src/RSPClass";
+import Lotto from "./src/LottoClass";
+
+class GameMatcher extends Component {
+  render() {
+    // let urlSearchParams = new URLSearchParams(this.props.location.search.slice(1));
+    // console.log(urlSearchParams.get("hello"));
+    if (this.props.match.params.name === "number-baseball") {
+      return <NumberBaseball />;
+    } else if (this.props.match.params.name === "rock-scissors-paper") {
+      return <RSP />;
+    } else if (this.props.match.params.name === "lotto-generator") {
+      return <Lotto />;
+    }
+    return <div>일치하는 게임이 없습니다.</div>;
+  }
+}
+export default GameMatcher;
---
+++ React/react-typescript-webgame/lecture/Games.tsx
@@ -0,0 +1,28 @@
+﻿import * as React from "react";
+import { Switch, BrowserRouter, Link, Route } from "react-router-dom";
+import GameMatcher from "./GameMatcher";
+
+const Games = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Link to="/game/number-baseball">숫자야구</Link>
+        &nbsp;
+        <Link to="/game/rock-scissors-paper">가위바위보</Link>
+        &nbsp;
+        <Link to="/game/lotto-generator">로또생성기</Link>
+        &nbsp;
+        <Link to="/game/index">게임 매쳐</Link>
+      </div>
+      <div>
+        <Switch>
+          <Route exact path="/" render={(props) => <GameMatcher {...props} />} />
+          <Route path="/game/:name" render={(props) => <GameMatcher {...props} />} />
+          {/* this.props.match.params.name으로 각 path가 걸러진다. */}
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default Games;
