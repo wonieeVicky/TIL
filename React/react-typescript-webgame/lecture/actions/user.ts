1d911fd316f978e1d079f414ad19abb652fd0942
Vicky <hwfongfing@gmail.com>
2021-05-10 23:56:17 +0900

    feat: add redux-thunk typing

--- React/react-typescript-webgame/lecture/actions/user.ts
+++ React/react-typescript-webgame/lecture/actions/user.ts
@@ -1,4 +1,7 @@
-﻿export const LOG_IN_REQUEST = "LOG_IN_REQUEST" as const;
+﻿import { AnyAction } from "redux";
+import { addPost } from "./post";
+
+export const LOG_IN_REQUEST = "LOG_IN_REQUEST" as const;
 export const LOG_IN_SUCCESS = "LOG_IN_SUCCESS" as const;
 export const LOG_IN_FAILURE = "LOG_IN_FAILURE" as const;
 export const LOG_OUT = "LOG_OUT" as const;
@@ -10,19 +13,62 @@ export interface LogInRequestAction {
     password: string;
   };
 }
+export const logInRequest = (data: { id: string; password: string }): LogInRequestAction => {
+  return {
+    type: LOG_IN_REQUEST,
+    data,
+  };
+};
 export interface LogInSuccessAction {
   type: typeof LOG_IN_SUCCESS;
   data: {
-    userId: string;
+    userId: number;
     nickname: string;
   };
 }
+export const logInSuccess = (data: { userId: number; nickname: string }): LogInSuccessAction => {
+  return {
+    type: LOG_IN_SUCCESS,
+    data,
+  };
+};
 export interface LogInFailureAction {
   type: typeof LOG_IN_FAILURE;
   error: Error;
 }
+export const logInFailure = (error: Error): LogInFailureAction => {
+  return {
+    type: LOG_IN_FAILURE,
+    error,
+  };
+};
 
-export const logIn = (data: { id: string; password: string }) => {};
+// TypeScript에서는 상호참조에 대한 타이핑도 지원한다 ThunkAction - ThunkDispatch
+interface ThunkDispatch {
+  (thunkAction: ThunkAction): void; // thunkAction인 경우 - 리턴값 없음
+  <A>(action: A): A; // 임의의 액션인 경우 - 리턴값 있음
+  <TAction>(action: TAction | ThunkAction): TAction; // 또는 thunkAction이거나 임의의 액션인 경우
+}
+
+type ThunkAction = (dispatch: ThunkDispatch) => void;
+export const logIn = (data: { id: string; password: string }): ThunkAction => {
+  return (dispatch) => {
+    dispatch(logInRequest(data));
+    try {
+      setTimeout(() => {
+        dispatch(
+          logInSuccess({
+            userId: 123,
+            nickname: "빅히언니",
+          })
+        );
+        dispatch(addPost(""));
+      }, 1000);
+    } catch (err) {
+      dispatch(logInFailure(err));
+    }
+  };
+};
 export interface LogOutAction {
   type: typeof LOG_OUT;
 }
--- React/react-typescript-webgame/lecture/store.ts
+++ React/react-typescript-webgame/lecture/store.ts
@@ -1,4 +1,5 @@
-﻿import { createStore } from "redux";
+﻿import { createStore, MiddlewareAPI, Dispatch, AnyAction, applyMiddleware, compose } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import reducer from "./reducers";
 
 const initialState = {
@@ -9,6 +10,25 @@ const initialState = {
   posts: [],
 };
 
-const store = createStore(reducer, initialState);
+// 함수가 분리되어있으면 타이핑을 해야한다. 미들웨어는 아래와 같이 타이핑을해준다.
+const firstMiddleware = (store: MiddlewareAPI) => (next: Dispatch<AnyAction>) => (action: AnyAction) => {
+  console.log("로깅:", action);
+  next(action);
+};
+
+const thunkMiddleware = (store: MiddlewareAPI) => (next: Dispatch<AnyAction>) => (action: any) => {
+  if (typeof action === "function") {
+    // 비동기
+    return action(store.dispatch, store.getState);
+  }
+  return next(action); // 동기
+};
+
+// 미들웨어 장착
+const enhance =
+  process.env.NODE_ENV === "production"
+    ? compose(applyMiddleware(firstMiddleware, thunkMiddleware))
+    : composeWithDevTools(applyMiddleware(firstMiddleware, thunkMiddleware));
+const store = createStore(reducer, initialState, enhance);
 
 export default store;
