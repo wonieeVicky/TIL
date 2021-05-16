da208ab8bf1e2d4ff45719470b3dbb3a42dd8133
Vicky <hwfongfing@gmail.com>
2021-05-16 23:02:58 +0900

    feat: create sequelize models

--- NodeJS/lecture/7.6/models/index.js
+++ NodeJS/lecture/7.6/models/index.js
@@ -38,11 +38,25 @@ module.exports = db;
  */
 
 const Sequelize = require("sequelize");
+const User = require("./user");
+const Comment = require("./comment");
+
 const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/../config/config.json")[env];
+const config = require("../config/config")[env];
 const db = {};
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
 db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+db.User = User;
+db.Comment = Comment;
+
+User.init(sequelize); // model과 MySQL 연결
+Comment.init(sequelize); // model과 MySQL 연결
+
+User.associate(db);
+Comment.associate(db);
 
 module.exports = db;
---
+++ NodeJS/lecture/7.6/models/user.js
@@ -0,0 +1,49 @@
+﻿const Sequelize = require("sequelize");
+
+module.exports = class User extends Sequelize.Model {
+  static init(sequelize) {
+    // super.init(모델에 대한 정의, 모델에 대한 설정)
+    return super.init(
+      {
+        // id는 알아서 넣어주므로 넣지 않아도 된다.
+        name: {
+          type: Sequelize.STRING(20),
+          allowNull: false, // Not null
+          unique: true, // 고유하게
+        },
+        age: {
+          type: Sequelize.INTEGER.UNSIGNED,
+          allowNull: false,
+        },
+        married: {
+          type: Sequelize.BOOLEAN,
+          allowNull: false,
+        },
+        comment: {
+          type: Sequelize.TEXT,
+          allowNull: true,
+        },
+        // 직접 구현해보았으나 timestamps 설정으로 자동 주입 가능
+        created_at: {
+          type: Sequelize.DATE, // DATETIME, MYSQL DATE -> Sequelize DateOnly
+          allowNull: false,
+          defaultValue: Sequelize.NOW,
+        },
+      },
+      {
+        sequelize,
+        timestamps: false, // default true -> createdAt, updatedAt 자동 들어간다.
+        underscored: false, // 자동으로 만들어주는 키들의 underscored 설정 - snake_case 사용 여부
+        modelName: "User", // 모델 이름, 자바스크립트에서 사용한다.
+        tableName: "users", // 테이블 이름 (모델이름이 Bird면 테이블 이름은 자동으로 birds로 저장된다.)
+        paranoid: false, // soft delete 사용 설정: deletedAt 자동 포함 여부
+        charset: "utf8",
+        collate: "utf8_general_ci", // utf8mb4_general_ci 이모티콘 쓸 수 있음
+      }
+    );
+  }
+
+  static associate(db) {
+    db.User.hasMany(db.Comment, { foreignKey: "commenter", sourceKey: "id" });
+  }
+};
