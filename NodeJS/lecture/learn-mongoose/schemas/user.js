c93b4a991b6398fb4032750aed0bfa75a98d67c5
Vicky <hwfongfing@gmail.com>
2021-05-27 22:33:12 +0900

    feat: project setting(mongoose)

---
+++ NodeJS/lecture/learn-mongoose/schemas/user.js
@@ -0,0 +1,25 @@
+﻿const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  married: {
+    type: Boolean,
+    required: true,
+  },
+  comment: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
