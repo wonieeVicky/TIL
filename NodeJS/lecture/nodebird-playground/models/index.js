6803ae287728c0ef05f61c633a0f0b177eb45275
Vicky <hwfongfing@gmail.com>
2021-05-31 23:36:05 +0900

    feat: sequelize db create 및 db models 설정

--- NodeJS/lecture/nodebird-playground/models/index.js
+++ NodeJS/lecture/nodebird-playground/models/index.js
@@ -1,37 +1,23 @@
-'use strict';
+const Sequelize = require("sequelize");
+const env = process.env.NODE_ENV || "development";
+const config = require("../config/config")[env]; // config.json 내 설정파일 불러오기
+const User = require("./user");
+const Post = require("./post");
+const Hashtag = require("./hashtag");
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
-
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
-
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
 db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+db.User = User;
+db.Post = Post;
+db.Hashtag = Hashtag;
+
+User.init(sequelize);
+Post.init(sequelize);
+Hashtag.init(sequelize);
+
+User.associate(db);
+Post.associate(db);
+Hashtag.associate(db);
 
 module.exports = db;
