4acd3329583b42ed1b34a6fb929ea4461fb48dc6
Vicky <hwfongfing@gmail.com>
2021-06-02 19:41:01 +0900

    feat: passport default, local 설정

---
+++ NodeJS/lecture/nodebird-playground/passport/index.js
@@ -0,0 +1,18 @@
+﻿const passport = require("passport");
+const local = require("./localStrategy");
+const kakao = require("./kakaoStrategy");
+const User = require("../models/user");
+
+module.exports = () => {
+  passport.serializeUser((user, done) => {
+    done(null, user.id); // 세션에 user의 id만 저장
+  });
+  // {id: 3, connect.sid: 1231ad9adkfaldfka }
+  passport.deserializeUser((id, done) => {
+    User.findOne({ where: { id } })
+      .then((user) => done(null, user))
+      .catch((err) => done(err));
+  });
+  local();
+  kakao();
+};
