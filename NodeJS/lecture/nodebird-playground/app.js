04c372d8b413f20fd5405306a9739f0a652e27f3
Vicky <hwfongfing@gmail.com>
2021-06-02 19:41:44 +0900

    feat: app.js에 passport 연결

--- NodeJS/lecture/nodebird-playground/app.js
+++ NodeJS/lecture/nodebird-playground/app.js
@@ -5,12 +5,16 @@ const path = require("path");
 const session = require("express-session");
 const nunjucks = require("nunjucks");
 const dotenv = require("dotenv");
+const passport = require("passport");
 
 dotenv.config();
 const pageRouter = require("./routes/page");
+const authRouter = require("./routes/auth");
 const { sequelize } = require("./models");
+const passportConfig = require("./passport");
 
 const app = express();
+passportConfig(); // 패스포트 설정
 app.set("port", process.env.PORT || 8001);
 app.set("view engine", "html");
 nunjucks.configure("views", {
@@ -18,7 +22,7 @@ nunjucks.configure("views", {
   watch: true,
 });
 sequelize
-  .sync({ force: false }) // force true 시 데이터를 모두 지운 뒤 새로 생성
+  .sync({ force: false })
   .then(() => {
     console.log("데이터베이스 연결 성공");
   })
@@ -29,7 +33,7 @@ sequelize
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(
   session({
@@ -42,7 +46,11 @@ app.use(
     },
   })
 );
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use("/", pageRouter);
+app.use("/auth", authRouter); // /auth/join ..
 
 app.use((req, res, next) => {
   const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
@@ -50,13 +58,13 @@ app.use((req, res, next) => {
   next(error);
 });
 
-// next는 반드시 존재해야 한다.
 app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
-  res.status(err.status || 500).render("error");
+  res.status(err.status || 500);
+  res.render("error");
 });
 
 app.listen(app.get("port"), () => {
-  console.log(app.get("port"), "번 포트에서 대기 중");
+  console.log(app.get("port"), "번 포트에서 대기중");
 });
