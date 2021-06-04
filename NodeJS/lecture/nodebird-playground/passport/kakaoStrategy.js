a97c52233d2112ad910cc8a88f03bb1f6002a77a
Vicky <hwfongfing@gmail.com>
2021-06-04 23:49:12 +0900

    feat: kakaoStrategy 설정

--- NodeJS/lecture/nodebird-playground/passport/kakaoStrategy.js
+++ NodeJS/lecture/nodebird-playground/passport/kakaoStrategy.js
@@ -1,1 +1,38 @@
-﻿
+﻿const passport = require("passport");
+const KakaoStrategy = require("passport-kakao").Strategy;
+
+const User = require("../models/user");
+
+module.exports = () => {
+  passport.use(
+    new KakaoStrategy(
+      {
+        clientID: process.env.KAKAO_ID,
+        callbackURL: "/auth/kakao/callback",
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        // OAUTH2
+        console.log("kakao profile", profile);
+        try {
+          const exUser = await User.findOne({
+            where: { snsId: profile.id, provider: "kakao" },
+          });
+          if (exUser) {
+            done(null, exUser);
+          } else {
+            const newUser = await User.create({
+              email: profile._json && profile._json.kakao_account_email,
+              nick: profile.displayName,
+              snsId: profile.id,
+              provider: "kakao",
+            });
+            done(null, newUser);
+          }
+        } catch (err) {
+          console.error(err);
+          done(err);
+        }
+      }
+    )
+  );
+};
