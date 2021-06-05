006be4843f06cc1b3d6da858112f54ab3877f222
Vicky <hwfongfing@gmail.com>
2021-06-05 23:41:13 +0900

    feat: post 라우터 구현(upload img - multer)

---
+++ NodeJS/lecture/nodebird-playground/routes/post.js
@@ -0,0 +1,78 @@
+﻿const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+
+const { Post, Hashtag } = require("../models");
+const { isLoggedIn } = require("./middlewares");
+
+const router = express.Router();
+
+try {
+  fs.readdirSync("uploads");
+} catch (err) {
+  console.error("upload 폴더가 없어 uploads 폴더를 생성합니다.");
+  fs.mkdirSync("uploads");
+}
+
+// upload 객체 안에 다양한 미들웨어가 포함된다.
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, cb) {
+      cb(null, "uploads/");
+    },
+    filename(req, file, cb) {
+      const ext = path.extname(file.originalname);
+      cb(null, path.basename(file.originalname, ext) + Date.now() + ext); // 이름 중복을 예방하기 위함
+    },
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 },
+});
+
+// POST /img
+router.post("/img", isLoggedIn, upload.single("img"), (req, res) => {
+  console.log(req.file);
+  res.json({ url: `/img/${req.file.filename}` }); // 실제 파일은 uploads에, 요청은 /img/~ : express.static이 처리해준다.
+});
+
+// POST /post - img upload 하지 않음
+router.post("/", isLoggedIn, upload.none(), async (req, res, next) => {
+  try {
+    const post = await Post.create({
+      content: req.body.content,
+      img: req.body.url,
+      UserId: req.user.id,
+    });
+    res.redirect("/");
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+/* 
+const upload2 = multer();
+router.post("/", isLoggedIn, upload2.none(), async (req, res, next) => {
+  try {
+    const post = await Post.create({
+      content: req.body.content,
+      img: req.body.url,
+    });*/
+// const hashtags = req.body.content.match(/#[^\s#]*/g);
+/*if (hashtags) {
+      const result = await Promise.all(
+        hashtags.map((tag) => {
+          return Hashtag.findOrCreate({
+            where: { title: tag.slice(1).toLowerCase() },
+          });
+        })
+      );
+      await post.addHashtags(result.map((r) => r[0]));
+    }
+    res.redirect("/");
+  } catch (err) {
+    console.error(err);
+    next(error);
+  }
+}); */
+
+module.exports = router;
