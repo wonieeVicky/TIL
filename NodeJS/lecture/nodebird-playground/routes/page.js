2190dfb4a28fecb31f626e0dd2ce54b43f25eac0
Vicky <hwfongfing@gmail.com>
2021-06-08 00:03:30 +0900

    feat: page 라우터에 GET /hashtag 구현

--- NodeJS/lecture/nodebird-playground/routes/page.js
+++ NodeJS/lecture/nodebird-playground/routes/page.js
@@ -1,5 +1,5 @@
 ﻿const express = require("express");
-const { Post, User } = require("../models");
+const { Post, User, Hashtag } = require("../models");
 const router = express.Router();
 
 router.use((req, res, next) => {
@@ -30,6 +30,29 @@ router.get("/", async (req, res, next) => {
     });
     res.render("main", {
       title: "NodeBird",
+      twits: posts,
+    });
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+
+// GET /hashtag?hashtag=노드
+router.get("/hashtag", async (req, res, next) => {
+  // API 요청 시 한글이 포함된 경우 : formdata가 아닐 경우 encodeURIComponent - decodeURIComponent 처리
+  const query = decodeURIComponent(req.query.hashtag);
+  if (!query) {
+    return res.redirect("/");
+  }
+  try {
+    const hashtag = await Hashtag.findOne({ where: { title: query } });
+    let posts = [];
+    if (hashtag) {
+      posts = await hashtag.getPosts({ include: [{ model: User, attributes: ["id", "nick"] }] });
+    }
+    return res.render("main", {
+      title: `#${query} 검색 결과 | NodeBird`,
       twits: posts,
     });
   } catch (err) {
