5615d1202f90d5467ef129d572aca6e806b6a6a8
Vicky <hwfongfing@gmail.com>
2021-06-14 23:59:10 +0900

    chore: v2 API 구현으로 인해 클라이언트 URL 데이터 교체

--- NodeJS/lecture/nodecat/routes/index.js
+++ NodeJS/lecture/nodecat/routes/index.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 
 const router = express.Router();
 
-const URL = "http://localhost:8002/v1";
+const URL = "http://localhost:8002/v2";
 axios.defaults.headers.origin = "http://localhost:4000";
 
 const request = async (req, api) => {
