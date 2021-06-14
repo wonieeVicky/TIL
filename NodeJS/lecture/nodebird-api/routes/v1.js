c1cbc5de7278155549359da4b69c3a524eb1f178
Vicky <hwfongfing@gmail.com>
2021-06-14 23:06:58 +0900

    feat: nodebird-api에 GET /posts/my, GET /posts/hashtag/:title 구현

--- NodeJS/lecture/nodebird-api/routes/v1.js
+++ NodeJS/lecture/nodebird-api/routes/v1.js
@@ -1,7 +1,7 @@
 ﻿const express = require("express");
 const jwt = require("jsonwebtoken");
 const { verifyToken } = require("./middlewares");
-const { Domain, User } = require("../models");
+const { Domain, User, Post, Hashtag } = require("../models");
 
 const router = express.Router();
 
@@ -50,6 +50,51 @@ router.post("/token", async (req, res) => {
 // 토큰 발급 테스트
 router.get("/test", verifyToken, (req, res) => {
   res.json(req.decoded);
+});
+
+router.get("/posts/my", verifyToken, (req, res) => {
+  Post.findAll({ where: { userId: req.decoded.id } })
+    .then((posts) => {
+      res.json({
+        code: 200,
+        payload: posts
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({
+        code: 500,
+        message: "서버 에러"
+      });
+    });
+});
+
+// 해시태그로 검색하는 라우터
+router.get("/posts/hashtag/:title", verifyToken, async (req, res) => {
+  try {
+    const hashtag = await Hashtag.findOne({
+      where: {
+        title: req.params.title
+      }
+    });
+    if (!hashtag) {
+      return res.status(404).json({
+        code: 404,
+        message: "검색 결과가 없습니다"
+      });
+    }
+    const posts = await hashtag.getPosts();
+    return res.json({
+      code: 200,
+      payload: posts
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({
+      code: 500,
+      message: "서버 에러"
+    });
+  }
 });
 
 module.exports = router;
