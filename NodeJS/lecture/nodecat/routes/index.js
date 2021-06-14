5f1d093d6fbd6232b2210c172e43be73e9cda5bd
Vicky <hwfongfing@gmail.com>
2021-06-14 23:07:34 +0900

    feat: 클라이언트에서 GET /mypost, GET /search/:hashtag 구현

--- NodeJS/lecture/nodecat/routes/index.js
+++ NodeJS/lecture/nodecat/routes/index.js
@@ -3,36 +3,56 @@ const axios = require("axios");
 
 const router = express.Router();
 
-// 토큰 테스트 라우터
-router.get("/test", async (req, res, next) => {
+const URL = "http://localhost:8002/v1";
+axios.defaults.headers.origin = "http://localhost:4000";
+
+const request = async (req, api) => {
   try {
     // 세션에 토큰이 없으면 토큰 발급 시도 - 초기
     if (!req.session.jwt) {
-      const tokenResult = await axios.post("http://localhost:8002/v1/token", {
+      const tokenResult = await axios.post(`${URL}/token`, {
         clientSecret: process.env.CLIENT_SECRET
       });
-      if (tokenResult.data && tokenResult.data.code === 200) {
-        // 토큰 발급 성공
-        req.session.jwt = tokenResult.data.token; // 세션에 토큰 저장
-      } else {
-        // 토큰 발급 실패
-        return res.json(tokenResult.data); // 발급 실패 사유 응답
-      }
+      req.session.jwt = tokenResult.data.token; // 세션에 토큰 저장
     }
+    return await axios.get(`${URL}${api}`, {
+      headers: { authorization: req.session.jwt }
+    });
     // 발급받은 토큰 테스트 :: verifyToken
-    const result = await axios.get("http://localhost:8002/v1/test", {
+    /* const result = await axios.get(`${URL}/test`, {
       headers: {
         authorization: req.session.jwt
       }
     });
-    return res.json(result.data);
+    return res.json(result.data); */
   } catch (error) {
     console.error(error);
     if (error.response.status === 419) {
       // 토큰 만료 시
-      return res.json(error.response.data);
+      delete req.session.jwt;
+      return request(req, api);
     }
-    return next(error);
+    return error.response;
+  }
+};
+
+router.get("/mypost", async (req, res, next) => {
+  try {
+    const result = await request(req, "/posts/my");
+    res.json(result.data);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.get("/search/:hashtag", async (req, res, next) => {
+  try {
+    const result = await request(req, `/posts/hashtag/${encodeURIComponent(req.params.hashtag)}`);
+    res.json(result.data);
+  } catch (error) {
+    console.error(error);
+    next(error);
   }
 });
 
