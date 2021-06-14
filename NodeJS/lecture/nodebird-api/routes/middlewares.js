5eda60db53357d46dccec3a6d6333c824fd4a494
Vicky <hwfongfing@gmail.com>
2021-06-14 23:57:16 +0900

    feat: apiLimiter, deprecated 미들웨어 구현

--- NodeJS/lecture/nodebird-api/routes/middlewares.js
+++ NodeJS/lecture/nodebird-api/routes/middlewares.js
@@ -1,4 +1,5 @@
 ﻿const jwt = require("jsonwebtoken");
+const slowDown = require("express-slow-down");
 
 exports.isLoggedIn = (req, res, next) => {
   if (req.isAuthenticated()) {
@@ -34,4 +35,23 @@ exports.verifyToken = (req, res, next) => {
       message: "유효하지 않은 토큰입니다."
     });
   }
+};
+
+exports.apiLimiter = slowDown({
+  windowMs: 60 * 1000, // 1분 간
+  delayAfter: 10, // 10번만 요청 가능
+  delayMs: 1000, // 요청 간 1초의 간격이 있다.
+  onLimitReached(req, res, options) {
+    res.status(429).json({
+      code: 429,
+      message: "1분에 10번만 요청할 수 있습니다."
+    });
+  }
+});
+
+exports.deprecated = (req, res) => {
+  res.status(410).json({
+    code: 410,
+    message: "새로운 버전이 나왔으니 버전 업그레이드를 해주세요!"
+  });
 };
