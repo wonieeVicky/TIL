83cb1a31232f8ae2bfb1e0c3a40a2b3375264e5a
Vicky <hwfongfing@gmail.com>
2021-06-18 19:01:38 +0900

    feat: 유닛 테스트를 위한 user routes 실행부를 controller로 분리

--- NodeJS/lecture/nodebird-test/routes/user.js
+++ NodeJS/lecture/nodebird-test/routes/user.js
@@ -1,23 +1,11 @@
 ﻿const express = require("express");
 
 const { isLoggedIn } = require("./middlewares");
+const { addFollowing } = require("../controllers/user");
 const User = require("../models/user");
 
 const router = express.Router();
 
-router.post("/:id/follow", isLoggedIn, async (req, res, next) => {
-  try {
-    const user = await User.findOne({ where: { id: req.user.id } });
-    if (user) {
-      await user.addFollowing(parseInt(req.params.id, 10));
-      res.send("success");
-    } else {
-      res.status(404).send("no user");
-    }
-  } catch (error) {
-    console.error(error);
-    next(error);
-  }
-});
+router.post("/:id/follow", isLoggedIn, addFollowing);
 
 module.exports = router;
