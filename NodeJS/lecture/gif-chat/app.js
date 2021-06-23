1fedc5d4f5f6a08dc39cba148ea3f116496395f7
Vicky <hwfongfing@gmail.com>
2021-06-23 23:50:08 +0900

    feat: app.js와 기본 라우터 구현

---
+++ NodeJS/lecture/gif-chat/app.js
@@ -0,0 +1,57 @@
+﻿const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const nunjucks = require("nunjucks");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const webSocket = require("./socket");
+const indexRouter = require("./routes");
+
+const app = express();
+app.set("port", process.env.PORT || 8005);
+app.set("view engine", "html");
+nunjucks.configure("views", {
+  express: app,
+  watch: true
+});
+
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false
+    }
+  })
+);
+
+app.use("/", indexRouter);
+
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.messagel;
+  res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+const server = app.listen(app.get("port"), () => {
+  console.log(app.get("port"), "번 포트에서 대기 중");
+});
+
+webSocket(server);
