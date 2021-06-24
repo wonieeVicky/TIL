cf98ce690e96350e57fe9fa07fad8601fabe4137
Vicky <hwfongfing@gmail.com>
2021-06-24 22:59:05 +0900

    feat: socket.js에 ws 대신 socket.io 적용하여 구현

--- NodeJS/lecture/gif-chat/socket.js
+++ NodeJS/lecture/gif-chat/socket.js
@@ -1,32 +1,30 @@
-﻿const WebSocket = require("ws");
+﻿const SocketIO = require("socket.io");
 
 module.exports = (server) => {
-  // web-socket-server
-  const wss = new WebSocket.Server({ server });
+  // 서버와 연결
+  const io = SocketIO(server, { path: "/socket.io" });
 
-  wss.on("connection", (ws, req) => {
-    // 웹 소켓 연결 시
-    const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress; // ip 파악
-    console.log("새로운 클라이언트 접속", ip);
-    // 클라이언트로부터 메시지
-    ws.on("message", (message) => {
-      console.log(message);
+  // 웹소켓 연결 시
+  io.on("connection", (socket) => {
+    const req = socket.request;
+    const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+    console.log("새로운 클라이언트 접속!", ip, socket.id, req.ip);
+    // 연결 종료 시
+    socket.on("disconnect", () => {
+      console.log("클라이언트 접속 해제", ip, socket.id);
+      clearInterval(socket.interval);
     });
     // 에러 시
-    ws.on("error", (error) => {
+    socket.on("error", (error) => {
       console.error(error);
     });
-    // 연결 종료 시
-    ws.on("close", () => {
-      console.log("클라이언트 접속 해제", ip);
-      clearInterval(ws.interval);
+    // 클라이언트로부터 메시지
+    socket.on("reply", (data) => {
+      console.log(data);
     });
-
     // 3초마다 클라이언트로 메시지 전송
-    ws.interval = setInterval(() => {
-      if (ws.readyState === ws.OPEN) {
-        ws.send("서버에서 클라이언트로 메시지를 보낸다.");
-      }
+    socket.interval = setInterval(() => {
+      socket.emit("news", "Hello Socket.IO");
     }, 3000);
   });
 };
