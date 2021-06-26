48ff7a4e3fa725f9ee6beb60f8901cf3b4e0c2ee
Vicky <hwfongfing@gmail.com>
2021-06-26 23:38:32 +0900

    feat: 실시간 채팅을 위한 chat, room, index 스키마 생성

---
+++ NodeJS/lecture/gif-chat/schemas/room.js
@@ -0,0 +1,27 @@
+﻿const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+const roomSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  max: {
+    type: Number,
+    required: true,
+    default: 10, // 최대 10명
+    min: 2 // 최소 2명
+  },
+  owner: {
+    // 방장
+    type: String,
+    required: true
+  },
+  password: String,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("Room", roomSchema);
