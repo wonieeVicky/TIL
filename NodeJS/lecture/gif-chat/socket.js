bcf67aef01a8d1b0db4f0a5e7f520dc2281e8b44
Vicky <hwfongfing@gmail.com>
2021-06-28 13:18:48 +0900

    feat: 클라이언트에서 웹소켓 연결시 transports 프로퍼티로 websocket 추가
    - socket.js에 미사용 코드 제거

--- NodeJS/lecture/gif-chat/socket.js
+++ NodeJS/lecture/gif-chat/socket.js
@@ -16,7 +16,7 @@ module.exports = (server, app) => {
 
   chat.on("connection", (socket) => {
     console.log("chat 네임스페이스에 접속");
-    const req = socekt.request;
+    const req = socket.request;
     const {
       headers: { referer }
     } = req;
@@ -28,28 +28,4 @@ module.exports = (server, app) => {
       socket.leave(roomId);
     });
   });
-
-  /* // 웹소켓 연결 시
-  io.on("connection", (socket) => {
-    const req = socket.request;
-    const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-    console.log("새로운 클라이언트 접속!", ip, socket.id, req.ip);
-    // 연결 종료 시
-    socket.on("disconnect", () => {
-      console.log("클라이언트 접속 해제", ip, socket.id);
-      clearInterval(socket.interval);
-    });
-    // 에러 시
-    socket.on("error", (error) => {
-      console.error(error);
-    });
-    // 클라이언트로부터 메시지
-    socket.on("reply", (data) => {
-      console.log(data);
-    });
-    // 3초마다 클라이언트로 메시지 전송
-    socket.interval = setInterval(() => {
-      socket.emit("news", "Hello Socket.IO");
-    }, 3000);
-  }); */
 };
