7757529d168aa0f9cf1730ace4eba33f266d4313
Vicky <hwfongfing@gmail.com>
2021-06-29 19:00:52 +0900

    feat: app.js에서 session 연결 영역을 미들웨어 변수로 분리

--- NodeJS/lecture/gif-chat/app.js
+++ NodeJS/lecture/gif-chat/app.js
@@ -21,24 +21,22 @@ nunjucks.configure("views", {
 });
 connect();
 
+const sessionMiddleware = session({
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false
+  }
+});
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(
-  session({
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.COOKIE_SECRET,
-    cookie: {
-      httpOnly: true,
-      secure: false
-    }
-  })
-);
+app.use(sessionMiddleware);
 
-// 세션이 끝나기 전까지는 req.session.color에 공유한 색상이 부여된다. 세션 종료 시 초기화
 app.use((req, res, next) => {
   if (!req.session.color) {
     const colorHash = new ColorHash();
@@ -56,14 +54,14 @@ app.use((req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-  res.locals.message = err.messagel;
+  res.locals.message = err.message;
   res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
   res.status(err.status || 500);
   res.render("error");
 });
 
 const server = app.listen(app.get("port"), () => {
-  console.log(app.get("port"), "번 포트에서 대기 중");
+  console.log(app.get("port"), "번 포트에서 대기중");
 });
 
-webSocket(server, app);
+webSocket(server, app, sessionMiddleware);
