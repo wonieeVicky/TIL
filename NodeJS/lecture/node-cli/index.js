dbb395a0b778e5ebacb19751921ae657bd59fdd8
Vicky <hwfongfing@gmail.com>
2021-07-11 01:30:33 +0900

    chore: node-cli에 간단한 stdIn, stdout 프로그램 구현

--- NodeJS/lecture/node-cli/index.js
+++ NodeJS/lecture/node-cli/index.js
@@ -1,2 +1,24 @@
-﻿#!/usr/bin/env node
-console.log("Hello CLI", process.argv);
+#!/usr/bin/env node
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin, // 콘솔에 입력
+  output: process.stdout, // 콘솔에 출력
+});
+
+console.clear(); // 콘솔 지우고 시작
+const answerCallback = (answer) => {
+  if (answer === "y") {
+    console.log("감사");
+    rl.close(); // 끝내기
+  } else if (answer === "n") {
+    console.log("죄송");
+    rl.close(); // 끝내기
+  } else {
+    console.log("y나 n만 입력하세요.");
+    rl.question("예제가 재미있습니까? (y/n)\n", answerCallback);
+  }
+};
+
+rl.question("예제가 재미있습니까? (y/n)\n", answerCallback);
