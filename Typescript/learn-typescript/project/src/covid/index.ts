f597566bb8be059d80e15ece096ba91215c986a9
Vicky <hwfongfing@gmail.com>
2021-07-26 23:51:27 +0900

    feat: add fetchCovidResponse type

---
+++ Typescript/learn-typescript/project/src/covid/index.ts
@@ -0,0 +1,30 @@
+﻿interface Country {
+  Country: string;
+  CountryCode: string;
+  Date: string;
+  ID: string;
+  NewConfirmed: number;
+  NewDeaths: number;
+  NewRecovered: number;
+  Premium: any;
+  Slug: string;
+  TotalConfirmed: number;
+  TotalDeaths: number;
+  TotalRecovered: number;
+}
+
+interface Global {
+  Date: string;
+  NewConfirmed: number;
+  NewDeaths: number;
+  NewRecovered: number;
+  TotalConfirmed: number;
+  TotalDeaths: number;
+}
+
+export interface CovidSummaryReponse {
+  Countries: Country[];
+  Date: string;
+  Global: Global;
+  Message: string;
+}
