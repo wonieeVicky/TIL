6dda824469fac8c86d78450a81178bf7ca2db9cd
Vicky <hwfongfing@gmail.com>
2022-03-05 23:55:34 +0900

    feat: xlsx parsing to json 구현

--- NodeJS/lecture/nodejs-crawler/1-csv-parsing-example/index.js
+++ NodeJS/lecture/nodejs-crawler/1-csv-parsing-example/index.js
@@ -1,8 +1,12 @@
-﻿const parse = require("csv-parse/lib/sync");
-const fs = require("fs");
+﻿const xlsx = require("xlsx");
 
-const csv = fs.readFileSync("csv/data.csv");
-const records = parse(csv.toString("utf-8"));
-records.forEach((r, i) => {
-  console.log(i, r);
-});
+const workbook = xlsx.readFile("xlsx/data.xlsx"); // readFile로 엑셀을 읽음
+
+// console.log(Object.keys(workbook.Sheets)); // 영화목록
+const ws = workbook.Sheets.영화목록;
+const records = xlsx.utils.sheet_to_json(ws);
+
+// 배열.entries를 쓰면 내부 배열이 [인덱스, 값]모양 이터레이터로 바뀜
+for (const [i, r] of records.entries()) {
+  console.log(i, r.제목, r.링크);
+}
