ca318d80251cba001f0679fe22d8efea6a26c60c
Vicky <hwfongfing@gmail.com>
2022-03-06 23:14:04 +0900

    feat: axios, cheerio 조합으로 xlsx 내부 정보를 기반으로한 웹 크롤링 구현

--- NodeJS/lecture/nodejs-crawler/1-csv-parsing-example/index.js
+++ NodeJS/lecture/nodejs-crawler/1-csv-parsing-example/index.js
@@ -1,12 +1,24 @@
 ﻿const xlsx = require("xlsx");
+const axios = require("axios"); // ajax 라이브러리
+const cheerio = require("cheerio"); // html 파싱
 
-const workbook = xlsx.readFile("xlsx/data.xlsx"); // readFile로 엑셀을 읽음
-
-// console.log(Object.keys(workbook.Sheets)); // 영화목록
+const workbook = xlsx.readFile("xlsx/data.xlsx");
 const ws = workbook.Sheets.영화목록;
 const records = xlsx.utils.sheet_to_json(ws);
 
-// 배열.entries를 쓰면 내부 배열이 [인덱스, 값]모양 이터레이터로 바뀜
-for (const [i, r] of records.entries()) {
-  console.log(i, r.제목, r.링크);
-}
+const crawler = async () => {
+  await Promise.all(
+    records.map(async (r) => {
+      const response = await axios.get(r.링크);
+      if (response.status === 200) {
+        // 응답이 성공한 경우
+        const html = response.data;
+        const $ = cheerio.load(html);
+        const text = $(".score.score_left .star_score").text();
+        console.log(r.제목, "평점:", text.trim());
+      }
+    })
+  );
+};
+
+crawler();
