2df630154135cba036e3dcbe13ea83abe1798f78
Vicky <hwfongfing@gmail.com>
2022-03-07 23:57:08 +0900

    feat: xlsx 패키지 각종 메서드 활용(ws["!ref"],xlsx.SheetName 등)

--- NodeJS/lecture/nodejs-crawler/1-csv-parsing-example/index.js
+++ NodeJS/lecture/nodejs-crawler/1-csv-parsing-example/index.js
@@ -3,10 +3,20 @@ const axios = require("axios"); // ajax 라이브러리
 const cheerio = require("cheerio"); // html 파싱
 
 const workbook = xlsx.readFile("xlsx/data.xlsx");
-const ws = workbook.Sheets.영화목록;
-const records = xlsx.utils.sheet_to_json(ws);
+for (const name of workbook.SheetNames) {
+  const ws = workbook.Sheets.name;
+  // 시트별로 따로 코딩
+}
+ws["!ref"] = ws["!ref"]
+  .split(":")
+  .map((v, i) => (i === 0 ? "A2" : v))
+  .join(":");
+// 혹은 ws["!ref"] = 'A2:B11'로 직접 변경 가능 ㅋ
 
-const crawler = async () => {
+const records = xlsx.utils.sheet_to_json(ws, { header: "A" });
+console.log(records);
+
+/* const crawler = async () => {
   for (const [i, r] of records.entries()) {
     const response = await axios.get(r.링크);
     if (response.status === 200) {
@@ -17,7 +27,7 @@ const crawler = async () => {
       console.log(r.제목, "평점:", text.trim());
     }
   }
-  /* await Promise.all(records.map(async (r) => {})); */
+  await Promise.all(records.map(async (r) => {}));
 };
 
-crawler();
+crawler(); */
