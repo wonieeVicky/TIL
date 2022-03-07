b621ae42672fb4d98ca1b960ba88ad148a0b00cb
Vicky <hwfongfing@gmail.com>
2022-03-07 23:34:35 +0900

    feat: for-of 문을 통한 웹 크롤링 순서 보장 처리 구현

--- NodeJS/lecture/nodejs-crawler/1-csv-parsing-example/index.js
+++ NodeJS/lecture/nodejs-crawler/1-csv-parsing-example/index.js
@@ -7,18 +7,17 @@ const ws = workbook.Sheets.영화목록;
 const records = xlsx.utils.sheet_to_json(ws);
 
 const crawler = async () => {
-  await Promise.all(
-    records.map(async (r) => {
-      const response = await axios.get(r.링크);
-      if (response.status === 200) {
-        // 응답이 성공한 경우
-        const html = response.data;
-        const $ = cheerio.load(html);
-        const text = $(".score.score_left .star_score").text();
-        console.log(r.제목, "평점:", text.trim());
-      }
-    })
-  );
+  for (const [i, r] of records.entries()) {
+    const response = await axios.get(r.링크);
+    if (response.status === 200) {
+      // 응답이 성공한 경우
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const text = $(".score.score_left .star_score").text();
+      console.log(r.제목, "평점:", text.trim());
+    }
+  }
+  /* await Promise.all(records.map(async (r) => {})); */
 };
 
 crawler();
