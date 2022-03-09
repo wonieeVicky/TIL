df8819c42cd4d9102deee3d1fdf56bfe68260d88
Vicky <hwfongfing@gmail.com>
2022-03-09 23:37:45 +0900

    feat: crawler 후 writeFile update 구현(result.xlsx)

--- NodeJS/lecture/nodejs-crawler/1-csv-parsing-example/index.js
+++ NodeJS/lecture/nodejs-crawler/1-csv-parsing-example/index.js
@@ -1,22 +1,14 @@
 ﻿const xlsx = require("xlsx");
 const axios = require("axios"); // ajax 라이브러리
 const cheerio = require("cheerio"); // html 파싱
+const add_to_sheet = require("./add_to_sheet");
 
 const workbook = xlsx.readFile("xlsx/data.xlsx");
-for (const name of workbook.SheetNames) {
-  const ws = workbook.Sheets.name;
-  // 시트별로 따로 코딩
-}
-ws["!ref"] = ws["!ref"]
-  .split(":")
-  .map((v, i) => (i === 0 ? "A2" : v))
-  .join(":");
-// 혹은 ws["!ref"] = 'A2:B11'로 직접 변경 가능 ㅋ
+const ws = workbook.Sheets.영화목록;
+const records = xlsx.utils.sheet_to_json(ws);
 
-const records = xlsx.utils.sheet_to_json(ws, { header: "A" });
-console.log(records);
-
-/* const crawler = async () => {
+const crawler = async () => {
+  add_to_sheet(ws, "C1", "s", "평점"); // C1에 평점 raw를 string 값을 넣는다.
   for (const [i, r] of records.entries()) {
     const response = await axios.get(r.링크);
     if (response.status === 200) {
@@ -25,9 +17,12 @@ console.log(records);
       const $ = cheerio.load(html);
       const text = $(".score.score_left .star_score").text();
       console.log(r.제목, "평점:", text.trim());
+      const newCell = "C" + (i + 2);
+      add_to_sheet(ws, newCell, "n", parseFloat(text.trim()));
     }
   }
-  await Promise.all(records.map(async (r) => {}));
+  xlsx.writeFile(workbook, "xlsx/result.xlsx");
+  // await Promise.all(records.map(async (r) => {}));
 };
 
-crawler(); */
+crawler();
