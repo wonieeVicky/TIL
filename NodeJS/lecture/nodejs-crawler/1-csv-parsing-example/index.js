a979808ea5e47260156f7f12e2e3863f2aef5bd5
Vicky <hwfongfing@gmail.com>
2022-03-11 23:37:38 +0900

    feat: headless option을 dev 환경일 때만 false 처리, Promise.all로 동시성 부여

--- NodeJS/lecture/nodejs-crawler/1-csv-parsing-example/index.js
+++ NodeJS/lecture/nodejs-crawler/1-csv-parsing-example/index.js
@@ -6,16 +6,14 @@ const csv = fs.readFileSync("csv/data.csv");
 const records = parse(csv.toString("utf-8"));
 
 const crawler = async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page1 = await browser.newPage();
-  const page2 = await browser.newPage();
-  const page3 = await browser.newPage();
-  await page1.goto("https://github.com/wonieeVicky");
-  await page2.goto("https://www.naver.com");
-  await page3.goto("https://www.google.com");
-  await page1.waitForTimeout(1000); // 3초 대기
-  await page2.waitForTimeout(1000); // 1초 대기
-  await page3.waitForTimeout(1000); // 1초 대기
+  const browser = await puppeteer.launch({ headless: process.env.NODE_ENV === "production" });
+  const [page1, page2, page3] = await Promise.all([browser.newPage(), browser.newPage(), browser.newPage()]);
+  await Promise.all([
+    page1.goto("https://github.com/wonieeVicky"),
+    page2.goto("https://www.naver.com"),
+    page3.goto("https://www.google.com"),
+  ]);
+  await Promise.all([page1.waitForTimeout(3000), page2.waitForTimeout(500), page3.waitForTimeout(2000)]);
   await page1.close(); // 페이지 Close
   await page2.close(); // 페이지 Close
   await page3.close(); // 페이지 Close
