4466e3d0e6dd6d7db59806af5f56794ae36fdb84
Vicky <hwfongfing@gmail.com>
2022-03-14 23:54:48 +0900

    feat: update nodejs-crawler-2(save updated data in result.csv) test code

--- NodeJS/lecture/nodejs-crawler/1-csv-parsing-example/index.js
+++ NodeJS/lecture/nodejs-crawler/1-csv-parsing-example/index.js
@@ -1,4 +1,5 @@
 ﻿const parse = require("csv-parse/lib/sync");
+const { stringify } = require("csv-stringify/sync");
 const fs = require("fs");
 const puppeteer = require("puppeteer");
 
@@ -6,24 +7,23 @@ const csv = fs.readFileSync("csv/data.csv");
 const records = parse(csv.toString("utf-8"));
 
 const crawler = async () => {
-  // try ~ catch는 async 함수 내부에서 사용하여 에러를 잡는다.
   try {
+    const result = [];
     const browser = await puppeteer.launch({ headless: process.env.NODE_ENV === "production" });
     await Promise.all(
       records.map(async (r, i) => {
-        // 내부에 async 함수가 한번 더 사용되었으니 try ~ catch로 한번 더 감싸준다.
         try {
-          const page = await browser.newPage(); // 동시에 페이지 10개가 열린다.
-          await page.goto(r[1]); // 동시에 페이지에 방문한다.
-          const scoreEl = await page.$(".score.score_left .star_score"); // 별점 엘리먼트로 이동
+          const page = await browser.newPage();
+          await page.goto(r[1]);
+          const scoreEl = await page.$(".score.score_left .star_score");
           if (scoreEl) {
-            // 태그를 잘 찾았으면
-            // evaluate 함수를 통해 찾은 태그로 textContent를 반환
-            const text = await page.evaluate((tag) => tag.textContent, scoreEl); // 평점
-            // console.log(r[0], "평점", text.trim());
+            const text = await page.evaluate((tag) => tag.textContent, scoreEl);
+            result[i] = [r[0], r[1], text.trim()]; // 순서 보장을 위해 result 배열의 인덱스로 정보를 저장
           }
-          await page.waitForTimeout(3000); // 웹크롤러 방지 코드에 걸리지 않도록 처리
+          await page.waitForTimeout(3000);
           await page.close();
+          const str = stringify(result);
+          fs.writeFileSync("csv/result.csv", str);
         } catch (e) {
           console.error(e);
         }
