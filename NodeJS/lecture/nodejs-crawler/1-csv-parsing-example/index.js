3aa1cf1a9610947ee8b8ec9cb29f50f483f8e5d9
Vicky <hwfongfing@gmail.com>
2022-03-14 23:33:49 +0900

    feat: update nodejs-crawler-2(puppeteer로 웹 크롤러 구현) test cod

--- NodeJS/lecture/nodejs-crawler/1-csv-parsing-example/index.js
+++ NodeJS/lecture/nodejs-crawler/1-csv-parsing-example/index.js
@@ -6,18 +6,33 @@ const csv = fs.readFileSync("csv/data.csv");
 const records = parse(csv.toString("utf-8"));
 
 const crawler = async () => {
-  const browser = await puppeteer.launch({ headless: process.env.NODE_ENV === "production" });
-  const [page1, page2, page3] = await Promise.all([browser.newPage(), browser.newPage(), browser.newPage()]);
-  await Promise.all([
-    page1.goto("https://github.com/wonieeVicky"),
-    page2.goto("https://www.naver.com"),
-    page3.goto("https://www.google.com"),
-  ]);
-  await Promise.all([page1.waitForTimeout(3000), page2.waitForTimeout(500), page3.waitForTimeout(2000)]);
-  await page1.close(); // 페이지 Close
-  await page2.close(); // 페이지 Close
-  await page3.close(); // 페이지 Close
-  await browser.close(); // 브라우저 Close
+  // try ~ catch는 async 함수 내부에서 사용하여 에러를 잡는다.
+  try {
+    const browser = await puppeteer.launch({ headless: process.env.NODE_ENV === "production" });
+    await Promise.all(
+      records.map(async (r, i) => {
+        // 내부에 async 함수가 한번 더 사용되었으니 try ~ catch로 한번 더 감싸준다.
+        try {
+          const page = await browser.newPage(); // 동시에 페이지 10개가 열린다.
+          await page.goto(r[1]); // 동시에 페이지에 방문한다.
+          const scoreEl = await page.$(".score.score_left .star_score"); // 별점 엘리먼트로 이동
+          if (scoreEl) {
+            // 태그를 잘 찾았으면
+            // evaluate 함수를 통해 찾은 태그로 textContent를 반환
+            const text = await page.evaluate((tag) => tag.textContent, scoreEl); // 평점
+            // console.log(r[0], "평점", text.trim());
+          }
+          await page.waitForTimeout(3000); // 웹크롤러 방지 코드에 걸리지 않도록 처리
+          await page.close();
+        } catch (e) {
+          console.error(e);
+        }
+      })
+    );
+    await browser.close(); // 브라우저 Close
+  } catch (e) {
+    console.error(e);
+  }
 };
 
 crawler();
