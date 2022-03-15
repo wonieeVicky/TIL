6c8dc786a1dda26e53fe4487a5fc359882dd99c2
Vicky <hwfongfing@gmail.com>
2022-03-15 14:23:24 +0900

    feat: update nodejs-crawler-2(xlsx 파일 생성) test code

--- NodeJS/lecture/nodejs-crawler/1-csv-parsing-example/index.js
+++ NodeJS/lecture/nodejs-crawler/1-csv-parsing-example/index.js
@@ -1,37 +1,36 @@
-﻿const parse = require("csv-parse/lib/sync");
-const { stringify } = require("csv-stringify/sync");
-const fs = require("fs");
-const puppeteer = require("puppeteer");
-
-const csv = fs.readFileSync("csv/data.csv");
-const records = parse(csv.toString("utf-8"));
+﻿const puppeteer = require("puppeteer");
+const add_to_sheet = require("./add_to_sheet");
+const xlsx = require("xlsx"); // xlsx 패키지 호출
+const workbook = xlsx.readFile("xlsx/data.xlsx");
+const ws = workbook.Sheets.영화목록; // 조회할 시트 가져오기
+const records = xlsx.utils.sheet_to_json(ws); // 시트를 json 형태로 변환
 
 const crawler = async () => {
   try {
-    const result = [];
     const browser = await puppeteer.launch({ headless: process.env.NODE_ENV === "production" });
-    await Promise.all(
-      records.map(async (r, i) => {
-        try {
-          const page = await browser.newPage();
-          await page.goto(r[1]);
-          const text = await page.evaluate(() => {
-            const score = document.querySelector(".score.score_left .star_score");
-            if (score) {
-              return score.textContent;
-            }
-          });
-          result[i] = [r[0], r[1], text.trim()];
-          await page.waitForTimeout(3000);
-          await page.close();
-          const str = stringify(result);
-          fs.writeFileSync("csv/result.csv", str);
-        } catch (e) {
-          console.error(e);
-        }
-      })
+    const page = await browser.newPage();
+    await page.setUserAgent(
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36"
     );
-    await browser.close(); // 브라우저 Close
+    add_to_sheet(ws, "C1", "s", "평점"); // sheet에 평점 column 추가
+    for (const [i, r] of records.entries()) {
+      await page.goto(r.링크);
+      const text = await page.evaluate(() => {
+        const score = document.querySelector(".score.score_left .star_score");
+        if (score) {
+          return score.textContent;
+        }
+      });
+      if (text) {
+        console.log(r.제목, "평점: ", text.trim());
+        const newCell = "C" + (i + 2);
+        add_to_sheet(ws, newCell, "n", parseFloat(text.trim())); // sheet에 평점 row 추가
+      }
+      await page.waitForTimeout(3000);
+    }
+    await page.close();
+    await browser.close();
+    xlsx.writeFile(workbook, "xlsx/result.xlsx"); // xlsx 파일로 export
   } catch (e) {
     console.error(e);
   }
