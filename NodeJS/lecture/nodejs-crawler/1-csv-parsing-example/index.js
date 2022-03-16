be43d0d11d9bf0aac979716559be4a954beec6c8
Vicky <hwfongfing@gmail.com>
2022-03-16 19:02:21 +0900

    feat: add nodejs-crawler-3(axios와 fs를 활용한 이미지 다운로드 구현)

--- NodeJS/lecture/nodejs-crawler/1-csv-parsing-example/index.js
+++ NodeJS/lecture/nodejs-crawler/1-csv-parsing-example/index.js
@@ -4,6 +4,23 @@ const xlsx = require("xlsx"); // xlsx 패키지 호출
 const workbook = xlsx.readFile("xlsx/data.xlsx");
 const ws = workbook.Sheets.영화목록; // 조회할 시트 가져오기
 const records = xlsx.utils.sheet_to_json(ws); // 시트를 json 형태로 변환
+const fs = require("fs");
+const axios = require("axios");
+
+fs.readdir("screenshot", (err) => {
+  // screenshot 폴더가 있는지 확인 후 없으면 에러 - 기존 폴더가 있을 경우 충돌 에러 발생하므로
+  if (err) {
+    console.error("screenshot 폴더가 없어 screenshot 폴더를 생성한다.");
+    fs.mkdirSync("screenshot"); // sync 메서드는 프로그램의 처음과 끝에만 사용하는 것이 좋음
+  }
+});
+fs.readdir("poster", (err) => {
+  // poster 폴더가 있는지 확인 후 없으면 에러 - 기존 폴더가 있을 경우 충돌 에러 발생하므로
+  if (err) {
+    console.error("poster 폴더가 없어 poster 폴더를 생성한다.");
+    fs.mkdirSync("poster");
+  }
+});
 
 const crawler = async () => {
   try {
@@ -15,16 +32,28 @@ const crawler = async () => {
     add_to_sheet(ws, "C1", "s", "평점"); // sheet에 평점 column 추가
     for (const [i, r] of records.entries()) {
       await page.goto(r.링크);
-      const text = await page.evaluate(() => {
-        const score = document.querySelector(".score.score_left .star_score");
-        if (score) {
-          return score.textContent;
+      const result = await page.evaluate(() => {
+        const scoreEl = document.querySelector(".score.score_left .star_score");
+        let score = "";
+        if (scoreEl) {
+          score = scoreEl.textContent;
         }
+        const imgEl = document.querySelector(".poster img");
+        let img = "";
+        if (imgEl) {
+          img = imgEl.src;
+        }
+        return { score, img };
       });
-      if (text) {
-        console.log(r.제목, "평점: ", text.trim());
+      if (result.score) {
+        console.log(r.제목, "평점: ", result.score.trim());
         const newCell = "C" + (i + 2);
-        add_to_sheet(ws, newCell, "n", parseFloat(text.trim())); // sheet에 평점 row 추가
+        add_to_sheet(ws, newCell, "n", parseFloat(result.score.trim())); // sheet에 평점 row 추가
+      }
+      if (result.img) {
+        // buffer가 연속적으로 들어있는 자료구조가 arraybuffer
+        const imgResult = await axios.get(result.img.replace(/\?.+$/, ""), { responseType: "arraybuffer" });
+        fs.writeFileSync(`poster/${r.제목}.jpg`, imgResult.data);
       }
       await page.waitForTimeout(3000);
     }
