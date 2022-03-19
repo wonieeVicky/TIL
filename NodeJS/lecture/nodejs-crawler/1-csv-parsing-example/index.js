3d66d4b2f88e5b72bce2d00a95065685e8d89919
Vicky <hwfongfing@gmail.com>
2022-03-19 23:18:57 +0900

    feat: update nodejs-crawler-3(크롤링 완료된 element 삭제 및 스크롤 다운, waitForSelector 메서드 적용) test code

--- NodeJS/lecture/nodejs-crawler/1-csv-parsing-example/index.js
+++ NodeJS/lecture/nodejs-crawler/1-csv-parsing-example/index.js
@@ -9,24 +9,25 @@ const crawler = async () => {
     await page.goto("https://unsplash.com");
     const result = await page.evaluate(() => {
       let imgs = [];
-      const imgEls = document.querySelectorAll(".ripi6 img.YVj9w");
+      const imgEls = document.querySelectorAll(".ripi6");
       if (imgEls.length) {
-        imgEls.forEach((v) => v.src && imgs.push(v.src));
+        imgEls.forEach((v) => {
+          let src = v.querySelector("img.YVj9w").src;
+          src && imgs.push(src);
+          v.parentElement.removeChild(v); // 해당 엘리먼트(.ripi6)의 부모로 옮겨가서 엘리먼트를 지워준다.
+        });
       }
+      window.scrollBy(0, 100); // 세로로 100px 내려준다.
       return imgs;
     });
     console.log("result:", result);
+    // waitForSelector는 특정 선택자를 기다릴 수 있다.
+    // 단, 30초간 기다린 후 선택자를 못 찾으면 timeout 에러
+    await page.waitForSelector(".ripi6");
+    console.log("새 이미지 태그 로드 완료");
   } catch (e) {
     console.error(e);
   }
 };
 
 crawler();
-result: [
-  "https://images.unsplash.com/photo-1640622842924-3ae860f77265?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&w=1000&q=80",
-  "https://images.unsplash.com/photo-1647533532539-ff164151cd89?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&w=1000&q=80",
-  "https://images.unsplash.com/photo-1647606472102-ca1959036bdf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHw%3D&w=1000&q=80",
-  "https://images.unsplash.com/photo-1638913661377-abd9e8cf1998?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fHw%3D&w=1000&q=80",
-  "https://images.unsplash.com/photo-1634072319894-107e61606191?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHw%3D&w=1000&q=80",
-  "https://images.unsplash.com/photo-1647610365194-662ed67a998e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8&w=1000&q=80",
-];
