a88b2809689c8602c2dbd0b5add6e83e3508fc43
Vicky <hwfongfing@gmail.com>
2022-03-20 23:25:50 +0900

    feat: update nodejs-crawler-3(인피니트 스크롤 이미지 로드 버그 개선 및 while문을 통한 이미지 재호출 로직 구현) test code

--- NodeJS/lecture/nodejs-crawler/1-csv-parsing-example/index.js
+++ NodeJS/lecture/nodejs-crawler/1-csv-parsing-example/index.js
@@ -7,24 +7,31 @@ const crawler = async () => {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     await page.goto("https://unsplash.com");
-    const result = await page.evaluate(() => {
-      let imgs = [];
-      const imgEls = document.querySelectorAll(".ripi6");
-      if (imgEls.length) {
-        imgEls.forEach((v) => {
-          let src = v.querySelector("img.YVj9w").src;
-          src && imgs.push(src);
-          v.parentElement.removeChild(v); // 해당 엘리먼트(.ripi6)의 부모로 옮겨가서 엘리먼트를 지워준다.
-        });
-      }
-      window.scrollBy(0, 100); // 세로로 100px 내려준다.
-      return imgs;
-    });
-    console.log("result:", result);
-    // waitForSelector는 특정 선택자를 기다릴 수 있다.
-    // 단, 30초간 기다린 후 선택자를 못 찾으면 timeout 에러
-    await page.waitForSelector(".ripi6");
-    console.log("새 이미지 태그 로드 완료");
+    let result = [];
+    // img srcs가 30개 모일 때까지 반복한다.
+    while (result.length <= 30) {
+      const srcs = await page.evaluate(() => {
+        window.scrollTo(0, 0);
+        let imgs = [];
+        const imgEls = document.querySelectorAll(".ripi6");
+        if (imgEls.length) {
+          imgEls.forEach((v) => {
+            let src = v.querySelector("img.YVj9w")?.src; // element가 있으면 src 담는다.
+            src && imgs.push(src);
+            v.innerHTML = "";
+            // v.parentElement.removeChild(v); // 해당 엘리먼트(.ripi6)의 부모로 옮겨가서 엘리먼트를 지워준다.
+          });
+        }
+        window.scrollBy(0, 100); // 세로로 300px 내려준다.
+        return imgs;
+      });
+      result = result.concat(srcs);
+      await page.waitForSelector(".ripi6");
+      console.log("새 이미지 태그 로드 완료");
+    }
+    console.log(result);
+    await page.close();
+    await browser.close();
   } catch (e) {
     console.error(e);
   }
