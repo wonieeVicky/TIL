a003c0fa669ab1793ede8348852aa36018fa3741
Vicky <hwfongfing@gmail.com>
2022-03-20 23:30:53 +0900

    fix: update nodejs-crawler-3(인피니트 스크롤 이미지 로드 버그 개선)

--- NodeJS/lecture/nodejs-crawler/1-csv-parsing-example/index.js
+++ NodeJS/lecture/nodejs-crawler/1-csv-parsing-example/index.js
@@ -11,18 +11,18 @@ const crawler = async () => {
     // img srcs가 30개 모일 때까지 반복한다.
     while (result.length <= 30) {
       const srcs = await page.evaluate(() => {
-        window.scrollTo(0, 0);
+        window.scrollTo(0, 0); // 절대 좌표
         let imgs = [];
         const imgEls = document.querySelectorAll(".ripi6");
         if (imgEls.length) {
           imgEls.forEach((v) => {
             let src = v.querySelector("img.YVj9w")?.src; // element가 있으면 src 담는다.
             src && imgs.push(src);
-            v.innerHTML = "";
-            // v.parentElement.removeChild(v); // 해당 엘리먼트(.ripi6)의 부모로 옮겨가서 엘리먼트를 지워준다.
+            v.innerHTML = ""; // imgEl 내부만 비워주는 방식으로 변경
+            // v.parentElement.removeChild(v); // 동작 x, 실제 사이트에서 같은 방법으로 시도했을때 이미지 로드 불가
           });
         }
-        window.scrollBy(0, 100); // 세로로 300px 내려준다.
+        window.scrollBy(0, 100); // 상대 좌표: 현재의 위치에서 스크롤 100px 이동
         return imgs;
       });
       result = result.concat(srcs);
