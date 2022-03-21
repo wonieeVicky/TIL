4f0908652fcfd733e985c765f78702d7c3e75202
Vicky <hwfongfing@gmail.com>
2022-03-21 23:34:19 +0900

    feat: update nodejs-crawler-3(src result를 axios로 요청하여 결과물 파일에 저장 구현) test code

--- NodeJS/lecture/nodejs-crawler/1-csv-parsing-example/index.js
+++ NodeJS/lecture/nodejs-crawler/1-csv-parsing-example/index.js
@@ -2,6 +2,14 @@
 const axios = require("axios");
 const puppeteer = require("puppeteer");
 
+// imgs 폴더 생성
+fs.readdir("imgs", (err) => {
+  if (err) {
+    console.error("imgs 폴더가 없어 imgs 폴더를 생성합니다.");
+    fs.mkdirSync("imgs");
+  }
+});
+
 const crawler = async () => {
   try {
     const browser = await puppeteer.launch({ headless: false });
@@ -9,17 +17,16 @@ const crawler = async () => {
     await page.goto("https://unsplash.com");
     let result = [];
     // img srcs가 30개 모일 때까지 반복한다.
-    while (result.length <= 30) {
+    while (result.length <= 5) {
       const srcs = await page.evaluate(() => {
         window.scrollTo(0, 0); // 절대 좌표
         let imgs = [];
         const imgEls = document.querySelectorAll(".ripi6");
         if (imgEls.length) {
           imgEls.forEach((v) => {
-            let src = v.querySelector("img.YVj9w")?.src; // element가 있으면 src 담는다.
+            let src = v.querySelector("img.YVj9w")?.src;
             src && imgs.push(src);
-            v.innerHTML = ""; // imgEl 내부만 비워주는 방식으로 변경
-            // v.parentElement.removeChild(v); // 동작 x, 실제 사이트에서 같은 방법으로 시도했을때 이미지 로드 불가
+            v.innerHTML = "";
           });
         }
         window.scrollBy(0, 100); // 상대 좌표: 현재의 위치에서 스크롤 100px 이동
@@ -27,9 +34,15 @@ const crawler = async () => {
       });
       result = result.concat(srcs);
       await page.waitForSelector(".ripi6");
-      console.log("새 이미지 태그 로드 완료");
     }
     console.log(result);
+    result.forEach(async (src) => {
+      const imgResult = await axios.get(src.replace(/\?.*$/, ""), {
+        responseType: "arraybuffer",
+      });
+      fs.writeFileSync(`imgs/${new Date().valueOf()}.jpeg`, imgResult.data);
+    });
+    // 크롤링 결과를 파일로 만들기
     await page.close();
     await browser.close();
   } catch (e) {
