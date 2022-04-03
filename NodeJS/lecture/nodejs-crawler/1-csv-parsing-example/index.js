d786ae4d63eef2aa135ef1a0baeef1f3262e6e18
Vicky <hwfongfing@gmail.com>
2022-04-03 15:53:37 +0900

    feat: update nodejs-crawler-6 (크롤러 결과 db.create 처리, findOne으로 정보 추출 및 connection 종료)

--- NodeJS/lecture/nodejs-crawler/1-csv-parsing-example/index.js
+++ NodeJS/lecture/nodejs-crawler/1-csv-parsing-example/index.js
@@ -24,28 +24,46 @@ const crawler = async () => {
       return ips.map((v, i) => ({
         ip: v,
         type: types[i],
-        latencies: latencies[i],
+        latency: latencies[i],
       }));
     });
     // type이 HTTP, HTTPS로 시작하고, latencies가 가장 작은, 즉 가장 빠른 ip를 찾는다.
-    const filtered = proxies.filter((v) => v.type.startsWith("HTTPS")).sort((p, c) => p.latencies - c.latencies);
-    console.log(filtered);
+    const filtered = proxies.filter((v) => v.type.startsWith("HTTP")).sort((p, c) => p.latency - c.latency);
+    // db Proxy에 프록시 정보 저장
+    await Promise.all(
+      filtered.map(async (v) => {
+        return db.Proxy.create({
+          ip: v.ip,
+          type: v.type,
+          latency: v.latency,
+        });
+      })
+    );
     await page.close();
     await browser.close();
 
-    // 다시 브라우저를 킨다.
+    // 가장 빠른 latency를 가져온다.
+    const fastestProxy = await db.Proxy.findOne({
+      order: [["latency", "ASC"]],
+    });
+    // 브라우저 restart
+    console.log(`restart ip: ${fastestProxy.ip}`);
     browser = await puppeteer.launch({
       headless: false,
       args: [
         "--window-size=1920,1080",
         "--disable-notifications",
-        `--proxy-server=${filtered[0].ip}`,
+        `--proxy-server=${fastestProxy.ip}`,
         "--ignore-certificate-errors",
         "--ignore-certificate-errors-spki-list ",
       ],
     });
     page = await browser.newPage();
-    await page.goto("https://www.daum.com");
+    await page.goto("https://whatismyipaddress.com/");
+    await page.waitForTimeout(10000);
+    await page.close();
+    await browser.close();
+    await db.sequelize.close(); // db connection 닫기
   } catch (e) {
     console.error(e);
   }
