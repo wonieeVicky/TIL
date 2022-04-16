6582c6f3e9f97fb5be36661a2e94c624a2e2c5ac
Vicky <hwfongfing@gmail.com>
2022-04-16 19:26:27 +0900

    feat: add nodejs-crawler-8 test code : userDataDir를 활용한 자동로그인 구현

--- NodeJS/lecture/nodejs-crawler/1-csv-parsing-example/index.js
+++ NodeJS/lecture/nodejs-crawler/1-csv-parsing-example/index.js
@@ -1,15 +1,16 @@
 ﻿const puppeteer = require("puppeteer");
 const dotenv = require("dotenv");
 
-const db = require("./models");
+// const db = require("./models");
 dotenv.config();
 
 const crawler = async () => {
   try {
-    await db.sequelize.sync();
+    // await db.sequelize.sync();
     const browser = await puppeteer.launch({
       headless: false,
       args: ["--window-size=1920,1080", "--disable-notifications"],
+      userDataDir: "/Users/uneedcomms/Library/Application Support/Google/Chrome/Default", // login 쿠키 삽입
     });
     const page = await browser.newPage();
     await page.setViewport({
@@ -17,26 +18,20 @@ const crawler = async () => {
       height: 1080,
     });
     await page.goto("https://instagram.com");
-    await page.waitForSelector("button:not([type=submit])"); // facebook으로 로그인
-    await page.click("button:not([type=submit])");
-    await page.waitForNavigation(); // facebook 로그인으로 넘어가는 것을 기다린다.
+    // 로그인 여부 확인하는 분기처리 추가
+    if (!(await page.waitForSelector('a[href = "/woniee_j/"]'))) {
+      await page.waitForSelector("button:not([type=submit])"); // facebook으로 로그인
+      await page.click("button:not([type=submit])");
+      await page.waitForNavigation(); // facebook 로그인으로 넘어가는 것을 기다린다.
 
-    await page.waitForSelector("#email");
-    await page.type("#email", process.env.EMAIL);
-    await page.type("#pass", process.env.PASSWORD);
-    await page.waitForTimeout(1000);
-    await page.waitForSelector("button[type=submit]");
-    await page.click("button[type=submit]");
-    await page.waitForNavigation(); // instagram으로 넘어가는 것을 기다린다.
-
-    // await page.waitForResponse((response) => {
-    //   // login 시점 체크
-    //   return response.url().includes("login");
-    // });
-    // // 혹시 나타날 검은 배경화면 없어지도록 ESC 버튼 클릭
-    // await page.keyboard.press("Escape");
-
-    // 페이지 전환을 기다려줘야 한다.
+      await page.waitForSelector("#email");
+      await page.type("#email", process.env.EMAIL);
+      await page.type("#pass", process.env.PASSWORD);
+      await page.waitForTimeout(1000);
+      await page.waitForSelector("#loginbutton");
+      await page.click("#loginbutton");
+      await page.waitForNavigation(); // instagram으로 넘어가는 것을 기다린다.
+    }
   } catch (e) {
     console.error(e);
   }
