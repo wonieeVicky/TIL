8ca435abd9adc3b162289247e3a6e333618d8641
Vicky <hwfongfing@gmail.com>
2022-04-17 23:55:10 +0900

    feat: update nodejs-crawler-8 test code : 더보기 버튼 클릭 및 10개 게시글 반복 크롤링 구현

--- NodeJS/lecture/nodejs-crawler/1-csv-parsing-example/index.js
+++ NodeJS/lecture/nodejs-crawler/1-csv-parsing-example/index.js
@@ -32,23 +32,45 @@ const crawler = async () => {
       await page.click("#loginbutton");
       await page.waitForNavigation(); // instagram으로 넘어가는 것을 기다린다.
     }
+    let result = [];
+    let prevPostId = "";
+    while (result.length < 10) {
+      // 더보기 버튼이 있으면 클릭한다.
+      // prettier-ignore
+      const moreButton = await page.$('article div[data-testid="post-comment-root"] span > div[role="button"] > div');
+      if (moreButton) {
+        await page.evaluate((btn) => btn.click(), moreButton);
+      }
+      const newPost = await page.evaluate(() => {
+        const article = document.querySelector("article:first-child");
+        const postId =
+          article.querySelector("time").parentElement.parentElement &&
+          article.querySelector("time").parentElement.parentElement.href;
+        const name = article.querySelector("span a[href]").textContent;
+        const img = article.querySelector('img[class="FFVAD"]') && article.querySelector('img[class="FFVAD"]').src;
+        const content = article.querySelector('div[data-testid="post-comment-root"] > span:last-child').textContent;
 
-    const newPost = await page.evaluate(() => {
-      const article = document.querySelector("article:first-child");
-      const postId =
-        article.querySelector("time").parentElement.parentElement &&
-        article.querySelector("time").parentElement.parentElement.href;
-      const name = article.querySelector("span a[href]").textContent;
-      const img = article.querySelector('img[class="FFVAD"]') && article.querySelector('img[class="FFVAD"]').src;
-      const content = article.querySelector('div[data-testid="post-comment-root"] > span:last-child').textContent;
-      return {
-        postId,
-        name,
-        img,
-        content,
-      };
-    });
-    console.log("newPost:", newPost);
+        return {
+          postId,
+          name,
+          img,
+          content,
+        };
+      });
+      // 중복되지 않은 게시글만 추가하기
+      if (newPost.postId !== prevPostId) {
+        console.log("newPost:", newPost);
+        // result에 없는 아이들만 넣어준다.
+        if (!result.find((v) => v.postId === newPost.postId)) {
+          result.push(newPost);
+        }
+      }
+      prevPostId = newPost.postId;
+      await page.waitForTimeout(500);
+      await page.evaluate(() => {
+        window.scrollBy(0, 800); // scroll down
+      });
+    }
   } catch (e) {
     console.error(e);
   }
