f77f07a57ac78681c45e26abd637c2e063647c34
Vicky <hwfongfing@gmail.com>
2022-04-27 23:51:16 +0900

    feat: update nodejs-crawler-9(크롤링 시 waitUntil 프로퍼티로 지연 구현) test code

--- NodeJS/lecture/nodejs-crawler/1-csv-parsing-example/index.js
+++ NodeJS/lecture/nodejs-crawler/1-csv-parsing-example/index.js
@@ -20,7 +20,17 @@ const crawler = async () => {
       width: 1080,
       height: 1080,
     });
-    await page.goto("https://youtube.com");
+    await page.goto("https://youtube.com", {
+      // page 전환되자마자 이벤트 실행되지 않도록 모든 이벤트를 기다리도록 커스텀
+      // 유튜브 동영상 로딩할 때에는 networkidle0을 쓰면 안된다.
+      waitUntil: "networkidle0", // 모든 네트워크가 다 호출되었을 때 실행, domcontentloaded 돔 호출 완료 시
+    });
+
+    await page.waitForSelector("#buttons ytd-button-renderer:last-child a");
+    await page.click("#buttons ytd-button-renderer:last-child a");
+    await page.waitForNavigation({
+      waitUntil: "networkidle2", // 2개정도의 네트워크는 마무리되지 않아도 실행하겠다.
+    });
 
     // await page.close();
     // await browser.close();
