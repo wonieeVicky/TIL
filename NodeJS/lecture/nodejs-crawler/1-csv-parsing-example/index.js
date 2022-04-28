c22bb3d9a641b4c4fd01171f2a7eeab18e0615c6
Vicky <hwfongfing@gmail.com>
2022-04-28 23:29:19 +0900

    feat: update nodejs-crawler-9(userDataDir로 로그인 유지 구현) test code

--- NodeJS/lecture/nodejs-crawler/1-csv-parsing-example/index.js
+++ NodeJS/lecture/nodejs-crawler/1-csv-parsing-example/index.js
@@ -7,50 +7,47 @@ dotenv.config();
 const crawler = async () => {
   try {
     // await db.sequelize.sync();
-    // const browserFetcher = puppeteer.createBrowserFetcher(); // 브라우저 가져오기
-    // const revisionInfo = await browserFetcher.download("995683"); // 버전정보 가져오기
+    const browserFetcher = puppeteer.createBrowserFetcher(); // 브라우저 가져오기
+    const revisionInfo = await browserFetcher.download("995683"); // 버전정보 가져오기
     const browser = await puppeteer.launch({
       headless: false,
-      // executablePath: revisionInfo.executablePath, // 995683 version 브라우저가 실행됨
+      executablePath: revisionInfo.executablePath, // 995683 version 브라우저가 실행됨
       args: ["--window-size=1920,1080", "--disable-notifications"],
-      // userDataDir: "/Users/uneedcomms/Library/Application Support/Google/Chrome/Default", // login 쿠키 삽입
+      userDataDir: "/Users/uneedcomms/Library/Application Support/Google/Chrome/Default", // login 쿠키 삽입 - 첫 시도는 직접 로그인
     });
     const page = await browser.newPage();
     await page.setViewport({
       width: 1080,
       height: 1080,
     });
-    await page.goto("https://youtube.com", {
-      // page 전환되자마자 이벤트 실행되지 않도록 모든 이벤트를 기다리도록 커스텀
-      // 유튜브 동영상 로딩할 때에는 networkidle0을 쓰면 안된다.
-      waitUntil: "networkidle0", // 모든 네트워크가 다 호출되었을 때 실행, domcontentloaded 돔 호출 완료 시
-    });
+    // 네트워크 로딩 후 작업 시작
+    await page.goto("https://youtube.com", { waitUntil: "networkidle0" });
 
-    await page.waitForSelector("#buttons ytd-button-renderer:last-child a");
-    await page.click("#buttons ytd-button-renderer:last-child a");
-    await page.waitForNavigation({
-      waitUntil: "networkidle2", // 2개정도의 네트워크는 마무리되지 않아도 실행하겠다.
-    });
+    if (!(await page.$("#avatar-btn"))) {
+      // 로그인 시도
+      await page.waitForSelector("#buttons ytd-button-renderer:last-child a");
+      await page.click("#buttons ytd-button-renderer:last-child a");
+      await page.waitForNavigation({ waitUntil: "networkidle2" });
 
-    await page.waitForSelector("#identifierId");
-    await page.type("#identifierId", process.env.EMAIL); // email type
-    await page.waitForSelector("#identifierNext");
-    await page.click("#identifierNext"); // 다음 창으로 넘어가기
+      await page.waitForSelector("#identifierId");
+      await page.type("#identifierId", process.env.EMAIL); // email type
+      await page.waitForSelector("#identifierNext");
+      await page.click("#identifierNext"); // 다음 창
 
-    await page.waitForSelector('input[type="password"]');
-    // await page.type('input[type="password"]', process.env.PASSWORD); // error 발생! 아래 방법으로 우회
-    await page.evaluate((password) => {
-      document.querySelector('input[type="password"]').value = password;
-    }, process.env.PASSWORD);
+      await page.waitForSelector('input[type="password"]');
+      // await page.type('input[type="password"]', process.env.PASSWORD);
+      await page.evaluate((password) => {
+        document.querySelector('input[type="password"]').value = password;
+      }, process.env.PASSWORD);
 
-    await page.waitForTimeout(3000);
-    await page.waitForSelector("#passwordNext");
-    await page.click("#passwordNext"); // 다음 창으로 넘어가기
+      await page.waitForTimeout(3000);
+      await page.waitForSelector("#passwordNext");
+      await page.click("#passwordNext"); // 다음 창
 
-    await page.waitForNavigation({
-      waitUntil: "networkidle2", // 2개정도의 네트워크는 마무리되지 않아도 실행하겠다.
-    });
-    console.log("youtube main!");
+      await page.waitForNavigation({ waitUntil: "networkidle2" });
+    } else {
+      console.log("이미 로그인 됨");
+    }
 
     // await page.close();
     // await browser.close();
