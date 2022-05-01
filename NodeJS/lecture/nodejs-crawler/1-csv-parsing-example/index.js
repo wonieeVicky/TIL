c128a5a89c5bf0e4547f344727c9464d798b17d5
Vicky <hwfongfing@gmail.com>
2022-05-01 23:30:14 +0900

    feat: add nodejs-crawler-10 (아마존 상품 pagination 타입 크롤링) test code

--- NodeJS/lecture/nodejs-crawler/1-csv-parsing-example/index.js
+++ NodeJS/lecture/nodejs-crawler/1-csv-parsing-example/index.js
@@ -4,17 +4,38 @@ const crawler = async () => {
   try {
     const browser = await puppeteer.launch({
       headless: false,
-      args: ["--window-size=1920,1080", "--disable-notifications"],
+      args: ["--window-size=1920,1080", "--disable-notifications", "--no-sandbox"],
     });
-    const page = await browser.newPage();
-    await page.setViewport({
-      width: 1080,
-      height: 1080,
-    });
-    const keyword = "mouse";
-    await page.goto(`https://www.amazon.com/s?k=${keyword}`, {
-      waitUntil: "networkidle0",
-    });
+    let result = [];
+    await Promise.all(
+      [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(async (v) => {
+        const page = await browser.newPage();
+        await page.setViewport({
+          width: 1080,
+          height: 1080,
+        });
+        const keyword = "mouse";
+        await page.goto(`https://www.amazon.com/s?k=${keyword}&page=${v}`, {
+          waitUntil: "networkidle0",
+        });
+
+        const r = await page.evaluate(() => {
+          const tags = document.querySelectorAll(".s-result-list > div");
+          const result = [];
+          tags.forEach((t) => {
+            result.push({
+              name: t && t.querySelector("h5") && t.querySelector("h5").textContent.trim(),
+              price: t && t.querySelector(".a-offscreen") && t.querySelector(".a-offscreen").textContent,
+            });
+          });
+          return result;
+        });
+        result = result.concat(r);
+      })
+    );
+
+    console.log(result.length); // 220
+    console.log(result[0]); // { name: 'S-Button Wired USB Optical Mouse..', price: '$8.99' }
 
     // await page.close();
     // await browser.close();
