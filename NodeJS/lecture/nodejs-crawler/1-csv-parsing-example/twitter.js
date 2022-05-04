1c62855a09bb175aa2ff5ea2462862632d6033b0
Vicky <hwfongfing@gmail.com>
2022-05-04 23:46:15 +0900

    feat: add nodejs-crawler-10(iframe 및 infinite-scrolling 환경에서 태그 분석) test code

--- NodeJS/lecture/nodejs-crawler/1-csv-parsing-example/twitter.js
+++ NodeJS/lecture/nodejs-crawler/1-csv-parsing-example/twitter.js
@@ -18,6 +18,32 @@ const crawler = async () => {
     await page.type(".LoginForm-password input", process.env.PASSWORD);
     await page.waitForSelector('input[type="submit"]');
     await page.click('input[type="submit"]');
+    await page.waitForNavigation();
+
+    while (await page.$(".js-stream-item")) {
+      const firstItem = await page.$(".js-stream-item:first-child");
+      if (await page.$(".js-macaw-cards-iframe-container")) {
+        await page.evaluate(() => {
+          window.scrollBy(0, 10);
+        });
+        console.log("iframe 발견");
+        await page.waitForResponse((response) => {
+          console.log(response.url());
+          return response.url().startsWith("https://twitter.com/i/cards/tfw/v1/");
+        });
+        await page.waitForSelector(".js-stream-item:first-child iframe");
+        const iframe = await page.frames()[0];
+        const result = await iframe.evaluate(() => {
+          return {
+            title: document.querySelector("h2") && document.querySelector("h2").textContent,
+          };
+        });
+        console.log(result);
+      } else {
+        console.log("iframe 없음");
+        await page.evaluate((item) => item.parentNode.removeChild(item), firstItem);
+      }
+    }
   } catch (e) {
     console.error(e);
   }
