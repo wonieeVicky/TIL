ed05ab366c7ecbf0b13d7403e535cb8c57393a3e
Vicky <hwfongfing@gmail.com>
2022-06-15 23:26:41 +0900

    feat: update svelte-core-3 (onMount, onDestroy) test code

--- Svelte/lecture/svelte-test/src/App.svelte
+++ Svelte/lecture/svelte-test/src/App.svelte
@@ -1,30 +1,10 @@
 <script>
-	export let name;
+  import Something from "./Something.svelte";
+  let toggle = false;
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<button on:click={() => (toggle = !toggle)}>Toggle</button>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+{#if toggle}
+  <Something />
+{/if}
---
+++ Svelte/lecture/svelte-test/src/Something.svelte
@@ -0,0 +1,28 @@
+﻿<script>
+  import { onMount, onDestroy } from "svelte";
+
+  const h1 = document.querySelector("h1");
+  // console.log(h1.innerText); // null, 위 코드가 컴포넌트가 화면에 렌더링 되지 않은 상태에서 찾았기 때문에 null
+
+  // 화면에 컴포넌트 렌더링 후 실행로직을 담는 라이프 사이클
+  onMount(() => {
+    // const h1 = document.querySelector("h1");
+    // console.log(h1.innerText);
+    // console.log("Mounted!");
+
+    // onDestroy가 생기기 전에는 아래와 같이 해결할 수 있다.
+    // 비동기가 없는 상황에서만 내부 onDestroy를 사용할 수 있다.
+    return () => {
+      console.log("Destroy in mount");
+    };
+  });
+
+  // 화면에 컴포넌트 연결이 해제되기 직전에 실행되는 라이프 사이클
+  onDestroy(() => {
+    // const h1 = document.querySelector("h1");
+    // console.log(h1.innerText); // Something..., 연결이 해제되기 직전에 실행되므로 값이 담긴다.
+    console.log("Before Destroy");
+  });
+</script>
+
+<h1>Something...</h1>
