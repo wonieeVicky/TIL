6c195633983c9e4313b3be08dcfaa422773d1448
Vicky <hwfongfing@gmail.com>
2022-06-19 23:32:19 +0900

    feat: update svelte-core-3 (tick 라이프사이클 구현) test code

--- Svelte/lecture/svelte-test/src/App.svelte
+++ Svelte/lecture/svelte-test/src/App.svelte
@@ -1,11 +1,14 @@
 <script>
-  import { lifecycle, delayRender } from "./lifecycle";
-  import Something from "./Something.svelte";
-  lifecycle();
-  let done = delayRender();
+  import { tick } from "svelte";
+  let name = "world";
+
+  async function handler() {
+    name = "Vicky";
+    // tick은 어떤 데이터가 화면에 갱신될 떄까지 기다린 뒤 실행하는 역할을 한다.
+    await tick(); // Promise 객체를 반환함
+    const h1 = document.querySelector("h1");
+    console.log(h1.innerText); // Hello Vicky!
+  }
 </script>
 
-<Something />
-{#if $done}
-  <h1>Hello Lifecycle!</h1>
-{/if}
+<h1 on:click={handler}>Hello {name}!</h1>
