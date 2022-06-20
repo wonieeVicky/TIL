55bb2ac37adf176cff2341be2f9dc3e84f288fa5
Vicky <hwfongfing@gmail.com>
2022-06-21 01:34:34 +0900

    feat: add svelte-core-4 (@html, @debug) test code

--- Svelte/lecture/svelte-test/src/App.svelte
+++ Svelte/lecture/svelte-test/src/App.svelte
@@ -1,19 +1,7 @@
 <script>
-  let href = "https://github.com/wonieeVicky";
   let name = "Vicky";
-  let value = "New input value!";
-  let isUpperCase = false;
+  let index = 0;
 </script>
 
-<!-- <a href="https://github.com/wonieeVicky">Vicky</a> -->
-<a {href}>{name}</a>
-
-<!-- <input type="text" value="Default value.." /> -->
-<!-- input 단방향 -->
-<input {value} on:input={(e) => (value = e.target.value)} />
-<!-- input 양방향 -->
-<!-- <input bind:value={value} /> -->
-<input bind:value />
-
-<!-- 보간법에서는 표현식도 직접 적어넣을 수 있다. 권장되는 방법 -->
-<div>{isUpperCase ? "DIV" : "div"}</div>
+{@debug index, name}
+<h1 on:click={() => index++}>Hello {name}!</h1>
