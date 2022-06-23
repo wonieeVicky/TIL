1b9ac6d00074d2260328964a0f76cfb71c39d1f9
Vicky <hwfongfing@gmail.com>
2022-06-23 23:54:49 +0900

    feat: update svelte-core-5(assignment, 보간법 특징) test code

--- Svelte/lecture/svelte-test/src/App.svelte
+++ Svelte/lecture/svelte-test/src/App.svelte
@@ -1,7 +1,24 @@
 <script>
-  let name = "Vicky";
-  let index = 0;
+  let user = {
+    name: "Vicky",
+    depth: {
+      a: "b",
+    },
+    numbers: [1, 2],
+  };
+
+  let numbers = user.numbers;
+
+  function assign() {
+    // user.name = "Wonny";
+    // user.depth.a = "c";
+    user.numbers.push(3);
+    numbers = numbers;
+  }
 </script>
 
-{@debug index, name}
-<h1 on:click={() => index++}>Hello {name}!</h1>
+<button on:click={assign}>Assign!</button>
+<h2>user name: {user.name}</h2>
+<h2>user depth a: {user.depth.a}</h2>
+<h2>user numbers: {user.numbers}</h2>
+<h2>numbers: {numbers}</h2>
