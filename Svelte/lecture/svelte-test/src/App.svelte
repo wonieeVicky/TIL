d1149c98ca4612d71eb5bfc13a8fdb4e25645130
Vicky <hwfongfing@gmail.com>
2022-06-26 00:19:54 +0900

    feat: update svelte-core-5(반응성 구문 패턴, if, IIFE, for, switch, scope) test code

--- Svelte/lecture/svelte-test/src/App.svelte
+++ Svelte/lecture/svelte-test/src/App.svelte
@@ -1,24 +1,69 @@
 <script>
-  import { tick } from "svelte";
-
   let count = 0;
-  let double = 0;
 
+  // 선언
+  $: double = count * 2; // double 별도 선언 불필요, let double 자동 처리
+
+  // 블록
   $: {
-    double = count * 2; // count 가 반응성을 가지므로 count가 변경될 때 double의 값도 바뀌게 됨
-    console.log("double!");
+    console.log("block - count:", count);
+    console.log("block - double:", double);
   }
 
-  // $: double = count * 2;
-  async function assign() {
+  // 함수 실행
+  $: count, log(); // count가 변경되면 실행, 반드시 함수를 실행해야 한다.
+
+  // 즉시 실행 함수(IIFE)
+  $: count,
+    (() => {
+      console.log("iife: Vicky!");
+    })();
+
+  // 조건문(If)
+  $: if (count > 0) {
+    console.log("if:", double);
+  }
+
+  // 반복문(For)
+  $: for (let i = 0; i < 3; i++) {
+    count; // 반응성 데이터를 for문 내에 둔다.
+    console.log("for:", i);
+  }
+
+  // 조건문(Switch)
+  $: switch (
+    count // 반응성 데이터를 switch 변수로 둔다
+  ) {
+    case 1:
+      console.log("switch: 1");
+      break;
+    default:
+      console.log("switch: default");
+  }
+
+  // 유효범위
+  $: {
+    function scope1() {
+      console.log("scope1");
+      function scope2() {
+        console.log("scope2");
+        function scope3() {
+          console.log("scope3", count);
+          console.log("---------");
+        }
+        scope3();
+      }
+      scope2();
+    }
+    scope1();
+  }
+
+  function log() {
+    console.log("fn: Vicky!");
+  }
+  function assign() {
     count++;
-    console.time("timer");
-    await tick();
-    console.timeEnd("timer");
-    console.log(double);
   }
 </script>
 
 <button on:click={assign}>Assign!</button>
-<h2>{count}</h2>
-<h2>{double}</h2>
