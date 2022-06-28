bd9484a60486bb642baa33eb859a7661b80e44b7
Vicky <hwfongfing@gmail.com>
2022-06-28 23:27:54 +0900

    feat: update svelte-core-6(Class 지시어 바인딩 패턴) test code

--- Svelte/lecture/svelte-test/src/App.svelte
+++ Svelte/lecture/svelte-test/src/App.svelte
@@ -1,32 +1,24 @@
 <script>
-  let active = false;
-  let color = {
-    t: "tomato",
-    w: "#fff",
-  };
-  let letterSpacing = "letter-spacing: 5px;";
+  let active = true
+  let test = true
+  let valid = false
+  let camelCase = true
+
+  function multi() {
+    return 'active multiple-class'
+  }
 </script>
 
-<button on:click={() => (active = !active)}>Toggle!</button>
-<div class:active>Hello</div>
+<div class={active ? 'active' : ''}>3항 연산자 보간</div>
 
-<h2 style="background-color: {color.t}; color: {color.w}; {letterSpacing}">Vicky!</h2>
+<div class:active={test}>Class 지시어(Directive) 바인딩</div>
 
-<style>
-  div {
-    width: 120px;
-    height: 200px;
-    background-color: royalblue;
-    border-radius: 10px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    color: white;
-    font-size: 20px;
-    transition: 0.4s;
-  }
-  .hello {
-    width: 250px;
-    background-color: tomato;
-  }
-</style>
+<!-- <div class:active={active}>Class 지시어 바인딩 단축 형태</div> -->
+<div class:active>Class 지시어 바인딩 단축 형태</div>
+
+<!-- camelCase dashCase, snackCase 등은 아래처럼 각각 처리할 수 있다.-->
+<!-- valid는 false이므로 추가되지 않음 -->
+<div class:active class:valid class:camelCase class:camel-case={camelCase}>다중(Mutiple) Class 지시어 바인딩</div>
+
+<!-- <div class="active multiple-class">함수 실행</div> 로 도출된다. -->
+<div class={multi()}>함수 실행</div>
