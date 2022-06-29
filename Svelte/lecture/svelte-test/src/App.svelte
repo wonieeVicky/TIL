198819938b81f91b9693d1a67419cfc85d369019
Vicky <hwfongfing@gmail.com>
2022-06-29 20:02:44 +0900

    feat: update svelte-core-6(스타일해쉬를 통한 유효범위 설정 및 스타일 전역화) test code

--- Svelte/lecture/svelte-test/src/App.svelte
+++ Svelte/lecture/svelte-test/src/App.svelte
@@ -1,24 +1,21 @@
 <script>
-  let active = true
-  let test = true
-  let valid = false
-  let camelCase = true
-
-  function multi() {
-    return 'active multiple-class'
-  }
+  import Fruits from "./Fruits.svelte";
 </script>
 
-<div class={active ? 'active' : ''}>3항 연산자 보간</div>
+<h2>App.svelte</h2>
+<ul class="fruits">
+  <li>Apple</li>
+  <li>Banana</li>
+  <li>Cherry</li>
+</ul>
 
-<div class:active={test}>Class 지시어(Directive) 바인딩</div>
+<Fruits />
 
-<!-- <div class:active={active}>Class 지시어 바인딩 단축 형태</div> -->
-<div class:active>Class 지시어 바인딩 단축 형태</div>
-
-<!-- camelCase dashCase, snackCase 등은 아래처럼 각각 처리할 수 있다.-->
-<!-- valid는 false이므로 추가되지 않음 -->
-<div class:active class:valid class:camelCase class:camel-case={camelCase}>다중(Mutiple) Class 지시어 바인딩</div>
-
-<!-- <div class="active multiple-class">함수 실행</div> 로 도출된다. -->
-<div class={multi()}>함수 실행</div>
+<style>
+  :global(.fruits) {
+    color: red;
+  }
+  :global(.vicky) {
+    color: orange;
+  }
+</style>
---
+++ Svelte/lecture/svelte-test/src/Fruits.svelte
@@ -0,0 +1,6 @@
+﻿<h2>Fruits.svelte</h2>
+<ul class="fruits">
+  <li>Apple</li>
+  <li>Banana</li>
+  <li>Cherry</li>
+</ul>
