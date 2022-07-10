dcbe5cdfc9b4f192b6eb5dcd9b15ce35313eaef2
Vicky <hwfongfing@gmail.com>
2022-07-10 23:57:43 +0900

    feat: update svelte-core-8(반복 블록 내 고유 key 적용) test code

--- Svelte/lecture/svelte-test/src/App.svelte
+++ Svelte/lecture/svelte-test/src/App.svelte
@@ -1,46 +1,21 @@
 <script>
-  let count = 0;
+  // let fruits = ["Apple", "Banana", "Cherry", "Orange"];
+  let fruits = [
+    { id: 1, name: "Apple" },
+    { id: 2, name: "Banana" },
+    { id: 3, name: "Cherry" },
+    { id: 4, name: "Banana" },
+  ];
+
+  function deleteFirst() {
+    fruits = fruits.slice(1); // ["Banana", "Cherry", "Orange"]
+  }
 </script>
 
-<button on:click={() => (count += 1)}>증가!</button>
-<button on:click={() => (count -= 1)}>감소!</button>
+<button on:click={deleteFirst}> Delete first fruit!</button>
 
-<h2>{count}</h2>
-
-<section>
-  <h2>if</h2>
-  {#if count > 3}
-    <div>count &gt; 3</div>
-  {/if}
-</section>
-
-<section>
-  <h2>if else</h2>
-  {#if count > 3}
-    <div>count &gt; 3</div>
-  {:else}
-    <div>count &lt;= 3</div>
-  {/if}
-</section>
-
-<section>
-  <h2>if else if</h2>
-  {#if count > 3}
-    <div>count &gt; 3</div>
-  {:else if count === 3}
-    <div>count === 3</div>
-  {:else}
-    <div>count &lt;= 3</div>
-  {/if}
-</section>
-
-<section>
-  <h2>다중 블록</h2>
-  {#if count > 3}
-    {#if count === 5}
-      count === 5
-    {:else}
-      count &gt; 3
-    {/if}
-  {/if}
-</section>
+<ul>
+  {#each fruits as fruit (fruit.id)}
+    <li>{fruit.name}</li>
+  {/each}
+</ul>
