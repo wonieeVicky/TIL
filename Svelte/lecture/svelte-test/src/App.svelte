ff49d83af1ad8693c98b49a053e9d036c0a8c0d5
Vicky <hwfongfing@gmail.com>
2022-07-16 23:43:17 +0900

    feat: add svelte-core-9(javascript 비동기 패턴 정리) test code

--- Svelte/lecture/svelte-test/src/App.svelte
+++ Svelte/lecture/svelte-test/src/App.svelte
@@ -1,10 +1,39 @@
 <script>
-  import Count from "./Count.svelte";
-  let reset = false;
+  function a() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        console.log("a");
+        resolve();
+      }, 1000);
+    });
+  }
+  function b() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        console.log("b");
+        resolve();
+      }, 1000);
+    });
+  }
+  function c() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        console.log("c");
+        resolve();
+      }, 1000);
+    });
+  }
+  function d() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        console.log("d");
+        resolve();
+      }, 1000);
+    });
+  }
+
+  a()
+    .then(() => b())
+    .then(() => c())
+    .then(() => d());
 </script>
-
-{#key reset}
-  <Count />
-{/key}
-
-<button on:click={() => (reset = !reset)}>Reset!</button>
