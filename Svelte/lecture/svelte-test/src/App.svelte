2b16460db0ad7a7a533c2457b5d5bb7073282af4
Vicky <hwfongfing@gmail.com>
2022-07-17 23:39:47 +0900

    feat: add svelte-core-9(javascript 비동기 패턴 정리2 - async ~ await) test code

--- Svelte/lecture/svelte-test/src/App.svelte
+++ Svelte/lecture/svelte-test/src/App.svelte
@@ -1,39 +1,37 @@
 <script>
+  let isError = false;
   function a() {
-    return new Promise((resolve) => {
+    return new Promise((resolve, reject) => {
+      if (isError) {
+        reject(new Error("Sorry.."));
+      }
       setTimeout(() => {
         console.log("a");
-        resolve();
-      }, 1000);
-    });
-  }
-  function b() {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        console.log("b");
-        resolve();
-      }, 1000);
-    });
-  }
-  function c() {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        console.log("c");
-        resolve();
-      }, 1000);
-    });
-  }
-  function d() {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        console.log("d");
-        resolve();
+        resolve("Vicky!");
       }, 1000);
     });
   }
 
   a()
-    .then(() => b())
-    .then(() => c())
-    .then(() => d());
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((e) => {
+      console.log(e);
+    })
+    .finally(() => {
+      console.log("Done");
+    });
+
+  // async function asyncFn() {
+  //   try {
+  //     const res = await a();
+  //     console.log(res);
+  //   } catch (e) {
+  //     console.log(e.message);
+  //   } finally {
+  //     console.log("Done");
+  //   }
+  // }
+  // asyncFn();
 </script>
