9378134af93d5432c576d890b96eb971d1ca53fc
Vicky <hwfongfing@gmail.com>
2022-07-18 11:39:57 +0900

    feat: update web-performace-optimization-2 (이미지 모달 컴포넌트에 lazy loading 적용) test code

--- React/react-performance-optimization/lecture-2/src/App.js
+++ React/react-performance-optimization/lecture-2/src/App.js
@@ -1,22 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState, Suspense, lazy } from 'react'
 import styled from 'styled-components'
 import Header from './components/Header'
 import InfoTable from './components/InfoTable'
 import SurveyChart from './components/SurveyChart'
 import Footer from './components/Footer'
-import ImageModal from './components/ImageModal'
+// import ImageModal from './components/ImageModal'
+
+const LazyImageModal = lazy(() => import('./components/ImageModal'))
 
 function App() {
     const [showModal, setShowModal] = useState(false)
 
     return (
-        <div className="App">
+        <div className='App'>
             <Header />
             <InfoTable />
-            <ButtonModal onClick={() => { setShowModal(true) }}>올림픽 사진 보기</ButtonModal>
+            <ButtonModal
+                onClick={() => {
+                    setShowModal(true)
+                }}>
+                올림픽 사진 보기
+            </ButtonModal>
             <SurveyChart />
             <Footer />
-            {showModal ? <ImageModal closeModal={() => { setShowModal(false) }} /> : null}
+            <Suspense fallback={null}>
+                {showModal ? (
+                    <LazyImageModal
+                        closeModal={() => {
+                            setShowModal(false)
+                        }}
+                    />
+                ) : null}
+            </Suspense>
         </div>
     )
 }
