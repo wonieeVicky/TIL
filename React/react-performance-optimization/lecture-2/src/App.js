65f5b81eddc3f0b96341c8ba8eca3b85a467e94c
Vicky <hwfongfing@gmail.com>
2022-07-20 11:21:08 +0900

    feat: update web-performace-optimization-2 (이미지 preload) test code

--- React/react-performance-optimization/lecture-2/src/App.js
+++ React/react-performance-optimization/lecture-2/src/App.js
@@ -22,6 +22,9 @@ function App() {
 
     useEffect(() => {
         LazyImageModal.preload()
+        const img = new Image()
+        img.src =
+            'https://stillmed.olympic.org/media/Photos/2016/08/20/part-1/20-08-2016-Football-Men-01.jpg?interpolation=lanczos-none&amp;resize=*:800'
     }, [])
 
     return (
