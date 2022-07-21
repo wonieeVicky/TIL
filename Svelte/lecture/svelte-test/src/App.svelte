19e637e81184e9f495130104f52a08d3576daa7a
Vicky <hwfongfing@gmail.com>
2022-07-21 18:42:19 +0900

    feat: update svelte-core-9(영화 검색 api fetch 구현) test code & update gitignore

--- Svelte/lecture/svelte-test/src/App.svelte
+++ Svelte/lecture/svelte-test/src/App.svelte
@@ -1,37 +1,38 @@
 <script>
-  let isError = false;
-  function a() {
-    return new Promise((resolve, reject) => {
-      if (isError) {
-        reject(new Error("Sorry.."));
-      }
-      setTimeout(() => {
-        console.log("a");
-        resolve("Vicky!");
-      }, 1000);
-    });
+  import { apikey } from "./auth.ts";
+  import axios from "axios";
+  let title = "";
+  let movies = null;
+  let error = null;
+  let loading = false;
+
+  async function searchMovies() {
+    if (loading) return; // 중복 클릭 방지 용도로 조건 추가
+    movies = null;
+    error = null;
+    loading = true;
+    try {
+      const res = await axios.get(`http://www.omdbapi.com/?apikey=${apikey}&s=${title}`);
+      movies = res.data.Search;
+    } catch (err) {
+      error = err;
+    } finally {
+      loading = false;
+    }
   }
-
-  a()
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((e) => {
-      console.log(e);
-    })
-    .finally(() => {
-      console.log("Done");
-    });
-
-  // async function asyncFn() {
-  //   try {
-  //     const res = await a();
-  //     console.log(res);
-  //   } catch (e) {
-  //     console.log(e.message);
-  //   } finally {
-  //     console.log("Done");
-  //   }
-  // }
-  // asyncFn();
 </script>
+
+<input type="text" bind:value={title} />
+<button on:click={searchMovies}>검색!</button>
+
+{#if loading}
+  <p style="color: royalblue;">Loading</p>
+{:else if error}
+  <p style="color: red;">{error.message}</p>
+{:else if movies}
+  <ul>
+    {#each movies as movie}
+      <li>{movie.Title}</li>
+    {/each}
+  </ul>
+{/if}
