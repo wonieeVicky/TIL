6049a8a21a20f2fd934aacef68b6f01843e61247
Vicky <hwfongfing@gmail.com>
2022-07-24 22:18:18 +0900

    feat: update svelte-core-9(영화 api fetch 로직을 await 블록으로 리팩토링) test code

--- Svelte/lecture/svelte-test/src/App.svelte
+++ Svelte/lecture/svelte-test/src/App.svelte
@@ -1,19 +1,52 @@
 <script>
-  let promise = Promise.resolve("Hmm..");
+  import { apikey } from "./auth.ts";
+  import axios from "axios";
+  let title = "";
+  let promise = Promise.resolve([]);
 
-  function fetchName() {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => resolve("Vicky"), 0);
+  function searchMovies() {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const res = await axios.get(`http://www.omdbapi.com/?apikey=${apikey}&s=${title}`);
+        resolve(res.data.Search);
+      } catch (err) {
+        reject(err);
+      } finally {
+        console.log("Done!");
+      }
     });
   }
 </script>
 
+<input type="text" bind:value={title} />
 <button
-  on:click={async () => {
-    promise = fetchName();
-  }}>Fetch Name!</button
+  on:click={() => {
+    promise = searchMovies();
+  }}>검색!</button
 >
 
-{#await promise then name}
-  <h1>{name}</h1>
+{#await promise}
+  <p style="color: royalblue;">Loading</p>
+{:then movies}
+  <ul>
+    {#each movies as movie}
+      <li>{movie.Title}</li>
+    {/each}
+  </ul>
+{:catch error}
+  <p style="color: red;">{error.message}</p>
 {/await}
+
+<!-- 
+{#if loading}
+  <p style="color: royalblue;">Loading</p>
+{:else if error}
+  <p style="color: red;">{error.message}</p>
+{:else if movies}
+  <ul>
+    {#each movies as movie}
+      <li>{movie.Title}</li>
+    {/each}
+  </ul>
+{/if}
+  -->
