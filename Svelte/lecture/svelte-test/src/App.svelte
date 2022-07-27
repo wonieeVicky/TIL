20af0c0b633e6ebbb5cf66364dc7e27de3243d1c
Vicky <hwfongfing@gmail.com>
2022-07-27 18:04:55 +0900

    feat: add svelte-core-11(basic svelte component binding) test code

--- Svelte/lecture/svelte-test/src/App.svelte
+++ Svelte/lecture/svelte-test/src/App.svelte
@@ -1,58 +1,13 @@
 <script>
-  function clickHandler(event) {
-    // console.log(event.target);
-    console.log(event.currentTarget);
-  }
-  function wheelHandler(event) {
-    console.log(event);
-  }
+  import { onMount } from "svelte";
+
+  import Vicky from "./Vicky.svelte";
+  let vicky;
+
+  onMount(() => {
+    console.log(vicky); // Vicky{$$: {}, $$set: f, $capture_stste: f, ...}
+    console.log(vicky.title); // Error: Props cannot be read directly ... -> 스벨트는 컴포넌트 내 데이터에 접근하는 것을 기본적으로 막는다.(accessors를 사용하면 접근가능)
+  });
 </script>
 
-<section>
-  <!-- 기본 동작 방지 -->
-  <!-- el.addEventListener('click', e => e.preventDefault()) -->
-  <h2>preventDefault</h2>
-  <a href="https://naver.com" target="_blank" on:click|preventDefault={clickHandler}>Naver</a>
-</section>
-
-<section>
-  <!-- 최초 실행 후 핸들러 삭제 -->
-  <h2>Once</h2>
-  <a href="https://naver.com" target="_blank" on:click|preventDefault|once={clickHandler}>Naver</a>
-</section>
-
-<section>
-  <!-- 이벤트 버블링 방지 -->
-  <!-- el.addEventListener('click', e => e.stopPropagation()) -->
-  <h2>stopPropagation</h2>
-  <div class="parent" on:click={clickHandler}>
-    <div class="child" on:click|stopPropagation={clickHandler} />
-  </div>
-</section>
-
-<section>
-  <!-- 캡쳐링에서 핸들러 실행 -->
-  <!-- el.addEventListener('click', e => {}, true) -->
-  <!-- el.addEventListener('click', e => {}, {capture: true}) -->
-  <h2>capture</h2>
-  <div class="parent" on:click|capture={clickHandler}>
-    <div class="child" on:click={clickHandler} />
-  </div>
-</section>
-
-<section>
-  <!-- event의 target 과 currentTarget이 일치하는 경우 핸들러 실행 -->
-  <h2>self</h2>
-  <div class="parent" on:click|self={clickHandler}>
-    <div class="child" />
-  </div>
-</section>
-
-<section>
-  <!-- 이벤트 처리를 완료하지 않고도 기본 속도로 화면을 스크롤 -->
-  <!-- el.addEventListener('wheel', e => {}, {passive: true}) -->
-  <h2>passive</h2>
-  <div class="parent wheel" on:wheel|passive={wheelHandler}>
-    <div class="child" />
-  </div>
-</section>
+<Vicky title="Good!" bind:this={vicky} />
---
+++ Svelte/lecture/svelte-test/src/Vicky.svelte
@@ -0,0 +1,12 @@
+﻿<script>
+  export let title = "Hello!";
+
+  let name = "Vicky";
+  let age = 33;
+  let email = "hwfongfing@gmail.com";
+</script>
+
+<h2>{title}</h2>
+<div>{name}</div>
+<div>{age}</div>
+<div>{email}</div>
