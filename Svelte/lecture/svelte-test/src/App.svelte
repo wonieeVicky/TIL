1b128434f6f42c917fb95fe05336754df9bc0a04
Vicky <hwfongfing@gmail.com>
2022-08-01 23:51:29 +0900

    feat: update svelte-core-11(child > parent event Dispatcher 구현) test code

--- Svelte/lecture/svelte-test/src/App.svelte
+++ Svelte/lecture/svelte-test/src/App.svelte
@@ -6,8 +6,15 @@
     { id: 2, title: "Lunch", done: false },
     { id: 3, title: "Dinner", done: false },
   ];
+
+  function deleteTodo(event) {
+    const { todoId } = event.detail;
+    const index = todos.findIndex((t) => t.id === todoId);
+    todos.splice(index, 1);
+    todos = todos;
+  }
 </script>
 
-{#each todos as todo, index (todo.id)}
-  <Todo bind:todos {todo} {index} />
+{#each todos as todo (todo.id)}
+  <Todo {todo} on:deleteMe={deleteTodo} />
 {/each}
--- Svelte/lecture/svelte-test/src/Todo.svelte
+++ Svelte/lecture/svelte-test/src/Todo.svelte
@@ -1,12 +1,14 @@
 ﻿<script>
-  export let todos;
+  import { createEventDispatcher } from "svelte";
   export let todo;
-  export let index;
+
+  // createEventDispatcher는 new CustomEvent()를 통해 커스텀 이벤트를 생성.
+  // 따라서 event.detail을 사용한다.
+  const dispatch = createEventDispatcher();
 
   function deleteTodo() {
-    todos.splice(index, 1);
-    todos = todos;
-    console.log(todos);
+    // custom dispatch 함수 생성
+    dispatch("deleteMe", { todoId: todo.id });
   }
 </script>
 
