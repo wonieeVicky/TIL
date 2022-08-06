8a1ef57cff9047f26541e043202ac209e370156f
Vicky <hwfongfing@gmail.com>
2022-08-06 23:12:44 +0900

    feat: update svelte-core-11(module context을 활용한 오디오 플레이어 구현) test code

--- Svelte/lecture/svelte-test/src/App.svelte
+++ Svelte/lecture/svelte-test/src/App.svelte
@@ -1,10 +1,13 @@
 <script>
-  import Fruit, { count } from "./Fruit.svelte";
-  let fruits = ["Apple", "Banana", "Cherry", "Mango", "Orange"];
+  import AudioPlayer, { stopAll } from "./AudioPlayer.svelte";
+  let audioTracks = [
+    "https://sveltejs.github.io/assets/music/strauss.mp3",
+    "https://sveltejs.github.io/assets/music/holst.mp3",
+    "https://sveltejs.github.io/assets/music/satie.mp3",
+  ];
 </script>
 
-<button on:click={() => console.log(count)}>Total count log!</button>
-
-{#each fruits as fruit}
-  <Fruit {fruit} />
+<button on:click={stopAll}>Stop all!</button>
+{#each audioTracks as src}
+  <AudioPlayer {src} />
 {/each}
---
+++ Svelte/lecture/svelte-test/src/AudioPlayer.svelte
@@ -0,0 +1,23 @@
+﻿<script context="module">
+  const players = new Set();
+  export function stopAll() {
+    players.forEach((p) => p.pause());
+  }
+</script>
+
+<script>
+  import { onMount } from "svelte";
+  export let src;
+  let player;
+
+  onMount(() => [
+    // Like players.push(player)
+    players.add(player),
+  ]);
+</script>
+
+<div>
+  <audio {src} bind:this={player} constrols>
+    <track kind="captions" />
+  </audio>
+</div>
