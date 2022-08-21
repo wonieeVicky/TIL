7f9569ed85e0845c2a8df225e703c84cfd18cfe5
Vicky <hwfongfing@gmail.com>
2022-08-21 23:35:30 +0900

    feat: update svelte-core-15(svelte.body, svelte.head) test code

--- Svelte/lecture/svelte-test/src/App.svelte
+++ Svelte/lecture/svelte-test/src/App.svelte
@@ -1,42 +1,10 @@
 <script>
-  let key = "";
-  // readable property
-  let innerWidth; // viewport 크기
-  let innerHeight;
-  let outerWidth; // browser 크기
-  let outerHeight;
-  let online; // 현재 상태가 online 상태인지 여부
-  // writable property
-  let scrollX;
-  let scrollY;
-
-  // window.addEventListener("keydown", (event) => {
-  //   key = event.key;
-  // });
+  import Jenny from "./Jenny.svelte";
+  let toggle = false;
 </script>
 
-<svelte:window
-  on:keydown={(e) => (key = e.key)}
-  bind:innerWidth
-  bind:innerHeight
-  bind:outerWidth
-  bind:outerHeight
-  bind:online
-  bind:scrollX
-  bind:scrollY
-/>
-<h1>{key}</h1>
-<div>innerWidth: {innerWidth}</div>
-<div>innerHeight: {innerHeight}</div>
-<div>outerWidth: {outerWidth}</div>
-<div>outerHeight: {outerHeight}</div>
-<div>online: {online}</div>
-<input type="number" bind:value={scrollX} />
-<input type="number" bind:value={scrollY} />
-<div class="for-scroll" />
+<button on:click={() => (toggle = !toggle)}>Toggle!</button>
 
-<style>
-  .for-scroll {
-    height: 2000px;
-  }
-</style>
+{#if toggle}
+  <Jenny />
+{/if}
---
+++ Svelte/lecture/svelte-test/src/Jenny.svelte
@@ -0,0 +1,8 @@
+﻿<!-- document.head -->
+<svelte:head>
+  <link rel="stylesheet" href="./main.css" />
+</svelte:head>
+
+<!-- document.body -->
+<svelte:body on:mousemove={(e) => console.log(e.clientX, e.clientY)} />
+<h1>Jenny!</h1>
