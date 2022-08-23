a68553cdf2f7b003676279f63373e2d8ecff32a5
Vicky <hwfongfing@gmail.com>
2022-08-23 19:30:11 +0900

    feat: update svelte-core-15(svelte.options accessors) test code

--- Svelte/lecture/svelte-test/src/App.svelte
+++ Svelte/lecture/svelte-test/src/App.svelte
@@ -1,22 +1,14 @@
 <script>
-  import Fruit from "./Fruit.svelte";
+  import Joy from "./Joy.svelte";
 
-  let fruits = [
-    { id: 1, name: "Apple" },
-    { id: 2, name: "Banana" },
-    { id: 3, name: "Cherry" },
-    { id: 4, name: "Mango" },
-    { id: 5, name: "Orange" },
-  ];
+  let joy;
+
+  function handler() {
+    console.log(joy);
+    console.log(joy.name);
+    console.log(joy.getAge());
+  }
 </script>
 
-<button
-  on:click={() => {
-    fruits[0] = { id: 1, name: "Apple" }; // 같은 데이터지만 객체 데이터는 가변성 데이터이므로 할당되는 메모리 위치는 달라진다.
-    fruits = fruits;
-  }}>Update!</button
->
-
-{#each fruits as { id, name } (id)}
-  <Fruit {name} />
-{/each}
+<button on:click={handler}>Toggle!</button>
+<Joy bind:this={joy} />
---
+++ Svelte/lecture/svelte-test/src/Joy.svelte
@@ -0,0 +1,11 @@
+﻿<svelte:options accessors />
+
+<script>
+  let age = 33;
+  export let name = "Joy";
+  export function getAge() {
+    console.log(age);
+  }
+</script>
+
+<h1 on:click={getAge}>{name}!</h1>
