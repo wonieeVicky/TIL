b647446889851870676d7156921574a321af3908
Vicky <hwfongfing@gmail.com>
2022-08-28 22:53:18 +0900

    feat: update web-performace-optimization-3(image lazy loading 구현) test code

--- React/react-performance-optimization/lecture-3/src/components/Card.js
+++ React/react-performance-optimization/lecture-3/src/components/Card.js
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, { useEffect, useRef } from "react";
 
 function Card(props) {
-	return (
-		<div className="Card text-center">
-			<img src={props.image}/>
-			<div className="p-5 font-semibold text-gray-700 text-xl md:text-lg lg:text-xl keep-all">
-				{props.children}
-			</div>
-		</div>
-	)
+  const imgRef = useRef(null);
+
+  useEffect(() => {
+    const options = {};
+    const callback = (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          console.log("is intersecting", entry.target.dataset.src);
+          // data-set에 넣어둔 src를 실제 src에 주입 - 이미지 로드
+          entry.target.src = entry.target.dataset.src;
+          // 이미지를 로드한 후 unobserve 처리
+          observer.unobserve(imgRef.current);
+        }
+      });
+    };
+    const observer = new IntersectionObserver(callback, options);
+    observer.observe(imgRef.current);
+  }, []);
+  return (
+    <div className="Card text-center">
+      <img ref={imgRef} data-src={props.image} />
+      <div className="p-5 font-semibold text-gray-700 text-xl md:text-lg lg:text-xl keep-all">{props.children}</div>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
