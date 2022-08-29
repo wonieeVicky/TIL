b4e7db6131a033859b9dc9d70d6404b0365cd8a5
Vicky <hwfongfing@gmail.com>
2022-08-29 19:01:52 +0900

    feat: fix web-performace-optimization-3(image lazy loading 시 unobserve 매개변수에 내부 인자가 반영되도록 개선)

--- React/react-performance-optimization/lecture-3/src/components/Card.js
+++ React/react-performance-optimization/lecture-3/src/components/Card.js
@@ -1,30 +1,30 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef } from 'react'
 
 function Card(props) {
-  const imgRef = useRef(null);
+  const imgRef = useRef(null)
 
   useEffect(() => {
-    const options = {};
+    const options = {}
     const callback = (entries, observer) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          console.log("is intersecting", entry.target.dataset.src);
+          console.log('is intersecting', entry.target.dataset.src)
           // data-set에 넣어둔 src를 실제 src에 주입 - 이미지 로드
-          entry.target.src = entry.target.dataset.src;
+          entry.target.src = entry.target.dataset.src
           // 이미지를 로드한 후 unobserve 처리
-          observer.unobserve(imgRef.current);
+          observer.unobserve(entry.target)
         }
-      });
-    };
-    const observer = new IntersectionObserver(callback, options);
-    observer.observe(imgRef.current);
-  }, []);
+      })
+    }
+    const observer = new IntersectionObserver(callback, options)
+    observer.observe(imgRef.current)
+  }, [])
   return (
     <div className="Card text-center">
       <img ref={imgRef} data-src={props.image} />
       <div className="p-5 font-semibold text-gray-700 text-xl md:text-lg lg:text-xl keep-all">{props.children}</div>
     </div>
-  );
+  )
 }
 
-export default Card;
+export default Card
