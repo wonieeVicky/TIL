2e1c9db1259cf309462fe3f0fb4c39389fb6b74d
Vicky <hwfongfing@gmail.com>
2022-08-29 19:43:33 +0900

    feat: update web-performace-optimization-3(이미지 리사이즈 및 picture tag를 이용한 .webp img 조건부 렌더링 구현) test code

--- React/react-performance-optimization/lecture-3/src/components/Card.js
+++ React/react-performance-optimization/lecture-3/src/components/Card.js
@@ -8,11 +8,14 @@ function Card(props) {
     const callback = (entries, observer) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          console.log('is intersecting', entry.target.dataset.src)
+          const target = entry.target
+          const previousSibling = target.previousSibling
+          // console.log('is intersecting', entry.target.dataset.src)
           // data-set에 넣어둔 src를 실제 src에 주입 - 이미지 로드
-          entry.target.src = entry.target.dataset.src
+          target.src = target.dataset.src
+          previousSibling.srcset = previousSibling.dataset.srcset
           // 이미지를 로드한 후 unobserve 처리
-          observer.unobserve(entry.target)
+          observer.unobserve(target)
         }
       })
     }
@@ -21,7 +24,10 @@ function Card(props) {
   }, [])
   return (
     <div className="Card text-center">
-      <img ref={imgRef} data-src={props.image} />
+      <picture>
+        <source data-srcset={props.webp} type="image/webp" />
+        <img ref={imgRef} data-src={props.image} />
+      </picture>
       <div className="p-5 font-semibold text-gray-700 text-xl md:text-lg lg:text-xl keep-all">{props.children}</div>
     </div>
   )
--- React/react-performance-optimization/lecture-3/src/pages/MainPage.js
+++ React/react-performance-optimization/lecture-3/src/pages/MainPage.js
@@ -4,9 +4,12 @@ import ThreeColumns from '../components/ThreeColumns'
 import TwoColumns from '../components/TwoColumns'
 import Card from '../components/Card'
 import Meta from '../components/Meta'
-import main1 from '../assets/main1.jpg'
-import main2 from '../assets/main2.jpg'
-import main3 from '../assets/main3.jpg'
+import main1_webp from '../assets/_main1.webp'
+import main2_webp from '../assets/_main2.webp'
+import main3_webp from '../assets/_main3.webp'
+import main1 from '../assets/_main1.jpg'
+import main2 from '../assets/_main2.jpg'
+import main3 from '../assets/_main3.jpg'
 import main_items from '../assets/main-items.jpg'
 import main_parts from '../assets/main-parts.jpg'
 import main_styles from '../assets/main-styles.jpg'
@@ -41,9 +44,15 @@ function MainPage(props) {
       <div className="mx-auto">
         <ThreeColumns
           columns={[
-            <Card image={main1}>롱보드는 아주 재밌습니다.</Card>,
-            <Card image={main2}>롱보드를 타면 아주 신납니다.</Card>,
-            <Card image={main3}>롱보드는 굉장히 재밌습니다.</Card>,
+            <Card webp={main1_webp} image={main1}>
+              롱보드는 아주 재밌습니다.
+            </Card>,
+            <Card webp={main2_webp} image={main2}>
+              롱보드를 타면 아주 신납니다.
+            </Card>,
+            <Card webp={main3_webp} image={main3}>
+              롱보드는 굉장히 재밌습니다.
+            </Card>,
           ]}
         />
         <TwoColumns
