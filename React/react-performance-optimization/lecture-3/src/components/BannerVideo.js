3aa250df040caf8f45f50f9f2101f2a566c532b7
Vicky <hwfongfing@gmail.com>
2022-08-30 20:01:22 +0900

    feat: update web-performace-optimization-3(동영상 사이즈 최적화) test code

--- React/react-performance-optimization/lecture-3/src/components/BannerVideo.js
+++ React/react-performance-optimization/lecture-3/src/components/BannerVideo.js
@@ -1,23 +1,32 @@
 import React from 'react'
-import video from '../assets/banner-video.mp4'
+import video_webm from '../assets/banner-video.webm'
+import video_mp4 from '../assets/banner-video.mp4'
 
 function BannerVideo() {
-	return (
-		<div className="BannerVideo w-full h-screen overflow-hidden relative bg-texture">
-			<div className="absolute h-screen w-full left-1/2">
-				<video src={video} className="absolute translateX--1/2 h-screen max-w-none min-w-screen -z-1 bg-black min-w-full min-h-screen" autoPlay loop muted/>
-			</div>
-			<div className="w-full h-full flex justify-center items-center">
-				<div className="text-white text-center">
-					<div className="text-6xl leading-none font-semibold">KEEP</div>
-					<div className="text-6xl leading-none font-semibold">CALM</div>
-					<div className="text-3xl leading-loose">AND</div>
-					<div className="text-6xl leading-none font-semibold">RIDE</div>
-					<div className="text-5xl leading-tight font-semibold">LONGBOARD</div>
-				</div>
-			</div>
-		</div>
-	)
+  return (
+    <div className="BannerVideo w-full h-screen overflow-hidden relative bg-texture">
+      <div className="absolute h-screen w-full left-1/2">
+        <video
+          className="absolute translateX--1/2 h-screen max-w-none min-w-screen -z-1 bg-black min-w-full min-h-screen"
+          autoPlay
+          loop
+          muted
+        >
+          <source src={video_webm} type="video/webm" />
+          <source src={video_mp4} type="video/mp4" />
+        </video>
+      </div>
+      <div className="w-full h-full flex justify-center items-center">
+        <div className="text-white text-center">
+          <div className="text-6xl leading-none font-semibold">KEEP</div>
+          <div className="text-6xl leading-none font-semibold">CALM</div>
+          <div className="text-3xl leading-loose">AND</div>
+          <div className="text-6xl leading-none font-semibold">RIDE</div>
+          <div className="text-5xl leading-tight font-semibold">LONGBOARD</div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default BannerVideo
