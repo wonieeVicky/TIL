56701af55092707ae255bfea8d62a38abf21a5df
Vicky <hwfongfing@gmail.com>
2022-08-31 23:45:09 +0900

    feat: update web-performace-optimization-3(FOIT, FOUT 현상 및 fontfaceobserver를 활용한 폰트 적용 시점 컨트롤 구현) test code

--- React/react-performance-optimization/lecture-3/src/components/BannerVideo.js
+++ React/react-performance-optimization/lecture-3/src/components/BannerVideo.js
@@ -1,8 +1,19 @@
-import React from 'react'
-import video_webm from '../assets/banner-video.webm'
-import video_mp4 from '../assets/banner-video.mp4'
+import React, { useEffect, useState } from "react"
+import video_webm from "../assets/banner-video.webm"
+import video_mp4 from "../assets/banner-video.mp4"
+import FontFaceObserver from "fontfaceobserver"
 
 function BannerVideo() {
+  const [isFontLoaded, setIsFontLoaded] = useState(false)
+  const font = new FontFaceObserver("BMYEONSUNG")
+
+  useEffect(() => {
+    font.load().then(function () {
+      console.log("BMYEONSUNG has loaded")
+      setIsFontLoaded(true)
+    })
+  }, [font])
+
   return (
     <div className="BannerVideo w-full h-screen overflow-hidden relative bg-texture">
       <div className="absolute h-screen w-full left-1/2">
@@ -16,7 +27,10 @@ function BannerVideo() {
           <source src={video_mp4} type="video/mp4" />
         </video>
       </div>
-      <div className="w-full h-full flex justify-center items-center">
+      <div
+        className="w-full h-full flex justify-center items-center"
+        style={{ opacity: isFontLoaded ? 1 : 0, transition: "opacity 0.5s ease" }}
+      >
         <div className="text-white text-center">
           <div className="text-6xl leading-none font-semibold">KEEP</div>
           <div className="text-6xl leading-none font-semibold">CALM</div>
