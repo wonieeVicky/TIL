fd9c135073aff77666e9468015745248700e1248
Vicky <hwfongfing@gmail.com>
2022-09-03 23:49:06 +0900

    feat: update svelte-trello-2 (Header 컴포넌트 구현) test code

---
+++ Svelte/svelte-trello-app/src/components/Header.svelte
@@ -0,0 +1,19 @@
+﻿<header>
+  <img src="/images/trello-logo.svg" alt="Trello" class="logo" />
+</header>
+
+<style>
+  header {
+    height: 40px;
+    box-sizing: border-box;
+    background-color: rgba(0, 0, 0, 0.4);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  header img.logo {
+    width: 80px;
+    height: auto;
+    opacity: 0.5;
+  }
+</style>
