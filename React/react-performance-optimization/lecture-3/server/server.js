27b6e430630f2a79a9c20a8b2b1b3b4cf14c1300
Vicky <hwfongfing@gmail.com>
2022-09-12 23:48:27 +0900

    feat: update web-performace-optimization-3(server.js 헤더 설정에 캐시 최적화 옵션 테스트) test code

--- React/react-performance-optimization/lecture-3/server/server.js
+++ React/react-performance-optimization/lecture-3/server/server.js
@@ -1,20 +1,19 @@
-const express = require('express')
+const express = require("express")
 const app = express()
 const port = 5000
-const path = require('path')
-
+const path = require("path")
 
 const header = {
-    setHeaders: (res, path) => {
-        res.setHeader('Cache-Control', 'private, no-cache, no-store, must-revalidate')
-        res.setHeader('Expires', '-1')
-        res.setHeader('Pragma', 'no-cache')
-    },
+  setHeaders: (res, path) => {
+    res.setHeader("Cache-Control", "max-age=20")
+    res.setHeader("Expires", "-1")
+    res.setHeader("Pragma", "no-cache")
+  },
 }
 
-app.use(express.static(path.join(__dirname, '../build'), header))
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build/index.html'))
+app.use(express.static(path.join(__dirname, "../build"), header))
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build/index.html"))
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
