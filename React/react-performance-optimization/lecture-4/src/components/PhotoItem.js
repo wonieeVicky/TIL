0ed33a8a58fb558124ad10be4f0192606f150432
Vicky <hwfongfing@gmail.com>
2022-09-19 19:38:21 +0900

    feat: update web-performace-optimization-4(이미지 렌더링 Layout shift 현상 개선)

--- React/react-performance-optimization/lecture-4/src/components/PhotoItem.js
+++ React/react-performance-optimization/lecture-4/src/components/PhotoItem.js
@@ -1,27 +1,36 @@
-import React from 'react';
-import styled from 'styled-components';
-import { useDispatch } from 'react-redux';
-import { showModal } from '../redux/imageModal';
+import React from "react"
+import styled from "styled-components"
+import { useDispatch } from "react-redux"
+import { showModal } from "../redux/imageModal"
 
 function PhotoItem({ photo: { urls, alt } }) {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
   const openModal = () => {
-    dispatch(showModal({ src: urls.full, alt }));
-  };
+    dispatch(showModal({ src: urls.full, alt }))
+  }
 
   return (
     <ImageWrap>
-      <Image src={urls.small + '&t=' + new Date().getTime()} alt={alt} onClick={openModal} />
+      <Image src={urls.small + "&t=" + new Date().getTime()} alt={alt} onClick={openModal} />
     </ImageWrap>
-  );
+  )
 }
 
-const ImageWrap = styled.div``;
+const ImageWrap = styled.div`
+  width: 100%;
+  padding-bottom: 56.25%;
+  position: relative;
+`
 
 const Image = styled.img`
   cursor: pointer;
   width: 100%;
-`;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+`
 
-export default PhotoItem;
+export default PhotoItem
