b529b8480b0de461e7c91619722978b42aa9113f
Vicky <hwfongfing@gmail.com>
2022-09-29 20:08:34 +0900

    feat: update svelte-trello-2 (소스코드 내 경로별칭 적용) test code

--- Svelte/svelte-trello-app/src/App.svelte
+++ Svelte/svelte-trello-app/src/App.svelte
@@ -1,5 +1,14 @@
 <script>
-  import Header from "./components/Header.svelte"
+  import Header from "~/components/Header.svelte"
+
+  // document.body.style.backgroundImage = "url(/images/bg.jpg)"
+  // document.body.style.backgroundSize = "cover"
+
+  Object.assign(document.body.style, {
+    backgroundColor: "darkgray",
+    backgroundImage: "url(/images/bg.jpg)",
+    backgroundSize: "cover",
+  })
 </script>
 
 <Header />
