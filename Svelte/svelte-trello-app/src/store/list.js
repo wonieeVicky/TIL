1c958ec9182a895f8796785235440c233b9bcff2
Vicky <hwfongfing@gmail.com>
2022-10-01 23:51:24 +0900

    feat: update svelte-trello-2 (로컬스토리지를 활용한 createList 커스텀 스토어 구현) test code

---
+++ Svelte/svelte-trello-app/src/store/list.js
@@ -0,0 +1,21 @@
+﻿import { writable } from "svelte/store"
+
+const repoLists = JSON.parse(window.localStorage.getItem("lists")) || []
+
+const _lists = writable(repoLists) // 외부에서 사용하지 않고 내부에서만 사용하는 정보
+_lists.subscribe(($lists) => window.localStorage.setItem("lists", JSON.stringify($lists)))
+
+export const lists = {
+  subscribe: _lists.subscribe, // 실행시키지 않고 참조관계로 연결
+  add(payload) {
+    const { title } = payload
+    _lists.update(($lists) => {
+      $lists.push({
+        id: "",
+        title,
+        cards: [],
+      })
+      return $lists
+    })
+  },
+}
