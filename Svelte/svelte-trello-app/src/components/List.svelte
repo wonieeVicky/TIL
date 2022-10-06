61be11f5cf31c5964b72e6760f8ef732172a0ecd
Vicky <hwfongfing@gmail.com>
2022-10-06 19:43:42 +0900

    feat: update svelte-trello-2 (LIst 컴포넌트 구조 이해 및 레이아웃 스타일 코드 추가) test code

--- Svelte/svelte-trello-app/src/components/List.svelte
+++ Svelte/svelte-trello-app/src/components/List.svelte
@@ -1,6 +1,21 @@
-﻿<div class="list" />
+﻿<div class="list">
+  <div class="list__inner">
+    <div class="list__heading" />
+    <div class="list__cards">
+      <div class="card" />
+      <div class="card" />
+    </div>
+    <div class="create-card" />
+  </div>
+</div>
 
 <style lang="scss">
+  body {
+    height: 100vh;
+    padding: 0;
+    padding: 30px;
+    box-sizing: border-box;
+  }
   .list {
     display: inline-block;
     font-size: 16px;
@@ -10,5 +25,29 @@
     border: 10px solid yellowgreen;
     box-sizing: border-box;
     margin: 0 4px;
+    & * {
+      box-sizing: border-box;
+    }
+  }
+  .list__inner {
+    border: 10px solid blue;
+    display: flex;
+    flex-direction: column;
+    max-height: 100%;
+  }
+  .list__heading {
+    border: 10px solid green;
+  }
+  .list__cards {
+    border: 10px solid orange;
+    overflow-y: auto;
+    overflow-x: hidden;
+  }
+  .list__cards .card {
+    height: 150px;
+    border: 10px solid;
+  }
+  .create-card {
+    border: 10px solid red;
   }
 </style>
