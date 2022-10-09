da1d58bd1d19e0cf0d187c055cf8161bf8ee7f17
Vicky <hwfongfing@gmail.com>
2022-10-09 19:43:21 +0900

    feat: update svelte-trello-2 (lodash를 활용해 store 내 list.edit, list.remove 이벤트 구현) test code

--- Svelte/svelte-trello-app/src/store/list.js
+++ Svelte/svelte-trello-app/src/store/list.js
@@ -1,5 +1,7 @@
 ﻿import { writable } from "svelte/store"
 import cryptoRandomString from "crypto-random-string"
+import _find from "lodash/find"
+import _remove from "lodash/remove"
 
 const generateId = () => cryptoRandomString({ length: 10 })
 
@@ -18,6 +20,22 @@ export const lists = {
         title,
         cards: [],
       })
+      return $lists
+    })
+  },
+  edit(payload) {
+    const { listId, title } = payload
+    _lists.update(($lists) => {
+      // const foundList = $lists.find((l) => l.id === listId)
+      const foundList = _find($lists, { id: listId })
+      foundList.title = title
+      return $lists
+    })
+  },
+  remove(payload) {
+    const { listId } = payload
+    _lists.update(($lists) => {
+      _remove($lists, { id: listId })
       return $lists
     })
   },
