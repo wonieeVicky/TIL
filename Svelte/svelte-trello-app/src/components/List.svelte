9d9dad04c2d2b73bbdc19f90e52193cbca8fb077
Vicky <hwfongfing@gmail.com>
2022-10-21 19:13:21 +0900

    feat: update svelte-trello-2(Card sortablejs 적용 및 dataset을 이용한 card.id 값 전달 구현)

--- Svelte/svelte-trello-app/src/components/List.svelte
+++ Svelte/svelte-trello-app/src/components/List.svelte
@@ -1,9 +1,35 @@
 ﻿<script>
+  import Sortable from "sortablejs"
+  import { onMount } from "svelte"
+  import { cards } from "~/store/list"
   import CreateCard from "~/components/CreateCard.svelte"
   import ListTitle from "~/components/ListTitle.svelte"
   import Card from "~/components/Card.svelte"
 
   export let list
+  let cardsEl
+  let sortableCards
+
+  onMount(() => {
+    // For Cards
+    sortableCards = new Sortable(cardsEl, {
+      group: "My Cards", // 한 목록에서 다른 목록으로 요소를 끌어오려면(DnD) 두 목록의 그룹 값이 같아야 합니다.
+      handle: ".card", // 드래그 핸들이 될 요소의 선택자를 입력합니다.
+      delay: 50, // 클릭이 밀리는 것을 방지하기 위해 약간의 지연 시간을 추가합니다.
+      animation: 0, // 정렬할 때 애니메이션 속도(ms)를 지정합니다.
+      forceFallback: true, // 다양한 환경의 일관된 Drag&Drop(DnD)을 위해 HTML5 기본 DnD 동작을 무시하고 내장 기능을 사용합니다.
+      // 요소의 DnD가 종료되면 실행할 핸들러(함수)를 지정합니다.
+      onEnd(event) {
+        console.log(event) // event 객체의 정렬에 대한 다양한 정보가 들어있어요.
+        cards.reorder({
+          fromListId: event.from.dataset.listId, // 출발 list__cards.id
+          toListId: event.to.dataset.listId, // 도착 list__cards.id
+          oldIndex: event.oldIndex,
+          newIndex: event.newIndex,
+        })
+      },
+    })
+  })
 </script>
 
 <div class="list">
@@ -12,7 +38,7 @@
       <ListTitle {list} />
       <p>{list.cards.length} cards</p>
     </div>
-    <div class="list__cards">
+    <div class="list__cards" bind:this={cardsEl} data-list-id={list.id}>
       {#each list.cards as card (card.id)}
         <Card listId={list.id} {card} />
       {/each}
