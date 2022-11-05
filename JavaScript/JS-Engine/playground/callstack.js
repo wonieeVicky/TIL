10edf7036407548f10a4903260b677e46a6a453a
Vicky <hwfongfing@gmail.com>
2022-11-05 23:58:23 +0900

    feat: add js-engine-1(함수 호출 스택 분석) test code

---
+++ JavaScript/JS-Engine/playground/callstack.js
@@ -0,0 +1,20 @@
+﻿const x = "x";
+function c() {
+  const y = "y";
+  console.log("c");
+  debugger;
+}
+
+function a() {
+  const x = "x";
+  console.log("a");
+  function b() {
+    const z = "z";
+    console.log("b");
+    c();
+  }
+  b();
+}
+
+a();
+c();
