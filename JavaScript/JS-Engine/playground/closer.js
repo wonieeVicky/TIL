c5cc564ce2bc25558f8aaae997b45683430a97df
Vicky <hwfongfing@gmail.com>
2022-11-19 00:07:38 +0900

    feat: update js-engine-2 test code(클로저를 활용한 순차적 데이터 확인)

--- JavaScript/JS-Engine/playground/closer.js
+++ JavaScript/JS-Engine/playground/closer.js
@@ -1,5 +1,5 @@
 ﻿function a() {
-  for (let i = 0; i < 5; i++) {
+  for (var i = 0; i < 5; i++) {
     setTimeout(() => {
       console.log(i);
     }, i * 1000);
@@ -7,3 +7,40 @@
 }
 
 a();
+// 5
+// 5
+// 5
+// 5
+// 5
+
+function b() {
+  for (var i = 0; i < 5; i++) {
+    (function (j) {
+      setTimeout(() => {
+        console.log(j);
+      }, i * 1000);
+    })(i);
+  }
+}
+
+b();
+// 0
+// 1
+// 2
+// 3
+// 4
+
+function c() {
+  for (let i = 0; i < 5; i++) {
+    setTimeout(() => {
+      console.log(i);
+    }, i * 1000);
+  }
+}
+
+c();
+// 0
+// 1
+// 2
+// 3
+// 4
