aef8ec16314683c724b6860983c584a6152e6345
Vicky <hwfongfing@gmail.com>
2022-11-27 23:27:43 +0900

    chore: add basic-python-2 (python data type 실습 코드 추가)

---
+++ Python/playground/python_module_test/my_first_program.py
@@ -0,0 +1,10 @@
+
+def wow_sum(x, y):
+    return x + y
+
+
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    c = wow_sum(a, b)
+    print(c, "이 출력이 되었습니다")
---
+++ Python/playground/python_module_test/my_second_program.py
@@ -0,0 +1,9 @@
+from my_first_program import wow_sum
+
+
+if __name__ == "__main__":
+    c = 2
+    d = 3
+
+    result = wow_sum(c, d)
+    print(result)
