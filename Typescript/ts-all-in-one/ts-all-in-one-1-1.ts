24caa8dbf3144dec955cb4f7abe92959d3efb68c
wonieeVicky <hwfongfing@gmail.com>
2022-12-07 22:31:55 +0900

    feat: add ts-all-in-one-1-2 test code + fix tsconfig target property(es5 > ESNext)

---
+++ Typescript/ts-all-in-one/ts-all-in-one-1-1.ts
@@ -0,0 +1,27 @@
+﻿const a: string = "5";
+const b: number = 5;
+const c: boolean = true;
+const d: undefined = undefined;
+const e: null = null;
+const f: symbol = Symbol.for("abc");
+const g: bigint = 1000000n;
+const h: any = "any";
+const i: any = true;
+
+/* function add(a: number, b: number): number {
+  return a + b;
+} */
+
+// type Add = (x: number, y: number) => number;
+interface Add {
+  (x: number, y: number): number;
+}
+const add: Add = (a, b) => a + b;
+
+const obj: { x: number; y: number } = { x: 1, y: 2 };
+
+const arr: string[] = ["a", "b", "c"];
+const arr2: Array<number> = [123, 456, 789];
+
+// 길이가 고정되었을 때 튜플로 정의
+const arr3: [number, number, string] = [1, 2, "3"];
