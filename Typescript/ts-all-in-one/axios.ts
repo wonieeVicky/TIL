e246466d24753d1962fb867ae93b6cc92375643b
wonieeVicky <hwfongfing@gmail.com>
2023-01-09 11:39:54 +0900

    feat: add ts-all-in-one-6 test code - jquery install, add basic axios code

---
+++ Typescript/ts-all-in-one/axios.ts
@@ -0,0 +1,13 @@
+﻿import axios from "./node_modules/axios/index";
+
+(async () => {
+  try {
+    await axios.get("https://jsonplaceholder.typicode.com/post/1");
+  } catch (err) {}
+})();
+
+const a = () => {};
+// 함수에 속성을 추가
+a.b = "c";
+a.e = "f";
+a.z = "123";
