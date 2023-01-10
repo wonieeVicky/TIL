37c9772e13ba3e674b22789ce61de834699dfd1b
wonieeVicky <hwfongfing@gmail.com>
2023-01-10 23:53:47 +0900

    fix: ts-all-in-one-6 tsconfig property & use ts-node for axios response

---
+++ Typescript/ts-all-in-one/axios.ts
@@ -0,0 +1,9 @@
+﻿import axios from "axios";
+console.log(11);
+(async () => {
+  try {
+    console.log(123);
+    const response = await axios.get("https://jsonplaceholder.typicode.com/posts/1");
+    console.log(response);
+  } catch (err) {}
+})();
