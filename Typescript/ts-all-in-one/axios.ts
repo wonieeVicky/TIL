839f73bf8b56b149026023ce841459ba3106601f
wonieeVicky <hwfongfing@gmail.com>
2023-01-11 11:39:03 +0900

    feat: add ts-all-in-one-6 test code - add typing code (axios.get, axios.post)

--- Typescript/ts-all-in-one/axios.ts
+++ Typescript/ts-all-in-one/axios.ts
@@ -1,9 +1,38 @@
-﻿import axios from "axios";
-console.log(11);
+﻿import axios, { AxiosResponse } from "axios";
+
+// interface Post {
+//   userId: number;
+//   id: number;
+//   title: string;
+//   body: string;
+// }
+type Post = { userId: number; id: number; title: string; body: string };
+interface Created {}
+interface Data {
+  title: string;
+  body: string;
+  userId: number;
+}
+
 (async () => {
   try {
-    console.log(123);
-    const response = await axios.get("https://jsonplaceholder.typicode.com/posts/1");
-    console.log(response);
+    const response = await axios.get<Post, AxiosResponse<Post>>("https://jsonplaceholder.typicode.com/posts/1");
+
+    // post<T = any, R = AxiosResponse<T>, D = any>(url: string, data?: D, config?: AxiosRequestConfig<D>): Promise<R>;
+    await axios.post<Created, AxiosResponse<Created>, Data>("https://jsonplaceholder.typicode.com/posts", {
+      title: "foo",
+      body: "bar",
+      userId: 1,
+    });
+
+    await axios<Created, AxiosResponse<Created>, Data>({
+      method: "post",
+      url: "https://jsonplaceholder.typicode.com/posts",
+      data: {
+        title: "foo",
+        body: "bar",
+        userId: 1,
+      },
+    });
   } catch (err) {}
 })();
