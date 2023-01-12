12421723b2823fb1ce3b80aa48de2d4ad09fdb40
wonieeVicky <hwfongfing@gmail.com>
2023-01-12 23:53:21 +0900

    feat: add ts-all-in-one-6 test code - axios typing(interface a)

--- Typescript/ts-all-in-one/axios.ts
+++ Typescript/ts-all-in-one/axios.ts
@@ -1,12 +1,12 @@
 ﻿import axios, { AxiosError, AxiosResponse } from "axios";
 
-// interface Post {
-//   userId: number;
-//   id: number;
-//   title: string;
-//   body: string;
-// }
-type Post = { userId: number; id: number; title: string; body: string };
+interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
 interface Created {}
 interface Data {
   title: string;
@@ -14,18 +14,35 @@ interface Data {
   userId: number;
 }
 
+interface Config<D = any> {
+  method?: "get" | "post" | "put" | "delete" | "head" | "options" | "patch";
+  url?: string;
+  data?: D;
+}
+interface A {
+  // 한번 선택값이 나오면 뒤에는 모두 선택값이 나와야한다.
+  // 즉, R = AxiosResponse<T>로 선택값이므로, D = any로 선택값 처리
+  get: <T = any, R = AxiosResponse<T>>(url: string) => Promise<R>;
+  post: <T = any, R = AxiosResponse<T>, D = any>(url: string, data: D) => Promise<R>;
+  <T, R = AxiosResponse<T>, D = any>(config: Config): Promise<R>;
+  isAxiosError: (err: unknown) => err is AxiosError;
+  (url: string, config: Config): void;
+}
+
+const a: A = axios;
+
 (async () => {
   try {
-    const response = await axios.get<Post, AxiosResponse<Post>>("https://jsonplaceholder.typicode.com/posts/1");
-
-    // post<T = any, R = AxiosResponse<T>, D = any>(url: string, data?: D, config?: AxiosRequestConfig<D>): Promise<R>;
-    await axios.post<Created, AxiosResponse<Created>, Data>("https://jsonplaceholder.typicode.com/posts", {
+    // get
+    await a.get<Post, AxiosResponse<Post>>("https://jsonplaceholder.typicode.com/posts/1");
+    // post
+    await a.post<Created, AxiosResponse<Created>, Data>("https://jsonplaceholder.typicode.com/posts", {
       title: "foo",
       body: "bar",
       userId: 1,
     });
-
-    await axios<Created, AxiosResponse<Created>, Data>({
+    // another case post
+    await a<Created, AxiosResponse<Created>, Data>({
       method: "post",
       url: "https://jsonplaceholder.typicode.com/posts",
       data: {
@@ -35,15 +52,8 @@ interface Data {
       },
     });
   } catch (err) {
-    if (axios.isAxiosError(err)) {
-      // { data: { message: 'Server Error' } }
-      err.response?.data.message;
-      // 만약 예상치 못한 데이터로 인해 타입 에러가 발생하면 아래와 같이 처리한다.
-      (err.response as AxiosResponse<{ result: string }>)?.data.result;
+    if (a.isAxiosError(err)) {
+      console.log((err.response as AxiosResponse<{ result: string }>)?.data.result);
     }
-    // console.error(err.response.data); // Error, 'err'은(는) 'unknown' 형식입니다.
-    // axios 가 아닌 다른 문법 에러가 발생할 수도 있음. 따라서 unknown 타입으로 지정해줌.
-    // const errorResponse = (err as AxiosError).response;
-    // console.log(errorResponse?.data); // Ok
   }
 })();
--- Typescript/ts-all-in-one/jquery.ts
+++ Typescript/ts-all-in-one/jquery.ts
@@ -1,40 +1,40 @@
-﻿const element = $("p");
+﻿// const element = $("p");
 
-// removeClass(className_function?: JQuery.TypeOrArray<string> | ((this: TElement, index: number, className: string) => string)): this;
-$("p").removeClass("myClass noClass").addClass("yourClass");
-$("p").removeClass((index: number, className: string) => "my Class noClass");
+// // removeClass(className_function?: JQuery.TypeOrArray<string> | ((this: TElement, index: number, className: string) => string)): this;
+// $("p").removeClass("myClass noClass").addClass("yourClass");
+// $("p").removeClass((index: number, className: string) => "my Class noClass");
 
-$(["p", "t"]).text("hello");
-$(["p", "t"]).text(function () {
-  console.log(this);
-  return true; // ok
-});
+// $(["p", "t"]).text("hello");
+// $(["p", "t"]).text(function () {
+//   console.log(this);
+//   return true; // ok
+// });
 
-const tag2 = $("ul li").addClass(function (index) {
-  return "item-" + index;
-});
+// const tag2 = $("ul li").addClass(function (index) {
+//   return "item-" + index;
+// });
 
-$(tag2).html(function (i: number) {
-  console.log(this);
-  return $(this).data("name") + "입니다";
-});
+// $(tag2).html(function (i: number) {
+//   console.log(this);
+//   return $(this).data("name") + "입니다";
+// });
 
-interface vQuery<T> {
-  text(param?: string | number | ((this: T, index: number) => string | number | boolean)): this;
-  html(param: string | Document | DocumentFragment): void;
-}
+// interface vQuery<T> {
+//   text(param?: string | number | ((this: T, index: number) => string | number | boolean)): this;
+//   html(param: string | Document | DocumentFragment): void;
+// }
 
-const $tag: vQuery<HTMLElement> = $(["p", "t"]) as unknown as vQuery<HTMLElement>;
-$tag.text("123");
-$tag.text(123);
-$tag.text().html(document);
-$tag.text(function (index) {
-  console.log(this, index);
-  return true; // ok
-});
+// const $tag: vQuery<HTMLElement> = $(["p", "t"]) as unknown as vQuery<HTMLElement>;
+// $tag.text("123");
+// $tag.text(123);
+// $tag.text().html(document);
+// $tag.text(function (index) {
+//   console.log(this, index);
+//   return true; // ok
+// });
 
-const tag = $("ul li").addClass(function (index) {
-  return "item-" + index;
-});
+// const tag = $("ul li").addClass(function (index) {
+//   return "item-" + index;
+// });
 
-$(tag).html(document);
+// $(tag).html(document);
