02f78b52df8b686ad35868ea1f883bf417dcc815
wonieeVicky <hwfongfing@gmail.com>
2023-01-25 23:45:06 +0900

    feat: add ts-all-in-one-9 test code - 기본 node server 코드 구현

---
+++ Typescript/ts-all-in-one/node.ts
@@ -0,0 +1,14 @@
+﻿import fs from "fs";
+import http from "http";
+import path from "path";
+
+http
+  .createServer((req, res) => {
+    fs.readFile(path.join(__dirname, "index.html"), (err, data) => {
+      res.writeHead(200);
+      res.end(data);
+    });
+  })
+  .listen(8080, () => {
+    console.log("server started");
+  });
