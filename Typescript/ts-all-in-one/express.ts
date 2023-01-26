24cea690a40214705f3e48407f31e927bce11271
wonieeVicky <hwfongfing@gmail.com>
2023-01-26 23:28:04 +0900

    feat: update ts-all-in-one-9 test code - @types/express 기본 구조 분석

--- Typescript/ts-all-in-one/express.ts
+++ Typescript/ts-all-in-one/express.ts
@@ -1,1 +1,11 @@
-﻿
+﻿import express from "express";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/", express.static("./public"));
+
+app.get("/", (req, res) => {});
+
+app.listen(8080, () => {});
