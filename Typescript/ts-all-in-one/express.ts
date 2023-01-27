92dea2c116fc305a075a581d80cb02ef8431d220
wonieeVicky <hwfongfing@gmail.com>
2023-01-27 23:36:56 +0900

    feat: update ts-all-in-one-9 test code - express middleware typing(declare global)

--- Typescript/ts-all-in-one/express.ts
+++ Typescript/ts-all-in-one/express.ts
@@ -1,4 +1,4 @@
-﻿import express from "express";
+﻿import express, { Request, Response, NextFunction } from "express";
 
 const app = express();
 
@@ -6,6 +6,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/", express.static("./public"));
 
+interface ResponseTest extends Response {
+  vicky: string;
+}
+
+const middleware = (req: Request, res: ResponseTest, next: NextFunction) => {
+  res.vicky;
+};
+
 app.get("/", (req, res) => {});
+
+app.use((err, req, res, next) => {
+  console.log(err.status);
+});
 
 app.listen(8080, () => {});
