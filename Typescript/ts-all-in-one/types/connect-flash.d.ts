fdd308414ce9acbc65429636a9b15cc0b6fcb731
wonieeVicky <hwfongfing@gmail.com>
2023-01-30 22:38:49 +0900

    feat: update ts-all-in-one-9 (타입 미존재 라이브러리에 대한 d.ts 생성 및 타이핑 구현 - connect-flash)

---
+++ Typescript/ts-all-in-one/types/connect-flash.d.ts
@@ -0,0 +1,15 @@
+﻿declare module "connect-flash" {
+  global {
+    namespace Express {
+      interface Request {
+        // 타입 확장
+        flash(message: string): void;
+        flash(event: string, message: string): void;
+        flash(): { [key: string]: string[] };
+      }
+    }
+  }
+  import { RequestHandler } from "express";
+  function flash(): RequestHandler;
+  export default flash;
+}
