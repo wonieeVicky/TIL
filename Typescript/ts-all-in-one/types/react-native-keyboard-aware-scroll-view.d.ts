62262504b74a2378b841311b7a83f8b67c1443ab
wonieeVicky <hwfongfing@gmail.com>
2023-01-30 22:19:40 +0900

    feat: update ts-all-in-one-9 test code - 타입 미존재 라이브러리에 대한 d.ts 타이핑 구현

---
+++ Typescript/ts-all-in-one/types/react-native-keyboard-aware-scroll-view.d.ts
@@ -0,0 +1,8 @@
+﻿declare module "react-native-keyboard-aware-scroll-view" {
+  import { Component } from "react";
+
+  class KeyboardAwareScrollViewComponent extends Component<ViewProps & { vicky: string }> {}
+  class keyboardAwareScrollView extends KeyboardAwareScrollViewComponent {}
+
+  export { KeyboardAwareScrollView };
+}
