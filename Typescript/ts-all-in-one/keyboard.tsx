5500c24ec647df90df27834d85ccc9bb15d49a6b
wonieeVicky <hwfongfing@gmail.com>
2023-01-30 22:19:08 +0900

    feat: update ts-all-in-one-9 test code - install library & write test code

---
+++ Typescript/ts-all-in-one/keyboard.tsx
@@ -0,0 +1,13 @@
+﻿import React, { FC, ReactNode } from "react";
+import { Keyboard, StyleSheetProperties, TouchableWithoutFeedback } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+
+const DismissKeyboardView: FC<{ children: ReactNode; style: StyleSheetProperties }> = ({ children, ...props }) => (
+  <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+    <KeyboardAwareScrollView {...props} style={props.style} vicky={true}>
+      {children}
+    </KeyboardAwareScrollView>
+  </TouchableWithoutFeedback>
+);
+
+export default DismissKeyboardView;
