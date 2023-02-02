bf625ff83029a4148d3942a1d39b99293ce2d95d
wonieeVicky <hwfongfing@gmail.com>
2023-02-02 23:51:28 +0900

    feat: update three-js-1-1 test code - main.css를 CopyWebpackPlugin 및 index.html 연결 및 기본 스타일 반영

--- GraphicLayout/threeJs/lecture/start/03_webpack/webpack.config.js
+++ GraphicLayout/threeJs/lecture/start/03_webpack/webpack.config.js
@@ -70,13 +70,13 @@ module.exports = {
     // patterns에 설정한 경로에 해당 파일이 없으면 에러가 발생합니다.
     // 사용하는 파일이나 폴더 이름이 다르다면 변경해주세요.
     // 그대로 사용할 파일들이 없다면 CopyWebpackPlugin을 통째로 주석 처리 해주세요.
-    // new CopyWebpackPlugin({
-    // 	patterns: [
-    // 		{ from: "./src/main.css", to: "./main.css" },
-    // 		// { from: "./src/images", to: "./images" },
-    // 		// { from: "./src/models", to: "./models" },
-    // 		// { from: "./src/sounds", to: "./sounds" }
-    // 	],
-    // })
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "./src/main.css", to: "./main.css" },
+        // { from: "./src/images", to: "./images" },
+        // { from: "./src/models", to: "./models" },
+        // { from: "./src/sounds", to: "./sounds" }
+      ],
+    }),
   ],
 };
