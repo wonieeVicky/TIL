05f312281c9f5bfd9272f8f056c78cb0ef5cf621
wonieeVicky <hwfongfing@gmail.com>
2023-02-04 00:03:53 +0900

    feat: add three-js-1-2 test code - 기본 장면 renderer 만들기 구현(동적 주입 or html 엘리먼트 사용)

--- GraphicLayout/threeJs/lecture/basic/src/main.js
+++ GraphicLayout/threeJs/lecture/basic/src/main.js
@@ -1,1 +1,10 @@
-﻿
+﻿import * as THREE from "three";
+
+// 동적으로 캔버스 조립하기
+// const renderer = new THREE.WebGLRenderer();
+// renderer.setSize(window.innerWidth, window.innerHeight);
+// document.body.appendChild(renderer.domElement); // renderer.domElement는 캔버스를 의미
+
+const canvas = document.querySelector("#three-canvas");
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(window.innerWidth, window.innerHeight);
