2a02356fe6a26697dd3c34dd079a770f22965f50
wonieeVicky <hwfongfing@gmail.com>
2023-02-05 17:56:23 +0900

    feat: update three-js-1-2 test code - 기본 장면 만들기 (geometry, material 설정, mesh 생성, renderer 연결)

--- GraphicLayout/threeJs/lecture/basic/src/main.js
+++ GraphicLayout/threeJs/lecture/basic/src/main.js
@@ -7,14 +7,35 @@
 
 // html에서 캔버스 가져와서 사용하기
 const canvas = document.querySelector("#three-canvas");
-const renderer = new THREE.WebGLRenderer({ canvas });
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+  antialias: true,
+});
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 // scene 생성
 const scene = new THREE.Scene();
 // camera 생성 - PerspectiveCamera(시야각fov, 종횡비aspect, near, far)
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-// 카메라를 뒤로..
+
+// camera 위치 설정
+camera.position.x = 1;
+camera.position.y = 2;
 camera.position.z = 5;
 // 무대에 올리기
 scene.add(camera);
+
+// Mesh 설정 - Geometry + Material
+const geometry = new THREE.BoxGeometry(1, 1, 1); // 1*1*1 정육면체 생성
+const material = new THREE.MeshBasicMaterial({
+  // color: 0x00ff00,
+  // color: "#00ff00",
+  color: "green",
+}); // 재질에 대한 여러 속성을 객체에 설정함. 녹색
+
+// Mesh 생성
+const mesh = new THREE.Mesh(geometry, material);
+// 무대에 올리기
+scene.add(mesh);
+// 그리기
+renderer.render(scene, camera);
