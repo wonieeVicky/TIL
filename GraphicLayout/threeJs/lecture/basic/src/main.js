b7bcdc66606e91e311c10488769953e6b86bf535
wonieeVicky <hwfongfing@gmail.com>
2023-02-05 17:13:35 +0900

    feat: add three-js-1-2 test code - scene 생성 및 PerspectiveCamera 연결

--- GraphicLayout/threeJs/lecture/basic/src/main.js
+++ GraphicLayout/threeJs/lecture/basic/src/main.js
@@ -5,6 +5,16 @@
 // renderer.setSize(window.innerWidth, window.innerHeight);
 // document.body.appendChild(renderer.domElement); // renderer.domElement는 캔버스를 의미
 
+// html에서 캔버스 가져와서 사용하기
 const canvas = document.querySelector("#three-canvas");
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
+
+// scene 생성
+const scene = new THREE.Scene();
+// camera 생성 - PerspectiveCamera(시야각fov, 종횡비aspect, near, far)
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+// 카메라를 뒤로..
+camera.position.z = 5;
+// 무대에 올리기
+scene.add(camera);
