64d4acc9a5100a14f462363adb6990349273d49a
wonieeVicky <hwfongfing@gmail.com>
2023-02-08 22:16:59 +0900

    feat: update three-js-1-2 test code - requestAnimationFrame, setAnimationLoop를 이용한 기본 애니메이션 구현

---
+++ GraphicLayout/threeJs/lecture/basic/src/ex05.js
@@ -0,0 +1,57 @@
+﻿import * as THREE from "three";
+
+// --- 주제: 애니메이션 기본
+
+export default function example() {
+  // renderer
+  const canvas = document.querySelector("#three-canvas");
+  const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+
+  // scene
+  const scene = new THREE.Scene();
+  // camera
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.z = 5;
+  scene.add(camera);
+
+  const light = new THREE.DirectionalLight(0xffffff, 1);
+  light.position.x = 1;
+  light.position.z = 2;
+  scene.add(light);
+
+  // Mesh
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshStandardMaterial({ color: "red" });
+  const mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  // 그리기
+  function draw() {
+    // mesh.rotation.y += 0.1;
+    mesh.rotation.y += THREE.MathUtils.degToRad(1); // three.js 내장 기능 - 각도를 라디안으로 변환
+    mesh.position.y += 0.05;
+    if (mesh.position.y > 3) {
+      mesh.position.y = 0;
+    }
+    renderer.render(scene, camera);
+    // window.requestAnimationFrame(draw); // requestAnimationFrame(draw);
+    renderer.setAnimationLoop(draw);
+  }
+
+  // resize 시 변화하는 것들을 새로 설정해준다.
+  function setSize() {
+    // camera
+    camera.aspect = window.innerWidth / window.innerHeight;
+    // updateProjectionMatrix 카메라 투영에 관련된 값에 변화가 있을 때는 실행해야 함
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  }
+
+  // Event
+  window.addEventListener("resize", setSize);
+
+  draw();
+}
--- GraphicLayout/threeJs/lecture/basic/src/main.js
+++ GraphicLayout/threeJs/lecture/basic/src/main.js
@@ -1,6 +1,7 @@
 ﻿// import example from "./ex01";
 // import example from "./ex02";
 // import example from "./ex03";
-import example from "./ex04";
+// import example from "./ex04";
+import example from "./ex05";
 
 example();
