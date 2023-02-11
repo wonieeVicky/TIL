04917ea9545686fc387c59947b91fb7964a38df6
wonieeVicky <hwfongfing@gmail.com>
2023-02-11 23:38:09 +0900

    feat: update three-js-1-2 test code - three.js fog 효과 구현

---
+++ GraphicLayout/threeJs/lecture/basic/src/ex07.js
@@ -0,0 +1,73 @@
+﻿import * as THREE from "three";
+
+// --- 주제: 애니메이션 기본
+
+export default function example() {
+  // renderer
+  const canvas = document.querySelector("#three-canvas");
+  const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+
+  // scene
+  const scene = new THREE.Scene();
+  // Fog
+  scene.fog = new THREE.Fog("black", 3, 8); // fog(color, near, far)
+
+  // camera
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.y = 1;
+  camera.position.z = 5;
+  scene.add(camera);
+
+  const light = new THREE.DirectionalLight(0xffffff, 1);
+  light.position.x = 1;
+  light.position.y = 3;
+  light.position.z = 5;
+  scene.add(light);
+
+  // Mesh
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshStandardMaterial({ color: "red" });
+
+  const meshes = [];
+  let mesh;
+  for (let i = 0; i < 10; i++) {
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.x = Math.random() * 5 - 2.5;
+    mesh.position.z = Math.random() * 5 - 2.5;
+    scene.add(mesh);
+    meshes.push(mesh);
+  }
+
+  let oldTime = Date.now();
+  // 그리기
+  function draw() {
+    const newTime = Date.now();
+    const deltaTime = newTime - oldTime;
+    oldTime = newTime;
+
+    meshes.forEach((mesh) => {
+      mesh.rotation.y += deltaTime * 0.001;
+    });
+
+    renderer.render(scene, camera);
+    // window.requestAnimationFrame(draw);
+    renderer.setAnimationLoop(draw);
+  }
+
+  // resize 시 변화하는 것들을 새로 설정해준다.
+  function setSize() {
+    // camera
+    camera.aspect = window.innerWidth / window.innerHeight;
+    // updateProjectionMatrix 카메라 투영에 관련된 값에 변화가 있을 때는 실행해야 함
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  }
+
+  // Event
+  window.addEventListener("resize", setSize);
+
+  draw();
+}
