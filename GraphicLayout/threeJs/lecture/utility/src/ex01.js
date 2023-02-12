0e24504c7926a2d850cc57dc344277b30f30ef07
wonieeVicky <hwfongfing@gmail.com>
2023-02-12 17:35:29 +0900

    feat: add three-js-1-3 test code - 축 헬퍼AxesHelper, 그리드 헬퍼GridHelper 구현

---
+++ GraphicLayout/threeJs/lecture/utility/src/ex01.js
@@ -0,0 +1,75 @@
+import * as THREE from "three";
+
+// ----- 주제: AxesHelper(축), GridHelper(그리드)
+
+export default function example() {
+  // Renderer
+  const canvas = document.querySelector("#three-canvas");
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true,
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+
+  // Scene
+  const scene = new THREE.Scene();
+
+  // Camera
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.x = 1;
+  camera.position.y = 5;
+  camera.position.z = 0;
+  scene.add(camera);
+
+  // 은은한 조명 추가
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+  scene.add(ambientLight);
+
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+  directionalLight.position.x = 1;
+  directionalLight.position.z = 2;
+  scene.add(directionalLight);
+
+  // AxesHelper
+  const axesHelper = new THREE.AxesHelper(3);
+  scene.add(axesHelper);
+
+  const gridHelper = new THREE.GridHelper(5);
+  scene.add(gridHelper);
+
+  // Mesh
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshStandardMaterial({
+    color: "seagreen",
+  });
+  const mesh = new THREE.Mesh(geometry, material);
+  mesh.position.x = 2;
+  scene.add(mesh);
+
+  camera.lookAt(mesh.position);
+
+  // 그리기
+  const clock = new THREE.Clock();
+
+  function draw() {
+    const time = clock.getElapsedTime();
+
+    mesh.rotation.y = time;
+
+    renderer.render(scene, camera);
+    renderer.setAnimationLoop(draw);
+  }
+
+  function setSize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  }
+
+  // 이벤트
+  window.addEventListener("resize", setSize);
+
+  draw();
+}
