3536f1477f1a04883b0b06cd4f685e29c38b2fa5
wonieeVicky <hwfongfing@gmail.com>
2023-02-23 19:44:43 +0900

    feat: update three-js-1-6 test code - TrackballControls 구현 및 부가 옵션 상세

---
+++ GraphicLayout/threeJs/lecture/controls/src/ex02.js
@@ -0,0 +1,80 @@
+import * as THREE from "three";
+import { TrackballControls } from "three/examples/jsm/controls/TrackballControls";
+
+// ----- 주제: TrackballControls
+
+export default function example() {
+  // Renderer
+  const canvas = document.querySelector("#three-canvas");
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true,
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+
+  // Scene
+  const scene = new THREE.Scene();
+
+  // Camera
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.y = 1.5;
+  camera.position.z = 4;
+  scene.add(camera);
+
+  // Light
+  const ambientLight = new THREE.AmbientLight("white", 0.5);
+  scene.add(ambientLight);
+
+  const directionalLight = new THREE.DirectionalLight("white", 1);
+  directionalLight.position.x = 1;
+  directionalLight.position.z = 2;
+  scene.add(directionalLight);
+
+  // Controls
+  const controls = new TrackballControls(camera, renderer.domElement);
+  // 기본적으로 enableDamping 효과는 적용되어 있다.
+  controls.maxDistance = 20; // 줌 아웃 최대
+  controls.minDistance = 5; // 줌 인 최대
+  controls.target.set(3, 3, 3); // draw에서 update를 해줘야 한다.
+
+  // Mesh
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  let mesh;
+  let material;
+  for (let i = 0; i < 20; i++) {
+    material = new THREE.MeshStandardMaterial({
+      color: `rgb(${50 + Math.floor(Math.random() * 205)}, ${50 + Math.floor(Math.random() * 205)}, ${
+        50 + Math.floor(Math.random() * 205)
+      })`,
+    });
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.x = (Math.random() - 0.5) * 4;
+    mesh.position.y = (Math.random() - 0.5) * 4;
+    mesh.position.z = (Math.random() - 0.5) * 4;
+    scene.add(mesh);
+  }
+
+  // 그리기
+  const clock = new THREE.Clock();
+
+  function draw() {
+    const delta = clock.getDelta();
+    controls.update();
+
+    renderer.render(scene, camera);
+    renderer.setAnimationLoop(draw);
+  }
+
+  function setSize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  }
+
+  // 이벤트
+  window.addEventListener("resize", setSize);
+
+  draw();
+}
