c33060bf005b05e66f4906ac90a2eccc4470538d
wonieeVicky <hwfongfing@gmail.com>
2023-02-24 23:45:59 +0900

    feat: update three-js-1-6 test code -  PointerLockControls 구현 및 부가 옵션 상세

---
+++ GraphicLayout/threeJs/lecture/controls/src/ex05.js
@@ -0,0 +1,87 @@
+import * as THREE from "three";
+import { PointerLockControls } from "three/examples/jsm/controls/PointerLockControls";
+
+// ----- 주제: PointerLockControls
+
+export default function example() {
+  // Renderer
+  const canvas = document.querySelector("#three-canvas");
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true,
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+
+  // Scene
+  const scene = new THREE.Scene();
+
+  // Camera
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.y = 1.5;
+  camera.position.z = 4;
+  scene.add(camera);
+
+  // Light
+  const ambientLight = new THREE.AmbientLight("white", 0.5);
+  scene.add(ambientLight);
+
+  const directionalLight = new THREE.DirectionalLight("white", 1);
+  directionalLight.position.x = 1;
+  directionalLight.position.z = 2;
+  scene.add(directionalLight);
+
+  // Controls
+  const controls = new PointerLockControls(camera, renderer.domElement);
+  // console.log(controls.domElement); // <canvas id="three-canvas" data-engine="three.js r149" width="2506" height="2608" style="width: 1253px; height: 1304px;"></canvas>
+  // console.log(controls.domElement === renderer.domElement); // true
+
+  controls.domElement.addEventListener("click", () => {
+    controls.lock(); // pointer lock api 활용함
+  });
+  controls.addEventListener("lock", () => {
+    console.log("lock"); // pointer lock api 실행 시
+  });
+  controls.addEventListener("unlock", () => {
+    console.log("unlock"); // pointer lock api 중단 시
+  });
+
+  // Mesh
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  let mesh;
+  let material;
+  for (let i = 0; i < 20; i++) {
+    material = new THREE.MeshStandardMaterial({
+      color: `rgb(${50 + Math.floor(Math.random() * 205)}, ${50 + Math.floor(Math.random() * 205)}, ${
+        50 + Math.floor(Math.random() * 205)
+      })`,
+    });
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.x = (Math.random() - 0.5) * 4;
+    mesh.position.y = (Math.random() - 0.5) * 4;
+    mesh.position.z = (Math.random() - 0.5) * 4;
+    scene.add(mesh);
+  }
+
+  // 그리기
+  const clock = new THREE.Clock();
+
+  function draw() {
+    const delta = clock.getDelta();
+
+    renderer.render(scene, camera);
+    renderer.setAnimationLoop(draw);
+  }
+
+  function setSize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  }
+
+  // 이벤트
+  window.addEventListener("resize", setSize);
+
+  draw();
+}
