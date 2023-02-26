06abc71885b388189c6b4f12947dc044b6b01ba9
wonieeVicky <hwfongfing@gmail.com>
2023-02-27 00:09:11 +0900

    feat: update three-js-1-6 test code -  PointerLockControls에 keycontroller를 통한 스타일 컨트롤 구현 및 부가 옵션 상세

---
+++ GraphicLayout/threeJs/lecture/controls/src/KeyController.js
@@ -0,0 +1,16 @@
+﻿export class KeyController {
+  constructor() {
+    // 생성자
+    this.keys = [];
+
+    window.addEventListener("keydown", (e) => {
+      console.log(e.code + " 누름");
+      this.keys[e.code] = true; // 키보드 누르면 true - 예를 들어 w키를 누르면 this.keys["KeyW"] = true;
+    });
+
+    window.addEventListener("keyup", (e) => {
+      console.log(e.code + " 뗌");
+      delete this.keys[e.code]; // 키보드 뗄 때 this.keys["KeyW"] 삭제
+    });
+  }
+}
