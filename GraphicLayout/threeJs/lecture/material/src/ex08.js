4029cef24341fb847d0657828d097b51d1e17923
wonieeVicky <hwfongfing@gmail.com>
2023-03-05 00:05:37 +0900

    feat: update three-js-1-7 test code - texture image 변환 구현(RepeatWrapping, offset, rotation, center) & test image upload

---
+++ GraphicLayout/threeJs/lecture/material/src/ex08.js
@@ -0,0 +1,79 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+// ----- 주제: 텍스쳐 이미지 변환
+
+export default function example() {
+  const textureLoader = new THREE.TextureLoader();
+  const texture = textureLoader.load("/textures/skull/Ground Skull_basecolor.jpg");
+  texture.wrapS = THREE.RepeatWrapping; // r
+  texture.wrapT = THREE.RepeatWrapping;
+
+  // texture.offset.x = 0.3; // 텍스쳐의 위치를 이동시킴
+  // texture.offset.y = 0.3; // 텍스쳐의 위치를 이동시킴
+
+  // texture.repeat.x = 2;
+  // texture.repeat.y = 2;
+
+  // texture.rotation = Math.PI * 0.25; // 텍스쳐의 회전
+  texture.rotation = THREE.MathUtils.degToRad(65); // 텍스쳐의 회전
+  texture.center.x = 0.5; // 텍스쳐의 중심점 변경 - x축 센터 정렬
+  texture.center.y = 0.5; // 텍스쳐의 중심점 뱐걍 - y축 센터 정렬
+
+  // Renderer
+  const canvas = document.querySelector("#three-canvas");
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true,
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+
+  // Scene
+  const scene = new THREE.Scene();
+  scene.background = new THREE.Color("white");
+
+  // Camera
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.y = 1.5;
+  camera.position.z = 4;
+  scene.add(camera);
+
+  // Light
+  const ambientLight = new THREE.AmbientLight("white", 0.5);
+  const directionalLight = new THREE.DirectionalLight("white", 1);
+  directionalLight.position.set(1, 1, 2);
+  scene.add(ambientLight, directionalLight);
+
+  // Controls
+  const controls = new OrbitControls(camera, renderer.domElement); // camera lookat으로 물체를 바라보므로 정중앙에 사물이 위치함
+
+  // Mesh
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshStandardMaterial({ map: texture });
+
+  const mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  // 그리기
+  const clock = new THREE.Clock();
+
+  function draw() {
+    const delta = clock.getDelta();
+
+    renderer.render(scene, camera);
+    renderer.setAnimationLoop(draw);
+  }
+
+  function setSize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  }
+
+  // 이벤트
+  window.addEventListener("resize", setSize);
+
+  draw();
+}
--- GraphicLayout/threeJs/lecture/material/src/main.js
+++ GraphicLayout/threeJs/lecture/material/src/main.js
@@ -4,6 +4,7 @@
 // import example from "./ex04";
 // import example from "./ex05";
 // import example from "./ex06";
-import example from "./ex07";
+// import example from "./ex07";
+import example from "./ex08";
 
 example();
