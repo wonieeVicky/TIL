c33c68796d049301c007e4be4ee94d5e327753f2
wonieeVicky <hwfongfing@gmail.com>
2023-04-17 22:21:15 +0900

    feat: update three-js-1-10 test code - glft 커스텀 모델 프로젝트에 로드 구현

--- GraphicLayout/threeJs/lecture/custom_model/src/ex01.js
+++ GraphicLayout/threeJs/lecture/custom_model/src/ex01.js
@@ -1,65 +1,66 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 // ----- 주제: glb 파일 불러오기
 
 export default function example() {
-	// Renderer
-	const canvas = document.querySelector('#three-canvas');
-	const renderer = new THREE.WebGLRenderer({
-		canvas,
-		antialias: true
-	});
-	renderer.setSize(window.innerWidth, window.innerHeight);
-	renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+  // Renderer
+  const canvas = document.querySelector("#three-canvas");
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
 
-	// Scene
-	const scene = new THREE.Scene();
+  // Scene
+  const scene = new THREE.Scene();
 
-	// Camera
-	const camera = new THREE.PerspectiveCamera(
-		75,
-		window.innerWidth / window.innerHeight,
-		0.1,
-		1000
-	);
-	camera.position.y = 1.5;
-	camera.position.z = 4;
-	scene.add(camera);
+  // Camera
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.y = 1.5;
+  camera.position.z = 4;
+  scene.add(camera);
 
-	// Light
-	const ambientLight = new THREE.AmbientLight('white', 0.5);
-	scene.add(ambientLight);
+  // Light
+  const ambientLight = new THREE.AmbientLight("white", 0.5);
+  scene.add(ambientLight);
 
-	const directionalLight = new THREE.DirectionalLight('white', 1);
-	directionalLight.position.x = 1;
-	directionalLight.position.z = 2;
-	scene.add(directionalLight);
+  const directionalLight = new THREE.DirectionalLight("white", 1);
+  directionalLight.position.x = 1;
+  directionalLight.position.z = 2;
+  scene.add(directionalLight);
 
-	// Controls
-	const controls = new OrbitControls(camera, renderer.domElement);
+  // Controls
+  const controls = new OrbitControls(camera, renderer.domElement);
 
-	// gltf loader
+  // gltf loader
+  const gltfLoader = new GLTFLoader();
+  gltfLoader.load("/models/ilbuni.glb", (gltf) => {
+    const character = gltf.scene.children[0];
+    scene.add(character);
+  });
 
-	// 그리기
-	const clock = new THREE.Clock();
+  // 그리기
+  const clock = new THREE.Clock();
 
-	function draw() {
-		const delta = clock.getDelta();
+  function draw() {
+    const delta = clock.getDelta();
 
-		renderer.render(scene, camera);
-		renderer.setAnimationLoop(draw);
-	}
+    renderer.render(scene, camera);
+    renderer.setAnimationLoop(draw);
+  }
 
-	function setSize() {
-		camera.aspect = window.innerWidth / window.innerHeight;
-		camera.updateProjectionMatrix();
-		renderer.setSize(window.innerWidth, window.innerHeight);
-		renderer.render(scene, camera);
-	}
+  function setSize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  }
 
-	// 이벤트
-	window.addEventListener('resize', setSize);
+  // 이벤트
+  window.addEventListener("resize", setSize);
 
-	draw();
+  draw();
 }
