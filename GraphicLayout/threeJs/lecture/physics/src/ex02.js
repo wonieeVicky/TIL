5dbdda60079e895877e3ee2470d275d737c0f0bc
wonieeVicky <hwfongfing@gmail.com>
2023-04-25 20:39:52 +0900

    feat: update three-js-1-10 test code - cannonWorld에 contactMaterial 을 통한 마찰력과 반발력 구현

---
+++ GraphicLayout/threeJs/lecture/physics/src/ex02.js
@@ -0,0 +1,133 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import * as CANNON from "cannon-es";
+
+// ----- 주제: Contact Material
+
+export default function example() {
+  // Renderer
+  const canvas = document.querySelector("#three-canvas");
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+  // Scene
+  const scene = new THREE.Scene();
+
+  // Camera
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.y = 4.5;
+  camera.position.z = 15;
+  scene.add(camera);
+
+  // Light
+  const ambientLight = new THREE.AmbientLight("white", 0.5);
+  scene.add(ambientLight);
+
+  const directionalLight = new THREE.DirectionalLight("white", 1);
+  directionalLight.position.x = 1;
+  directionalLight.position.z = 2;
+  directionalLight.castShadow = true;
+  scene.add(directionalLight);
+
+  // Controls
+  const controls = new OrbitControls(camera, renderer.domElement);
+
+  const cannonWorld = new CANNON.World();
+  cannonWorld.gravity.set(0, -10, 0); // 중력을 세팅(x, y, z축 설정)
+
+  // Contact Material
+  const defaultMaterial = new CANNON.Material("default");
+  const rubberMaterial = new CANNON.Material("rubber");
+  const ironMaterial = new CANNON.Material("iron");
+  const defaultContactMaterial = new CANNON.ContactMaterial(defaultMaterial, defaultMaterial, {
+    friction: 0.5, // 마찰력,
+    restitution: 0.3 // 반발력
+  }); // 부딪힐 재질을 두 개 넣음 + 상세 설정 추가
+  cannonWorld.defaultContactMaterial = defaultContactMaterial;
+
+  const rubberDefaultContactMaterial = new CANNON.ContactMaterial(rubberMaterial, defaultMaterial, {
+    friction: 0.5,
+    restitution: 0.7
+  });
+  cannonWorld.addContactMaterial(rubberDefaultContactMaterial);
+
+  const ironDefaultContactMaterial = new CANNON.ContactMaterial(ironMaterial, defaultMaterial, {
+    friction: 0.5,
+    restitution: 0
+  });
+  cannonWorld.addContactMaterial(ironDefaultContactMaterial);
+
+  const floorShape = new CANNON.Plane(); // 바닥을 만들기 위한 모양
+  const floorBody = new CANNON.Body({
+    // 무게를 가지는 바닥 - 물리엔진이 적용된 실체(유리컴)
+    mass: 0, // 무게 설정
+    position: new CANNON.Vec3(0, 0, 0), // 바닥의 위치
+    shape: floorShape, // 바닥의 모양
+    material: defaultMaterial
+  });
+  floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(-1, 0, 0), Math.PI * 0.5); // 바닥의 회전(축의 방향(x축으로 설정), 각도(90도))
+  cannonWorld.addBody(floorBody); // 바닥을 월드에 추가
+
+  const sphereShape = new CANNON.Sphere(0.5); // 구의 반지름을 넣음
+  const sphereBody = new CANNON.Body({
+    mass: 1,
+    position: new CANNON.Vec3(0, 10, 0),
+    shape: sphereShape,
+    material: ironMaterial
+  });
+  cannonWorld.addBody(sphereBody); // 박스를 월드에 추가
+
+  // Mesh
+  const floorMesh = new THREE.Mesh(
+    new THREE.PlaneGeometry(10, 10),
+    new THREE.MeshStandardMaterial({ color: "slategray" })
+  );
+  floorMesh.rotation.x = -Math.PI * 0.5; // 앞면이 위로 향하기 위해 음수 처리
+  floorMesh.receiveShadow = true;
+  scene.add(floorMesh);
+
+  const sphereGeometry = new THREE.SphereGeometry(0.5); // 반지름 입력
+  const sphereMaterial = new THREE.MeshStandardMaterial({
+    color: "seagreen"
+  });
+  const sphereMesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
+  sphereMesh.position.y = 0.5;
+  sphereMesh.castShadow = true;
+  scene.add(sphereMesh);
+
+  // 그리기
+  const clock = new THREE.Clock();
+
+  function draw() {
+    const delta = clock.getDelta();
+
+    let cannonStepTime = 1 / 60;
+    if (delta < 0.01) cannonStepTime = 1 / 120;
+
+    cannonWorld.step(cannonStepTime, delta, 3); // 물리 엔진을 계산(시간, 델타, 반복 횟수)
+    // floorMesh.position.copy(floorBody.position); // 바닥의 위치를 바닥 메쉬에 적용å
+    sphereMesh.position.copy(sphereBody.position); // 박스의 위치를 박스 메쉬에 적용
+    sphereMesh.quaternion.copy(sphereBody.quaternion); // 박스의 회전을 박스 메쉬에 적용
+
+    renderer.render(scene, camera);
+    renderer.setAnimationLoop(draw);
+  }
+
+  function setSize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  }
+
+  // 이벤트
+  window.addEventListener("resize", setSize);
+
+  draw();
+}
