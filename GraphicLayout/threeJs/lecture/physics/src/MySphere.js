b53e447e5e8fa375dcc18ff9fc7ae147d713f4a4
wonieeVicky <hwfongfing@gmail.com>
2023-04-28 23:32:07 +0900

    feat: update three-js-1-11 test code - 랜덤한 위치에 공을 생성하는 click 이벤트 구현(MySphere 기본 클래스 구현)

---
+++ GraphicLayout/threeJs/lecture/physics/src/MySphere.js
@@ -0,0 +1,19 @@
+﻿import { Mesh } from "three";
+
+export class MySphere {
+  constructor(info) {
+    this.scene = info.scene;
+    this.geometry = info.geometry;
+    this.material = info.material;
+    this.x = info.x;
+    this.y = info.y;
+    this.z = info.z;
+    this.scale = info.scale;
+
+    this.mesh = new Mesh(this.geometry, this.material);
+    this.mesh.scale.set(this.scale, this.scale, this.scale);
+    this.mesh.castShadow = true;
+    this.mesh.position.set(this.x, this.y, this.z); // position 설정은 cannon body에서 설정하면 mesh가 따라가므로 무의미함
+    this.scene.add(this.mesh);
+  }
+}
