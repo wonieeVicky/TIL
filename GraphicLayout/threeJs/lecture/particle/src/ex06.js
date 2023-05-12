1d2db03b66c908716d45f25309f0b1650cae8721
wonieeVicky <hwfongfing@gmail.com>
2023-05-12 23:29:03 +0900

    feat: update three-js-1-12 test code - 형태가 바뀌는 이미지 패널 구현 (ImagePanel 생성부 구현)

---
+++ GraphicLayout/threeJs/lecture/particle/src/ex06.js
@@ -0,0 +1,85 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+// ----- 주제: 형태가 바뀌는 이미지 패널
+
+export default function example() {
+  // Renderer
+  const canvas = document.querySelector("#three-canvas");
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+
+  // Scene
+  const scene = new THREE.Scene();
+
+  // Camera
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.y = 1.5;
+  camera.position.z = 4;
+  scene.add(camera);
+
+  // Light
+  const ambientLight = new THREE.AmbientLight("white", 0.5);
+  scene.add(ambientLight);
+
+  const directionalLight = new THREE.DirectionalLight("white", 1);
+  directionalLight.position.x = 1;
+  directionalLight.position.z = 2;
+  scene.add(directionalLight);
+
+  // Controls
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.enableDamping = true;
+
+  // Mesh
+  const planeGeometry = new THREE.PlaneGeometry(0.3, 0.3);
+
+  // textureLoader
+  const textureLoader = new THREE.TextureLoader();
+
+  // Points
+  const sphereGeometry = new THREE.SphereGeometry(1, 8, 8);
+  const positionArray = sphereGeometry.attributes.position.array; // planeMesh의 position 정보
+
+  // 여러 개의 Plane Mesh 생성
+  let imagePanel;
+  for (let i = 0; i < positionArray.length; i += 3) {
+    imagePanel = new ImagePanel({
+      textureLoader,
+      scene,
+      geometry: planeGeometry,
+      imageSrc: `/images/0${Math.ceil(Math.random() * 5)}.jpg`,
+      x: positionArray[i],
+      y: positionArray[i + 1],
+      z: positionArray[i + 2]
+    });
+  }
+
+  // 그리기
+  const clock = new THREE.Clock();
+
+  function draw() {
+    const delta = clock.getDelta();
+
+    controls.update();
+
+    renderer.render(scene, camera);
+    renderer.setAnimationLoop(draw);
+  }
+
+  function setSize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  }
+
+  // 이벤트
+  window.addEventListener("resize", setSize);
+
+  draw();
+}
--- GraphicLayout/threeJs/lecture/particle/src/main.js
+++ GraphicLayout/threeJs/lecture/particle/src/main.js
@@ -2,6 +2,7 @@
 // import example from './ex02';
 // import example from './ex03';
 // import example from './ex04';
-import example from "./ex05";
+// import example from "./ex05";
+import example from "./ex06";
 
 example();
