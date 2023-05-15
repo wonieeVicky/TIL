3e55a14e162bb4976b137351e0a65cace31c0811
wonieeVicky <hwfongfing@gmail.com>
2023-05-15 19:48:49 +0900

    feat: update three-js-12 webpack.config - models glb 파일 호출을 위한 설정 추가

--- GraphicLayout/threeJs/lecture/scroll_page/webpack.config.js
+++ GraphicLayout/threeJs/lecture/scroll_page/webpack.config.js
@@ -67,8 +67,8 @@ module.exports = {
 		new CopyWebpackPlugin({
 			patterns: [
 				{ from: "./src/main.css", to: "./main.css" },
+				{ from: "./src/models", to: "./models" },
 				// { from: "./src/images", to: "./images" },
-				// { from: "./src/models", to: "./models" },
 				// { from: "./src/sounds", to: "./sounds" }
 			],
 		})
