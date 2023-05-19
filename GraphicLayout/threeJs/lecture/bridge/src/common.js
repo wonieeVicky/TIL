858c6be70d4faef10871b4f5c43d79dd49cc54f5
wonieeVicky <hwfongfing@gmail.com>
2023-05-19 18:58:36 +0900

    feat: update three-js-1-14(three.js bridge 3d game) - 기본 장면 구성 시 공통 common 모듈에서 사용하도록 cm1, cm2 객체 구현

---
+++ GraphicLayout/threeJs/lecture/bridge/src/common.js
@@ -0,0 +1,10 @@
+﻿import { Scene } from "three";
+
+export const cm1 = {
+  canvas: document.querySelector("#three-canvas"),
+  scene: new Scene()
+};
+
+export const cm2 = {
+  backgroundColor: "#3e1322"
+};
--- GraphicLayout/threeJs/lecture/bridge/src/main.js
+++ GraphicLayout/threeJs/lecture/bridge/src/main.js
@@ -1,73 +1,67 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { cm1, cm2 } from "./common";
 
 // ----- 주제: The Bridge 게임 만들기
 
 // Renderer
-const canvas = document.querySelector('#three-canvas');
 const renderer = new THREE.WebGLRenderer({
-	canvas,
-	antialias: true
+  canvas: cm1.canvas,
+  antialias: true
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
 
-// Scene
-const scene = new THREE.Scene();
+// scene
+cm1.scene.background = new THREE.Color(cm2.backgroundColor);
 
 // Camera
-const camera = new THREE.PerspectiveCamera(
-	75,
-	window.innerWidth / window.innerHeight,
-	0.1,
-	1000
-);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.y = 1.5;
 camera.position.z = 4;
-scene.add(camera);
+cm1.scene.add(camera);
 
 // Light
-const ambientLight = new THREE.AmbientLight('white', 0.5);
-scene.add(ambientLight);
+const ambientLight = new THREE.AmbientLight("white", 0.5);
+cm1.scene.add(ambientLight);
 
-const directionalLight = new THREE.DirectionalLight('white', 1);
+const directionalLight = new THREE.DirectionalLight("white", 1);
 directionalLight.position.x = 1;
 directionalLight.position.z = 2;
-scene.add(directionalLight);
+cm1.scene.add(directionalLight);
 
 // Controls
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 
-
 // Mesh
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshStandardMaterial({
-	color: 'seagreen'
+  color: "seagreen"
 });
 const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+cm1.scene.add(mesh);
 
 // 그리기
 const clock = new THREE.Clock();
 
 function draw() {
-	const delta = clock.getDelta();
+  const delta = clock.getDelta();
 
-	controls.update();
+  controls.update();
 
-	renderer.render(scene, camera);
-	renderer.setAnimationLoop(draw);
+  renderer.render(cm1.scene, camera);
+  renderer.setAnimationLoop(draw);
 }
 
 function setSize() {
-	camera.aspect = window.innerWidth / window.innerHeight;
-	camera.updateProjectionMatrix();
-	renderer.setSize(window.innerWidth, window.innerHeight);
-	renderer.render(scene, camera);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.render(cm1.scene, camera);
 }
 
 // 이벤트
-window.addEventListener('resize', setSize);
+window.addEventListener("resize", setSize);
 
 draw();
