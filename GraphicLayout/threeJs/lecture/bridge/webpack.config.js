9aa69f1abf655e120c35213c657e6ab2398370b9
wonieeVicky <hwfongfing@gmail.com>
2023-05-19 18:45:38 +0900

    feat: three-js-1-14(three.js bridge 3d animation) sounds 파일 추가, webpack 설정 내 sounds 폴더 반영

--- GraphicLayout/threeJs/lecture/bridge/webpack.config.js
+++ GraphicLayout/threeJs/lecture/bridge/webpack.config.js
@@ -69,7 +69,7 @@ module.exports = {
 				{ from: "./src/main.css", to: "./main.css" },
 				// { from: "./src/images", to: "./images" },
 				{ from: "./src/models", to: "./models" },
-				// { from: "./src/sounds", to: "./sounds" }
+				{ from: "./src/sounds", to: "./sounds" }
 			],
 		})
 	]
