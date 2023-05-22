b0ac8f95f51e4d3fae1f343bdcb319cc4cfda929
wonieeVicky <hwfongfing@gmail.com>
2023-05-22 23:19:45 +0900

    feat: update three-js-1-14(three.js bridge 3d game) - Pillar, Floor 클래스 호출하여 바닥, 기둥 렌더링

--- GraphicLayout/threeJs/lecture/bridge/src/main.js
+++ GraphicLayout/threeJs/lecture/bridge/src/main.js
@@ -1,6 +1,8 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { cm1, cm2 } from "./common";
+import { Pillar } from "./Pillar";
+import { Floor } from "./Floor";
 
 // ----- 주제: The Bridge 게임 만들기
 
@@ -12,15 +14,16 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
 renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap; // 그림자를 부드럽게 보이게 함
 
 // scene
 cm1.scene.background = new THREE.Color(cm2.backgroundColor);
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.y = 1.5;
-camera.position.z = 4;
+camera.position.x = -4;
+camera.position.y = 19;
+camera.position.z = 14;
 cm1.scene.add(camera);
 
 // Light
@@ -45,13 +48,25 @@ cm1.scene.add(spotLight1, spotLight2, spotLight3, spotLight4);
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 
-// Mesh
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshStandardMaterial({
-  color: "seagreen"
+// 물체 만들기
+const glassUnitSize = 1.2; // 유리칸의 사이즈
+
+// 기둥
+const pillar1 = new Pillar({
+  name: "pillar",
+  x: 0,
+  y: 5.5, // pillar의 높이가 10이라서 절반으로 조정
+  z: -glassUnitSize * 12 - glassUnitSize / 2 // 1.2는 다리의 유리칸 하나의 사이즈, 12는 다리의 유리칸 개수, 0.6은 다리의 유리칸 사이의 간격
 });
-const mesh = new THREE.Mesh(geometry, material);
-cm1.scene.add(mesh);
+const pillar2 = new Pillar({
+  name: "pillar",
+  x: 0,
+  y: 5.5,
+  z: glassUnitSize * 12 + glassUnitSize / 2 // 1.2는 다리의 유리칸 하나의 사이즈, 12는 다리의 유리칸 개수, 0.6은 다리의 유리칸 사이의 간격
+});
+
+// 바닥
+const floor = new Floor({ name: "floor" });
 
 // 그리기
 const clock = new THREE.Clock();
