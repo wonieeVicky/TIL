c1ae22aceb8a25e91a58c616ecc49d1782b60cb2
wonieeVicky <hwfongfing@gmail.com>
2023-05-24 23:37:40 +0900

    feat: update three-js-1-14(three.js bridge 3d game) - Glass 클래스 (일반 유리, 강화 유리) 구현,

---
+++ GraphicLayout/threeJs/lecture/bridge/src/Glass.js
@@ -0,0 +1,29 @@
+﻿import { Mesh } from "three";
+import { Stuff } from "./Stuff";
+import { cm1, geo, mat } from "./common";
+
+export class Glass extends Stuff {
+  constructor(info) {
+    super(info);
+
+    this.type = info.type;
+
+    this.geometry = geo.glass;
+    switch (this.type) {
+      case "normal":
+        this.material = mat.glass1;
+        break;
+      case "strong":
+        this.material = mat.glass2;
+        break;
+    }
+    // this.material = mat.glass;
+
+    this.mesh = new Mesh(this.geometry, this.material);
+    this.mesh.position.set(this.x, this.y, this.z);
+    this.mesh.castShadow = true;
+    this.mesh.receiveShadow = true;
+
+    cm1.scene.add(this.mesh);
+  }
+}
--- GraphicLayout/threeJs/lecture/bridge/src/common.js
+++ GraphicLayout/threeJs/lecture/bridge/src/common.js
@@ -10,19 +10,23 @@ export const cm2 = {
   lightColor: "#ffe9ac",
   pillarColor: "#071d28",
   floorColor: "#111",
-  barColor: "#441c1d"
+  barColor: "#441c1d",
+  glassColor: "#9fdfff"
 };
 
 export const geo = {
   pillar: new BoxGeometry(5, 10, 5),
   floor: new BoxGeometry(200, 1, 200),
   bar: new BoxGeometry(0.1, 0.3, 1.2 * 21),
-  sideLight: new SphereGeometry(0.1, 6, 6)
+  sideLight: new SphereGeometry(0.1, 6, 6),
+  glass: new BoxGeometry(1.2, 0.1, 1.2)
 };
 
 export const mat = {
   pillar: new MeshPhongMaterial({ color: cm2.pillarColor }),
   floor: new MeshPhongMaterial({ color: cm2.floorColor }),
   bar: new MeshPhongMaterial({ color: cm2.barColor }),
-  sideLight: new MeshPhongMaterial({ color: cm2.lightColor })
+  sideLight: new MeshPhongMaterial({ color: cm2.lightColor }),
+  glass1: new MeshPhongMaterial({ color: cm2.glassColor, transparent: true, opacity: 0.1 }),
+  glass2: new MeshPhongMaterial({ color: cm2.glassColor, transparent: true, opacity: 0.5 })
 };
