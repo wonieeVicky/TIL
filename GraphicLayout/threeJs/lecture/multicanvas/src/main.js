284fd40663abf35f867070b3e15dd9ea0973392d
wonieeVicky <hwfongfing@gmail.com>
2023-06-07 23:29:10 +0900

    chore: three-js-1-15(three.js multicanvas) test env initialize

---
+++ GraphicLayout/threeJs/lecture/multicanvas/src/main.js
@@ -0,0 +1,34 @@
+import * as THREE from 'three';
+
+// ----- 주제: 여러개의 캔버스 사용하기
+
+// Renderer
+const canvas = document.querySelector('#three-canvas');
+const renderer = new THREE.WebGLRenderer({
+	canvas,
+	antialias: true
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+
+// 그리기
+const clock = new THREE.Clock();
+
+function draw() {
+	const delta = clock.getDelta();
+
+	renderer.render(scene, camera);
+	renderer.setAnimationLoop(draw);
+}
+
+function setSize() {
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.render(scene, camera);
+}
+
+// 이벤트
+window.addEventListener('resize', setSize);
+
+draw();
