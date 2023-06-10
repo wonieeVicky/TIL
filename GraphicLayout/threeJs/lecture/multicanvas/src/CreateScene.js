e2ca44f299fb5e97e75d13ff4d969d255b292078
wonieeVicky <hwfongfing@gmail.com>
2023-06-10 23:49:47 +0900

    feat: update three-js-1-15(three.js multi canvas) - CreateScene 클래스 생성 후 Scene, Camera 설정 기능 구현

---
+++ GraphicLayout/threeJs/lecture/multicanvas/src/CreateScene.js
@@ -0,0 +1,27 @@
+﻿import { Color, PerspectiveCamera, Scene } from "three";
+
+export class CreateScene {
+  constructor(info) {
+    this.renderer = info.renderer;
+    this.el = document.querySelector(info.placeholder);
+    const rect = this.el.getBoundingClientRect(); // DOMRect {x: 8, y: 8, width: 784, …}
+    console.log("rect:", rect);
+
+    const bgColor = info.bgColor || "white";
+    const fov = info.fov || 75; // field of view
+    const aspect = rect.width / rect.height;
+    const near = info.near || 0.1;
+    const far = info.far || 100;
+    const cameraPosition = info.cameraPosition || { x: 0, y: 0, z: 3 };
+
+    // Scene
+    this.scene = new Scene();
+    this.scene.background = new Color(bgColor);
+
+    // Camera
+    this.camera = new PerspectiveCamera(fov, aspect, near, far);
+    this.camera.position.set(cameraPosition.x, cameraPosition.y, cameraPosition.z);
+
+    this.scene.add(this.camera);
+  }
+}
--- GraphicLayout/threeJs/lecture/multicanvas/src/main.js
+++ GraphicLayout/threeJs/lecture/multicanvas/src/main.js
@@ -1,34 +1,36 @@
-import * as THREE from 'three';
+import * as THREE from "three";
+import { CreateScene } from "./CreateScene";
 
 // ----- 주제: 여러개의 캔버스 사용하기
 
 // Renderer
-const canvas = document.querySelector('#three-canvas');
+const canvas = document.querySelector("#three-canvas");
 const renderer = new THREE.WebGLRenderer({
-	canvas,
-	antialias: true
+  canvas,
+  antialias: true
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+
+const scene1 = new CreateScene({
+  renderer,
+  bgColor: "pink",
+  placeholder: ".canvas-placeholder.a"
+});
 
 // 그리기
 const clock = new THREE.Clock();
 
 function draw() {
-	const delta = clock.getDelta();
-
-	renderer.render(scene, camera);
-	renderer.setAnimationLoop(draw);
+  const delta = clock.getDelta();
+  renderer.setAnimationLoop(draw);
 }
 
 function setSize() {
-	camera.aspect = window.innerWidth / window.innerHeight;
-	camera.updateProjectionMatrix();
-	renderer.setSize(window.innerWidth, window.innerHeight);
-	renderer.render(scene, camera);
+  renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 // 이벤트
-window.addEventListener('resize', setSize);
+window.addEventListener("resize", setSize);
 
 draw();
