ced4505d4427f8ba2ff0f4d281199e1bd14a93ff
wonieeVicky <hwfongfing@gmail.com>
2023-12-06 22:24:44 +0900

    feat: update game func typing

---
+++ Typescript/ts-with-oop/2-types-projects-quiz/game.ts
@@ -0,0 +1,34 @@
+/**
+ * Let's make a game 🕹
+ */
+
+let position = { x: 0, y: 0 };
+type Direction = 'up' | 'down' | 'left' | 'right';
+function move(direction: Direction) {
+  switch (direction) {
+    case 'up':
+      position.y += 1;
+      break;
+    case 'down':
+      position.y -= 1;
+      break;
+    case 'left':
+      position.x -= 1;
+      break;
+    case 'right':
+      position.x += 1;
+      break;
+    default:
+      throw Error('unknown direction');
+  }
+}
+
+console.log(position); // { x: 0, y: 0}
+move('up');
+console.log(position); // { x: 0, y: 1}
+move('down');
+console.log(position); // { x: 0, y: 0}
+move('left');
+console.log(position); // { x: -1, y: 0}
+move('right');
+console.log(position); // { x: 0, y: 0}
