13337aebdb36c79aa458dc6733a86f0c71efaa12
wonieeVicky <hwfongfing@gmail.com>
2023-12-11 00:02:45 +0900

    feat: update ts-with-oop-3-1-without-oop(절차지향적 함수 구현)

---
+++ Typescript/ts-with-oop/3-oop/3-1-without-oop.ts
@@ -0,0 +1,24 @@
+﻿{
+  type CoffeeCup = {
+    shots: number;
+    hasMilk: boolean;
+  };
+
+  const BEANS_GRAMM_PER_SHOT = 7; // 커피를 내릴 때 필요한 원두의 양
+  let coffeeBeans: number = 0;
+  function makeCoffee(shots: number): CoffeeCup {
+    if (coffeeBeans < shots * BEANS_GRAMM_PER_SHOT) {
+      throw new Error('Not enough coffee beans!');
+    }
+    coffeeBeans -= shots * BEANS_GRAMM_PER_SHOT;
+
+    return {
+      shots,
+      hasMilk: false
+    };
+  }
+
+  coffeeBeans += 3 * BEANS_GRAMM_PER_SHOT;
+  const coffee = makeCoffee(2);
+  console.log(coffee);
+}
