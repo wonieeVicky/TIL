e81b106c086c0836f683ffa227746a8ac7591037
wonieeVicky <hwfongfing@gmail.com>
2023-12-12 23:31:11 +0900

    feat: update ts-with-oop-3-2-class(객체지향 함수 구현)

---
+++ Typescript/ts-with-oop/3-oop/3-2-class.ts
@@ -0,0 +1,43 @@
+﻿{
+  type CoffeeCup = {
+    shots: number;
+    hasMilk: boolean;
+  };
+
+  class CoffeeMaker {
+    static BEANS_GRAMM_PER_SHOT: number = 7; // 중복적으로 사용되는 변수는 class level로 설정
+    coffeeBeans: number = 0; // instance (object) level
+
+    // instance를 만들 때 초기에 항상 호출되는 함수
+    constructor(coffeeBeans: number) {
+      this.coffeeBeans += coffeeBeans; // this.coffeeBeans !== coffeeBeans
+    }
+
+    // constructor를 사용하지 않고, static method를 사용하여 instance를 생성하는 방법
+    static makeMachine(coffeeBeans: number): CoffeeMaker {
+      return new CoffeeMaker(coffeeBeans);
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      if (this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT) {
+        throw new Error('Not enough coffee beans!');
+      }
+
+      this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT;
+
+      return {
+        shots,
+        hasMilk: false
+      };
+    }
+  }
+
+  const makerInstance = new CoffeeMaker(32);
+  console.log(makerInstance); // CoffeeMaker { coffeeBeans: 32 }
+
+  const makerInstance2 = new CoffeeMaker(14);
+  console.log(makerInstance2); // CoffeeMaker { coffeeBeans: 14 }
+
+  const makerInstance3 = CoffeeMaker.makeMachine(3);
+  console.log(makerInstance3); // CoffeeMaker { coffeeBeans: 3 }
+}
