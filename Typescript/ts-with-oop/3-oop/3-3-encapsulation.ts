930c71ad06d6fb2fbf344ca86a44b58cea84b459
wonieeVicky <hwfongfing@gmail.com>
2023-12-13 22:38:16 +0900

    feat: update ts-with-oop-3-3-encapsulation(클래스 캡슐화 구현)

---
+++ Typescript/ts-with-oop/3-oop/3-3-encapsulation.ts
@@ -0,0 +1,44 @@
+﻿{
+  type CoffeeCup = {
+    shots: number;
+    hasMilk: boolean;
+  };
+
+  class CoffeeMaker {
+    private static BEANS_GRAMM_PER_SHOT: number = 7; // class level
+    private coffeeBeans: number = 0; // instance (object) level
+
+    // instance를 만들 때 초기에 항상 호출되는 함수
+    private constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans; // this.coffeeBeans !== coffeeBeans
+    }
+
+    // constructor를 사용하지 않고, static method를 사용하여 instance를 생성하는 방법
+    static makeMachine(coffeeBeans: number): CoffeeMaker {
+      return new CoffeeMaker(coffeeBeans);
+    }
+
+    fillCoffeeBeans(beans: number) {
+      if (beans < 0) {
+        throw new Error('value for beans should be greater than 0');
+      }
+
+      this.coffeeBeans += beans;
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      if (this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT) {
+        throw new Error('Not enough coffee beans!');
+      }
+
+      this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT;
+
+      return {
+        shots,
+        hasMilk: false
+      };
+    }
+  }
+
+  const makerInstance = CoffeeMaker.makeMachine(32);
+}
