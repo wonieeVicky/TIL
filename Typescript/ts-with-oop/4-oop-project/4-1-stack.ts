4c1ec798e471fef3000200106edaf00e63b1b3a8
wonieeVicky <hwfongfing@gmail.com>
2023-12-30 00:09:42 +0900

    feat: stack class 연결리스트로 구현

--- Typescript/ts-with-oop/4-oop-project/4-1-stack.ts
+++ Typescript/ts-with-oop/4-oop-project/4-1-stack.ts
@@ -1,30 +1,57 @@
 ﻿{
-  class Stack {
-    private data: string = '';
-    constructor() {
-      this.data = '';
+  // 인터페이스로 정의해서 좋은 이유?
+  // 스택을 사용하는 사람들이 나중에 스택을 사용하거나 다른 스택을 사용할 때
+  // 사용하는 방법을 쉽게 알 수 있게 해준다.
+  interface Stack {
+    readonly size: number;
+    push(value: string): void;
+    pop(): string;
+  }
+
+  type StackNode = {
+    readonly value: string;
+    readonly next?: StackNode;
+  };
+
+  class StackImpl implements Stack {
+    private _size: number = 0; // 내부에서 사용하는 변수임을 _로 표현
+    private head?: StackNode;
+    constructor(private capacity: number) {}
+
+    get size() {
+      return this._size;
     }
 
-    push(item: string) {
-      this.data.length === 0 ? (this.data = item) : (this.data += `, ${item}`);
+    push(value: string) {
+      if (this.size === this.capacity) {
+        throw new Error('Stack is full!');
+      }
+      const node: StackNode = {
+        value,
+        next: this.head
+      };
+      this.head = node;
+      this._size++;
     }
 
-    pop() {
-      this.data = this.data.split(', ').slice(0, -1).join(', ');
-    }
-
-    size() {
-      return this.data.split(', ').length;
+    pop(): string {
+      // null == undefined, null !== undefined
+      if (this.head == null) {
+        throw new Error('Stack is empty');
+      }
+      const node = this.head;
+      this.head = node.next;
+      this._size--;
+      return node.value;
     }
   }
-  const stackInstance = new Stack();
-  stackInstance.push('a');
-  console.log(stackInstance);
-  stackInstance.push('b');
-  console.log(stackInstance);
-  stackInstance.push('c');
-  console.log(stackInstance);
-  stackInstance.pop();
-  console.log(stackInstance);
-  console.log(stackInstance.size());
+
+  const stack = new StackImpl(5); // max capacity = 5
+  stack.push('Vicky1');
+  stack.push('Vicky2');
+  stack.push('Vicky3');
+  while (stack.size !== 0) {
+    console.log(stack.pop());
+  }
+  console.log(stack.pop()); // error
 }
