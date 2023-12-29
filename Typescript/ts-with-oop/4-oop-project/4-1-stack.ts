836edbbe6c61ead2cae59d97889c6719d3498ce0
wonieeVicky <hwfongfing@gmail.com>
2023-12-29 23:42:37 +0900

    feat: stack class 구현

---
+++ Typescript/ts-with-oop/4-oop-project/4-1-stack.ts
@@ -0,0 +1,30 @@
+﻿{
+  class Stack {
+    private data: string = '';
+    constructor() {
+      this.data = '';
+    }
+
+    push(item: string) {
+      this.data.length === 0 ? (this.data = item) : (this.data += `, ${item}`);
+    }
+
+    pop() {
+      this.data = this.data.split(', ').slice(0, -1).join(', ');
+    }
+
+    size() {
+      return this.data.split(', ').length;
+    }
+  }
+  const stackInstance = new Stack();
+  stackInstance.push('a');
+  console.log(stackInstance);
+  stackInstance.push('b');
+  console.log(stackInstance);
+  stackInstance.push('c');
+  console.log(stackInstance);
+  stackInstance.pop();
+  console.log(stackInstance);
+  console.log(stackInstance.size());
+}
