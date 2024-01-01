eab3b3cbd085bcba25a4b0ce9c9022e8442f5edd
wonieeVicky <hwfongfing@gmail.com>
2024-01-01 23:35:57 +0900

    feat: generic function 문제 내용 코딩 추가

---
+++ Typescript/ts-with-oop/5-generic/5-1-function.ts
@@ -0,0 +1,27 @@
+﻿{
+  function checkNotNullBad(arg: number | null): number {
+    if (arg == null) {
+      throw new Error('not valid number!');
+    }
+    return arg;
+  }
+  const result = checkNotNullBad(123); // 123
+  checkNotNullBad(null); // ErcheckNotNullBadror: not valid number!
+
+  // 타입의 정보가 없어지므로 any를 사용하면 좋지 않다.
+  function checkNotNullAnyBad(arg: any | null): any {
+    if (arg == null) {
+      throw new Error('not valid number!');
+    }
+    return arg;
+  }
+
+  function checkNoNull<GENERIC>(arg: GENERIC | null): GENERIC {
+    if (arg == null) {
+      throw new Error('not valid number!');
+    }
+    return arg;
+  }
+  const number = checkNoNull(123);
+  const string = checkNoNull('123');
+}
