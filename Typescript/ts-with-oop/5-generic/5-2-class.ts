3845e09f5ee2a44f49b6b4f5eda85e65b4197040
wonieeVicky <hwfongfing@gmail.com>
2024-01-03 00:36:26 +0900

    feat: update ts-with-oop-5-2-class(클래스 내 제네릭 활용 구현)

---
+++ Typescript/ts-with-oop/5-generic/5-2-class.ts
@@ -0,0 +1,44 @@
+﻿{
+  // Either: a or b
+  interface Either<L, R> {
+    left: () => L;
+    right: () => R;
+  }
+
+  class SimpleEither<L, R> implements Either<L, R> {
+    constructor(private leftValue: L, private rightValue: R) {}
+
+    left(): L {
+      return this.leftValue;
+    }
+
+    right(): R {
+      return this.rightValue;
+    }
+  }
+
+  const either: Either<number, number> = new SimpleEither(4, 5);
+  either.left(); // 4
+  either.right(); // 5
+  const best = new SimpleEither(3, 'vicky'); // const best: SimpleEither<number, string>
+  const name = new SimpleEither({ name: 'vicky' }, 'hello'); // const name: SimpleEither<{name: string}, string>
+
+  // interface Either {
+  //   left: () => number;
+  //   right: () => number;
+  // }
+
+  // class SimpleEither implements Either {
+  //   constructor(private leftValue: number, private rightValue: number) {}
+
+  //   left(): number {
+  //     return this.leftValue;
+  //   }
+
+  //   right(): number {
+  //     return this.rightValue;
+  //   }
+  // }
+
+  // const either = new SimpleEither(4, 5);
+}
