4497a5e2b9893d12dcefc16a6230fbcba6d53a28
wonieeVicky <hwfongfing@gmail.com>
2024-01-07 18:23:35 +0900

    feat: update ts-with-oop-6-1-array-api(read Array type definition API, overloading type, type-guard)

---
+++ Typescript/ts-with-oop/6-api/6-1-api.ts
@@ -0,0 +1,30 @@
+﻿type Student = {
+  passed: boolean;
+};
+
+const students: Student[] = [
+  {
+    passed: true
+  },
+  {
+    passed: true
+  },
+  {
+    passed: false
+  }
+];
+
+console.log(students.every((student) => student.passed)); // false
+
+class Animal {}
+class Cat extends Animal {
+  isCat: boolean = true;
+}
+class Dog extends Animal {
+  isDog: boolean = false;
+}
+const animals: Animal[] = [new Cat(), new Cat(), new Dog()];
+function isCat(animal: Animal): animal is Cat {
+  return (animal as Cat).isCat !== undefined;
+}
+animals.every<Cat>(isCat); // false
