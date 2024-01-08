03fc0873f2e3143cdb1de8e0d2536a20eb988c5f
wonieeVicky <hwfongfing@gmail.com>
2024-01-08 23:50:19 +0900

    feat: add ts-with-oop-5-7-exception - error와 exception의 차이. error의 개념

---
+++ Typescript/ts-with-oop/7-exception/7-1-exception.ts
@@ -0,0 +1,21 @@
+﻿// Java: Exception
+// Javascript: Error
+const array = new Array(100000000); //  RangeError
+
+function move(direction: 'up' | 'down' | 'left' | 'right' | 'he') {
+  switch (direction) {
+    case 'up':
+      break;
+    case 'down':
+      break;
+    case 'left':
+      break;
+    case 'right':
+      break;
+    case 'he':
+      break;
+    default:
+      const invalid: never = direction; // Ok
+      throw new Error('unknown direction: ' + invalid);
+  }
+}
