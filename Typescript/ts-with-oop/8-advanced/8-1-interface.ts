0f317945e2694c85989652eeaa1bd0a9c7b560b5
wonieeVicky <hwfongfing@gmail.com>
2024-01-15 00:00:38 +0900

    feat: add ts-with-oop-5-8-advanced(type alias와 interface 차이점)

---
+++ Typescript/ts-with-oop/8-advanced/8-1-interface.ts
@@ -0,0 +1,52 @@
+﻿type PositionType = {
+  x: number;
+  y: number;
+};
+
+interface PositionInterface {
+  x: number;
+  y: number;
+}
+
+// object ★
+const obj1: PositionType = {
+  x: 1,
+  y: 2
+};
+// object ★
+const obj12: PositionInterface = {
+  x: 4,
+  y: 5,
+  z: 1
+};
+
+// class ★
+class Pos1 implements PositionType {
+  x: number;
+  y: number;
+}
+class Pos2 implements PositionInterface {
+  x: number;
+  y: number;
+  z: number;
+}
+
+// Type, interface의 확장 Extends
+interface ZPositionInterface extends PositionInterface {
+  z: number;
+}
+type ZPositionType = PositionType & { z: number };
+
+// only interfaces can be merged.
+interface PositionInterface {
+  z: number;
+}
+
+// Type aliases can use computed properties
+type Person = {
+  name: string;
+  age: number;
+};
+type Name = Person['name']; // string type
+type NumberType = number; // type만 쓸 수 있는 특징
+type Direction = 'left' | 'right'; // union type
