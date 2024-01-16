c667d3577b07465c0881d68b67a4047107626e7e
wonieeVicky <hwfongfing@gmail.com>
2024-01-16 23:54:20 +0900

    feat: update ts-with-oop-5-8-advanced test code - index types

---
+++ Typescript/ts-with-oop/8-advanced/8-2-index.ts
@@ -0,0 +1,29 @@
+﻿{
+  const obj = {
+    name: 'vicky'
+  };
+  obj.name; // vicky
+  obj['name']; // vicky
+
+  type Animal = {
+    name: string;
+    age: number;
+    gender: 'male' | 'female';
+  };
+
+  type Name = Animal['name']; // string
+  const text: Name = 'hello'; // Ok!
+
+  type Gender = Animal['gender']; // male | female
+  type Keys = keyof Animal; // name | age | gender
+  const key: Keys = 'gender'; // Ok
+
+  type Person = {
+    name: string;
+    gender: Animal['gender'];
+  };
+  const person: Person = {
+    name: 'vicky',
+    gender: 'female'
+  };
+}
