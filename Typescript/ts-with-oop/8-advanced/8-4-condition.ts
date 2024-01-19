3c3bbd266e0eea2debb6850ec7ead28c68178670
wonieeVicky <hwfongfing@gmail.com>
2024-01-19 23:25:14 +0900

    feat: update ts-with-oop-5-8-advanced test code - conditional Type

---
+++ Typescript/ts-with-oop/8-advanced/8-4-condition.ts
@@ -0,0 +1,22 @@
+﻿type Check<T> = T extends string ? boolean : number;
+// 위와 같은 조건부 타입으로 만들 수 있다.
+type Type = Check<string>; // boolean
+type Type2 = Check<'aa'>; // boolean
+
+// conditional type은 조건부로 타입을 결정할 수 있는 타입을 의미함
+// T extends U ? X : Y
+type TypeName<T> = T extends string
+  ? 'string'
+  : T extends number
+  ? 'number'
+  : T extends boolean
+  ? 'boolean'
+  : T extends undefined
+  ? 'undefined'
+  : T extends Function
+  ? 'function'
+  : 'object';
+
+type T0 = TypeName<string>; // 'string' type
+type T1 = TypeName<'a'>; // 'string' type
+type T2 = TypeName<() => void>; // 'function' type
