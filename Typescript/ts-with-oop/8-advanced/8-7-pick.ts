fc1b0a9c443e180f85630ed63e41bd9bd16a6093
wonieeVicky <hwfongfing@gmail.com>
2024-01-20 23:37:11 +0900

    feat: update ts-with-oop-5-8-advanced test code - pick type

---
+++ Typescript/ts-with-oop/8-advanced/8-7-pick.ts
@@ -0,0 +1,28 @@
+﻿{
+  type Video = {
+    id: string;
+    title: string;
+    url: string;
+    data: string;
+  };
+
+  type VideoMetadata = Pick<Video, 'id' | 'title'>;
+
+  // data를 모두 가져오므로 무거운 함수
+  function getVideo(id: string): Video {
+    return {
+      id,
+      title: 'video',
+      url: 'https://..',
+      data: 'byte-data..'
+    };
+  }
+
+  // 기존 Video 타입에서 id와 title만 가져오는 함수
+  function getVideoMetadata(id: string): VideoMetadata {
+    return {
+      id,
+      title: 'title'
+    };
+  }
+}
