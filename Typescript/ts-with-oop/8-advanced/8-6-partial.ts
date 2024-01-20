c4a2f9e51e7d5d97036bc9b9a623d158eacc7de5
wonieeVicky <hwfongfing@gmail.com>
2024-01-20 23:33:33 +0900

    feat: update ts-with-oop-5-8-advanced test code - partial type

---
+++ Typescript/ts-with-oop/8-advanced/8-6-partial.ts
@@ -0,0 +1,28 @@
+﻿{
+  type Todo = {
+    title: string;
+    description: string;
+    label: string;
+    priority: 'high' | 'low';
+  };
+
+  function updateTodo(todo: Todo, fieldsToUpdated: Partial<Todo>): Todo {
+    return { ...todo, ...fieldsToUpdated };
+  }
+  const todo: Todo = {
+    title: 'learn TypeScript',
+    description: 'learn utility types',
+    label: 'study',
+    priority: 'high'
+  };
+  const updated = updateTodo(todo, { priority: 'low' });
+  console.log(updated);
+  /*
+    {
+      title: 'learn TypeScript',
+      description: 'learn utility types',
+      label: 'study',
+      priority: 'low'
+    }
+  */
+}
