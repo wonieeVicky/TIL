aadfc950a3644dfe336f8b0a1e16d14b8fa52d2b
wonieeVicky <hwfongfing@gmail.com>
2024-01-24 23:45:12 +0900

    feat: update ts-with-oop-5-9-javascript test code - js type module

---
+++ Typescript/ts-with-oop/9-javascript/9-3-module1.js
@@ -0,0 +1,9 @@
+﻿export function add(a, b) {
+  return a + b;
+}
+
+export function print() {
+  console.log('print');
+}
+
+export const number = 10;
---
+++ Typescript/ts-with-oop/9-javascript/9-3-module2.js
@@ -0,0 +1,4 @@
+﻿import * as calculator from './9-3-module1.js';
+console.log(calculator.add(1, 2));
+calculator.print();
+calculator.number; // 10
