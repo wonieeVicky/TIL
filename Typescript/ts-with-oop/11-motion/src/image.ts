5a3f879916fa8a49838bb7ca1c49a2c8f6d0996a
wonieeVicky <hwfongfing@gmail.com>
2024-02-08 00:02:24 +0900

    feat: ModalImage 생성자 내 addClickEvent, drawModal public event 구현

--- Typescript/ts-with-oop/11-motion/src/image.ts
+++ Typescript/ts-with-oop/11-motion/src/image.ts
@@ -7,4 +7,34 @@
       '<button class="image" id="MotionBtn">IMAGE</button>';
     return new MotionImage();
   }
+
+  addClickEvent() {
+    setTimeout(() => {
+      const $btn = document.querySelector(
+        '#MotionBtn.image'
+      ) as HTMLButtonElement;
+      $btn?.addEventListener('click', this.drawModal);
+    }, 1000);
+  }
+
+  drawModal() {
+    const $modalContent = document.createElement('div');
+    $modalContent.className = 'modalInner';
+    $modalContent.innerHTML = `
+      <div>
+        <label for="title">Title</label>
+        <input type="text" name="title" />
+      </div>
+      <div>
+        <label for="url">Url</label>
+        <input type="text" name="url" />
+      </div>
+      <div>
+        <button class="add">ADD</button>
+      </div>
+    `;
+    document.querySelector('#MotionModal')!.appendChild($modalContent);
+    const modal = document.querySelector('#MotionModal') as HTMLElement;
+    modal.classList.add('active');
+  }
 }
