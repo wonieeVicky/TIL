7bfa757a0e04808bac7a232366bc129113f39c86
wonieeVicky <hwfongfing@gmail.com>
2024-02-10 23:46:35 +0900

    fix: class 구조 변경

--- Typescript/ts-with-oop/11-motion/src/motion.ts
+++ Typescript/ts-with-oop/11-motion/src/motion.ts
@@ -1,7 +1,38 @@
-﻿window.addEventListener('load', () => {
-  const image = MotionImage.render().addClickEvent();
+﻿type MotionData = {};
 
-  const video = MotionVideo.render();
-  const note = MotionNote.render();
-  const task = MotionTask.render();
-});
+class MotionFunction {
+  title: string = '';
+  url: string = '';
+  type: string = ''; // image, video, note, task
+
+  constructor(type: string) {
+    this.type = type;
+  }
+
+  addClickEvent() {}
+
+  drawModal() {
+    const $modalContent = document.createElement('div');
+    $modalContent.className = 'modalInner';
+    $modalContent.innerHTML = `
+      <div>
+        <label for="title">Title</label>
+        <input type="text" name="title" />
+      </div>
+      <div>
+        <label for="url">Url</label>
+        <input type="text" name="url" />
+      </div>
+      <div>
+        <button class="add">ADD</button>
+      </div>
+    `;
+    document.querySelector('#MotionModal')!.appendChild($modalContent);
+    const modal = document.querySelector('#MotionModal') as HTMLElement;
+    modal.classList.add('active');
+  }
+}
+
+{
+  const image = new MotionFunction('image');
+}
