5a44cf766aa94f6b859faa5f28faf8679690ad1a
wonieeVicky <hwfongfing@gmail.com>
2024-02-23 23:53:09 +0900

    feat: App 클래스에 PageComponent 적용

---
+++ Typescript/ts-with-oop/11-motion-rebuild/src/app.ts
@@ -0,0 +1,11 @@
+﻿import { PageComponent } from './components/page.js';
+
+class App {
+  private readonly page: PageComponent;
+  constructor(appRoot: HTMLElement) {
+    this.page = new PageComponent();
+    this.page.attachTo(appRoot);
+  }
+}
+
+new App(document.querySelector('.document')! as HTMLElement);
