3724315b68daf464e9590ad33d84b2253b176083
wonieeVicky <hwfongfing@gmail.com>
2024-02-24 21:47:43 +0900

    feat: ImageComponent 구현 및 App 클래스 내 적용

---
+++ Typescript/ts-with-oop/11-motion-rebuild/src/components/page/item/image.ts
@@ -0,0 +1,34 @@
+﻿/**
+ * ImageComponent
+ */
+export class ImageComponent {
+  private element: HTMLElement;
+
+  constructor(readonly title: string, readonly url: string) {
+    const template = document.createElement('template');
+    template.innerHTML = `
+      <section class="image">
+        <div class="image__holder">
+          <img class="image__thumbnail" />
+        </div>
+        <p class="image__title"></p>
+      </section>`;
+
+    // template.innerHTML로 데이터를 바로 주입하지 않고 필요한 부분만 아래처럼 업데이트 해준다.
+    this.element = template.content.firstElementChild! as HTMLElement;
+    const imageElement = this.element.querySelector(
+      '.image__thumbnail'
+    )! as HTMLImageElement;
+    imageElement.src = url;
+    imageElement.alt = title;
+
+    const titleElement = this.element.querySelector(
+      '.image__title'
+    )! as HTMLParagraphElement;
+    titleElement.textContent = title;
+  }
+
+  attachTo(parent: HTMLElement, position: InsertPosition = 'afterbegin') {
+    parent.insertAdjacentElement(position, this.element);
+  }
+}
