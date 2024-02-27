57356e904b2387664fd186e7d35c622b8b4b659e
wonieeVicky <hwfongfing@gmail.com>
2024-02-28 00:01:47 +0900

    fix: convertToEmbeddedURL 함수 정규표현식으로 처리하도록 개선

--- Typescript/ts-with-oop/11-motion-rebuild/src/components/page/item/video.ts
+++ Typescript/ts-with-oop/11-motion-rebuild/src/components/page/item/video.ts
@@ -1,12 +1,15 @@
 ﻿import { BaseComponent } from '../../component.js';
 
-function generateEmbedUrl(url: string) {
-  if (url.includes('/embed/')) {
-    return url;
+function convertToEmbeddedURL(url: string) {
+  const regExp =
+    /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9-]{11}))|(?:youtu.be\/([a-zA-Z0-9-]{11})))/;
+  const match = url.match(regExp);
+  const videoId = match ? match[1] || match[2] : undefined;
+
+  if (videoId) {
+    return `https://www.youtube.com/embed/${videoId}`;
   }
-  const videoId = url.split('v=')[1];
-  const embedUrl = `https://www.youtube.com/embed/${videoId}`;
-  return embedUrl;
+  return url;
 }
 
 /**
@@ -25,7 +28,7 @@ export class VideoComponent extends BaseComponent<HTMLElement> {
     const videoElement = this.element.querySelector(
       '.video__iframe'
     )! as HTMLIFrameElement;
-    videoElement.src = generateEmbedUrl(url);
+    videoElement.src = convertToEmbeddedURL(url);
 
     const titleElement = this.element.querySelector(
       '.video__title'
