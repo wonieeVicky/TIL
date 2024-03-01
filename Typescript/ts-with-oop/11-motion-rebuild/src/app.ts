484d0cb45fb95e8fa820449771dd8d57fc7415f9
wonieeVicky <hwfongfing@gmail.com>
2024-03-01 23:38:58 +0900

    feat: App 내 page 타이핑 변경, class 별 attach element 방식 변경(attachTo > this.page.addChild)

--- Typescript/ts-with-oop/11-motion-rebuild/src/app.ts
+++ Typescript/ts-with-oop/11-motion-rebuild/src/app.ts
@@ -1,11 +1,12 @@
-﻿import { ImageComponent } from './components/page/item/image.js';
+﻿import { Component } from './components/component.js';
+import { ImageComponent } from './components/page/item/image.js';
 import { NoteComponent } from './components/page/item/note.js';
 import { TodoComponent } from './components/page/item/todo.js';
 import { VideoComponent } from './components/page/item/video.js';
-import { PageComponent } from './components/page/page.js';
+import { Composable, PageComponent } from './components/page/page.js';
 
 class App {
-  private readonly page: PageComponent;
+  private readonly page: Component & Composable;
   constructor(appRoot: HTMLElement) {
     this.page = new PageComponent();
     this.page.attachTo(appRoot);
@@ -14,19 +15,20 @@ class App {
       'Image Title',
       'https://picsum.photos/600/300'
     );
-    image.attachTo(appRoot, 'beforeend');
+    this.page.addChild(image);
 
     const video = new VideoComponent(
       'Video Title',
       'https://www.youtube.com/watch?v=HfaIcB4Ogxk'
     );
     video.attachTo(appRoot, 'beforeend');
+    this.page.addChild(video);
 
     const note = new NoteComponent('Note Title', 'This is a simple note');
-    note.attachTo(appRoot, 'beforeend');
+    this.page.addChild(note);
 
     const todo = new TodoComponent('Todo Title', 'This is a simple todo item');
-    todo.attachTo(appRoot, 'beforeend');
+    this.page.addChild(todo);
   }
 }
 
