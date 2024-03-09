29f2a8909c5fd2c736c6738cc7e71df6a0dd2b63
wonieeVicky <hwfongfing@gmail.com>
2024-03-09 22:49:31 +0900

    feat: InputDialog 컴포넌트 구현, app 내 image 버튼 내 적용

--- Typescript/ts-with-oop/11-motion-rebuild/src/app.ts
+++ Typescript/ts-with-oop/11-motion-rebuild/src/app.ts
@@ -1,4 +1,5 @@
 ﻿import { Component } from './components/component.js';
+import { InputDialog } from './components/dialog/dialog.js';
 import { ImageComponent } from './components/page/item/image.js';
 import { NoteComponent } from './components/page/item/note.js';
 import { TodoComponent } from './components/page/item/todo.js';
@@ -33,6 +34,19 @@ class App {
 
     const todo = new TodoComponent('Todo Title', 'This is a simple todo item');
     this.page.addChild(todo);
+
+    const imageBtn = document.querySelector('#new-image')! as HTMLButtonElement;
+    imageBtn.addEventListener('click', () => {
+      const dialog = new InputDialog();
+      dialog.setOnCloseListener(() => {
+        dialog.removeFrom(document.body);
+      });
+      dialog.setOnSubmitListener(() => {
+        // TODO: 섹션 구현 후 페이지 추가
+        dialog.removeFrom(document.body);
+      });
+      dialog.attachTo(document.body);
+    });
   }
 }
 
---
+++ Typescript/ts-with-oop/11-motion-rebuild/src/components/dialog/dialog.ts
@@ -0,0 +1,47 @@
+﻿import { BaseComponent, Component } from '../../components/component.js';
+import { Composable } from '../page/page';
+
+type OnCloseListener = () => void;
+type OnSubmitListener = () => void;
+
+export class InputDialog
+  extends BaseComponent<HTMLElement>
+  implements Composable
+{
+  private closeListener?: OnCloseListener;
+  private submitListener?: OnSubmitListener;
+
+  constructor() {
+    super(`<section class="dialog">
+            <div class="dialog__container">
+              <button class="close">&times;</button>
+              <div id="dialog__body"></div>
+              <button class="dialog__submit">ADD</button>
+            </div>
+          </section>`);
+    const closeBtn = this.element.querySelector('.close')! as HTMLElement;
+
+    // 버튼을 다른 곳에서 활용할 경우 addEventListener를 사용하는 것이 좋다.
+    closeBtn.onclick = () => {
+      this.closeListener && this.closeListener();
+    };
+    const submitBtn = this.element.querySelector(
+      '.dialog__submit'
+    )! as HTMLElement;
+    submitBtn.onclick = () => {
+      this.submitListener && this.submitListener();
+    };
+  }
+
+  setOnCloseListener(listener: OnCloseListener): void {
+    this.closeListener = listener;
+  }
+  setOnSubmitListener(listener: OnSubmitListener): void {
+    this.submitListener = listener;
+  }
+
+  addChild(child: Component): void {
+    const body = this.element.querySelector('#dialog__body')! as HTMLElement;
+    child.attachTo(body);
+  }
+}
