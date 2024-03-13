e6abd2d0f8cae0f28333fbbf1ddb173ec5e75876
wonieeVicky <hwfongfing@gmail.com>
2024-03-13 23:57:00 +0900

    feat: MediaSectionInput 컴포넌트 구현, app 내 image button 내 적용

--- Typescript/ts-with-oop/11-motion-rebuild/src/app.ts
+++ Typescript/ts-with-oop/11-motion-rebuild/src/app.ts
@@ -1,10 +1,10 @@
 ﻿import { Component } from './components/component.js';
 import { InputDialog } from './components/dialog/dialog.js';
-import { InputDialogContent } from './components/dialog/dialogContent.js';
+import { MediaSectionInput } from './components/dialog/input/media-input.js';
 import { ImageComponent } from './components/page/item/image.js';
-import { NoteComponent } from './components/page/item/note.js';
-import { TodoComponent } from './components/page/item/todo.js';
-import { VideoComponent } from './components/page/item/video.js';
+// import { NoteComponent } from './components/page/item/note.js';
+// import { TodoComponent } from './components/page/item/todo.js';
+// import { VideoComponent } from './components/page/item/video.js';
 import {
   Composable,
   PageComponent,
@@ -13,70 +13,31 @@ import {
 
 class App {
   private readonly page: Component & Composable;
-  constructor(appRoot: HTMLElement) {
+
+  constructor(appRoot: HTMLElement, dialogRoot: HTMLElement) {
     this.page = new PageComponent(PageItemComponent);
     this.page.attachTo(appRoot);
 
     const imageBtn = document.querySelector('#new-image')! as HTMLButtonElement;
     imageBtn.addEventListener('click', () => {
       const dialog = new InputDialog();
-      const image = new InputDialogContent('image');
-      dialog.addChild(image);
-      dialog.setOnCloseListener(() => dialog.removeFrom(document.body));
+      const inputSection = new MediaSectionInput();
+      dialog.addChild(inputSection);
+      dialog.attachTo(dialogRoot);
+
+      dialog.setOnCloseListener(() => {
+        dialog.removeFrom(dialogRoot);
+      });
       dialog.setOnSubmitListener(() => {
-        const { title, value } = image.getValues();
-        const imageComponent = new ImageComponent(title, value);
+        const imageComponent = new ImageComponent(
+          inputSection.title,
+          inputSection.url
+        );
         this.page.addChild(imageComponent);
-        dialog.removeFrom(document.body);
+        dialog.removeFrom(dialogRoot);
       });
-      dialog.attachTo(document.body);
-    });
-
-    const videoBtn = document.querySelector('#new-video')! as HTMLButtonElement;
-    videoBtn.addEventListener('click', () => {
-      const dialog = new InputDialog();
-      const video = new InputDialogContent('video');
-      dialog.addChild(video);
-      dialog.setOnCloseListener(() => dialog.removeFrom(document.body));
-      dialog.setOnSubmitListener(() => {
-        const { title, value } = video.getValues();
-        const videoComponent = new VideoComponent(title, value);
-        this.page.addChild(videoComponent);
-        dialog.removeFrom(document.body);
-      });
-      dialog.attachTo(document.body);
-    });
-
-    const noteBtn = document.querySelector('#new-note')! as HTMLButtonElement;
-    noteBtn.addEventListener('click', () => {
-      const dialog = new InputDialog();
-      const note = new InputDialogContent('note');
-      dialog.addChild(note);
-      dialog.setOnCloseListener(() => dialog.removeFrom(document.body));
-      dialog.setOnSubmitListener(() => {
-        const { title, value } = note.getValues();
-        const videoComponent = new NoteComponent(title, value);
-        this.page.addChild(videoComponent);
-        dialog.removeFrom(document.body);
-      });
-      dialog.attachTo(document.body);
-    });
-
-    const todoBtn = document.querySelector('#new-todo')! as HTMLButtonElement;
-    todoBtn.addEventListener('click', () => {
-      const dialog = new InputDialog();
-      const todo = new InputDialogContent('todo');
-      dialog.addChild(todo);
-      dialog.setOnCloseListener(() => dialog.removeFrom(document.body));
-      dialog.setOnSubmitListener(() => {
-        const { title, value } = todo.getValues();
-        const videoComponent = new TodoComponent(title, value);
-        this.page.addChild(videoComponent);
-        dialog.removeFrom(document.body);
-      });
-      dialog.attachTo(document.body);
     });
   }
 }
 
-new App(document.querySelector('.document')! as HTMLElement);
+new App(document.querySelector('.document')! as HTMLElement, document.body);
---
+++ Typescript/ts-with-oop/11-motion-rebuild/src/components/dialog/input/media-input.ts
@@ -0,0 +1,26 @@
+﻿import { BaseComponent } from '../../component.js';
+
+export class MediaSectionInput extends BaseComponent<HTMLElement> {
+  constructor() {
+    super(`<div>
+            <div class="form__container">
+              <label for="title">Title</label>
+              <input type="text" id="title" />
+            </div>
+            <div class="form__container">
+              <label for="url">URL</label>
+              <input type="text" id="url" />
+            </div>
+          </div>`);
+  }
+
+  // getter
+  get url(): string {
+    const element = this.element.querySelector('#url')! as HTMLInputElement;
+    return element.value;
+  }
+  get title(): string {
+    const element = this.element.querySelector('#title')! as HTMLInputElement;
+    return element.value;
+  }
+}
