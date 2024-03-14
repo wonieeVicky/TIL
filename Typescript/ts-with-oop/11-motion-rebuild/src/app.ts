1a618baca32502a875f220bee1dab8fc1d3abd9c
wonieeVicky <hwfongfing@gmail.com>
2024-03-14 23:19:19 +0900

    feat: TextSectionInput 컴포넌트 구현, app 내 note, todo 내 적용

--- Typescript/ts-with-oop/11-motion-rebuild/src/app.ts
+++ Typescript/ts-with-oop/11-motion-rebuild/src/app.ts
@@ -2,14 +2,15 @@
 import { InputDialog } from './components/dialog/dialog.js';
 import { MediaSectionInput } from './components/dialog/input/media-input.js';
 import { ImageComponent } from './components/page/item/image.js';
-// import { NoteComponent } from './components/page/item/note.js';
-// import { TodoComponent } from './components/page/item/todo.js';
-// import { VideoComponent } from './components/page/item/video.js';
+import { VideoComponent } from './components/page/item/video.js';
+import { NoteComponent } from './components/page/item/note.js';
+import { TodoComponent } from './components/page/item/todo.js';
 import {
   Composable,
   PageComponent,
   PageItemComponent
 } from './components/page/page.js';
+import { TextSectionInput } from './components/dialog/input/text-input.js';
 
 class App {
   private readonly page: Component & Composable;
@@ -34,6 +35,66 @@ class App {
           inputSection.url
         );
         this.page.addChild(imageComponent);
+        dialog.removeFrom(dialogRoot);
+      });
+    });
+
+    const videoBtn = document.querySelector('#new-video')! as HTMLButtonElement;
+    videoBtn.addEventListener('click', () => {
+      const dialog = new InputDialog();
+      const inputSection = new MediaSectionInput();
+      dialog.addChild(inputSection);
+      dialog.attachTo(dialogRoot);
+
+      dialog.setOnCloseListener(() => {
+        dialog.removeFrom(dialogRoot);
+      });
+      dialog.setOnSubmitListener(() => {
+        const videoComponent = new VideoComponent(
+          inputSection.title,
+          inputSection.url
+        );
+        this.page.addChild(videoComponent);
+        dialog.removeFrom(dialogRoot);
+      });
+    });
+
+    const noteBtn = document.querySelector('#new-note')! as HTMLButtonElement;
+    noteBtn.addEventListener('click', () => {
+      const dialog = new InputDialog();
+      const inputSection = new TextSectionInput();
+      dialog.addChild(inputSection);
+      dialog.attachTo(dialogRoot);
+
+      dialog.setOnCloseListener(() => {
+        dialog.removeFrom(dialogRoot);
+      });
+      dialog.setOnSubmitListener(() => {
+        const noteComponent = new NoteComponent(
+          inputSection.title,
+          inputSection.body
+        );
+        this.page.addChild(noteComponent);
+        dialog.removeFrom(dialogRoot);
+      });
+    });
+
+    const todoBtn = document.querySelector('#new-todo')! as HTMLButtonElement;
+    todoBtn.addEventListener('click', () => {
+      const dialog = new InputDialog();
+      const inputSection = new TextSectionInput();
+      dialog.addChild(inputSection);
+      dialog.attachTo(dialogRoot);
+
+      dialog.setOnCloseListener(() => {
+        dialog.removeFrom(dialogRoot);
+      });
+      dialog.setOnSubmitListener(() => {
+        const todoComponent = new TodoComponent(
+          inputSection.title,
+          inputSection.body
+        );
+        this.page.addChild(todoComponent);
         dialog.removeFrom(dialogRoot);
       });
     });
---
+++ Typescript/ts-with-oop/11-motion-rebuild/src/components/dialog/input/text-input.ts
@@ -0,0 +1,26 @@
+﻿import { BaseComponent } from '../../component';
+
+export class TextSectionInput extends BaseComponent<HTMLElement> {
+  constructor() {
+    super(`<div>
+            <div class="form__container">
+              <label for="title">Title</label>
+              <input type="text" id="title" />
+            </div>
+            <div class="form__container">
+              <label for="body">Body</label>
+              <textarea type="text" row="3" id="body" />
+            </div>
+          </div>`);
+  }
+
+  // getter
+  get body(): string {
+    const element = this.element.querySelector('#body')! as HTMLInputElement;
+    return element.value;
+  }
+  get title(): string {
+    const element = this.element.querySelector('#title')! as HTMLInputElement;
+    return element.value;
+  }
+}
