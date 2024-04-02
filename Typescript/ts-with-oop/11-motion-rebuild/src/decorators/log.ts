35da3856759eb307d8092d6479b842c057e26e21
wonieeVicky <hwfongfing@gmail.com>
2024-04-02 23:31:53 +0900

    feat: decorator 사용 예시 log.ts 추가 및 테스트 코드 구현

---
+++ Typescript/ts-with-oop/11-motion-rebuild/src/decorators/log.ts
@@ -0,0 +1,30 @@
+﻿function Log(
+  _: any,
+  name: string,
+  descriptor: PropertyDescriptor
+): PropertyDescriptor {
+  const newDescriptor = {
+    ...descriptor,
+    value: function (...args: any[]): any {
+      console.log(`Calling ${name} with arguments.`);
+      const start = performance.now();
+      const result = descriptor.value.apply(this, args);
+      const end = performance.now();
+      console.log(`${name} (${args}) took ${end - start} ms`);
+      console.log(`result: ${result}`);
+      return result;
+    }
+  };
+
+  return newDescriptor;
+}
+
+class Calculator {
+  @Log
+  add(x: number, y: number) {
+    return x + y;
+  }
+}
+
+const calculator = new Calculator();
+console.log(calculator.add(2, 3));
