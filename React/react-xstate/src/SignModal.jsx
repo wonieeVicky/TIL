8ae9c49fbf0f2e7b8307f6ac169e96ca6e591e45
wonieeVicky <hwfongfing@gmail.com>
2024-04-16 00:04:32 +0900

    feat: 인증 도메인 xstate 상태 정의 구현체 추가, SignModal 컴포넌트 추가

---
+++ React/react-xstate/src/SignModal.jsx
@@ -0,0 +1,45 @@
+﻿import { useMachine } from "@xstate/react";
+import { signMachine } from "./signMachine";
+
+export const SignModal = () => {
+  const [state, send, service] = useMachine(signMachine);
+  const { value: signState, history } = state;
+  const openState = signState !== "done" && !!history;
+  const { isLoading: loadingGithub, mutate: signinGithub } =
+    useSigninWithProvider("GITHUB", send);
+  const { isLoading: loadingGoogle, mutate: signinGoogle } =
+    useSigninWithProvider("GOOGLE", send);
+
+  return (
+    <Modal
+      open={openState}
+      hasCloseIcon
+      onClose={() => {
+        send("CLEAR");
+      }}
+    >
+      <Modal.Trigger>
+        <Button variant="outline" color="primary-500">
+          로그인
+        </Button>
+      </Modal.Trigger>
+      <Modal.Header>로그인</Modal.Header>
+      <Modal.Body>
+        {signState === "selection" && ( //selection 상태일 때에만 로그인 선택 UI 표현
+          <Stack direction="vertical">
+            <Button onClick={() => send("EMAIL")}>Email로 로그인</Button>
+            <Button onClick={() => signinGithub()}>
+              GitHub 계정으로 로그인
+            </Button>
+            <Button onClick={() => signinGoogle()}>
+              Google 계정으로 로그인
+            </Button>
+          </Stack>
+        )}
+        {signState === "email" && (
+          <EmailPasswordForm signMachine={service} signup={false} />
+        )}
+      </Modal.Body>
+    </Modal>
+  );
+};
---
+++ React/react-xstate/src/signMachine.js
@@ -0,0 +1,73 @@
+﻿import { useMachine } from "@xstate/react";
+import { createMachine } from "xstate";
+
+/**
+ * 인증 도메인 상태 정의
+ * selection > email or oauth > registry > done
+ */
+export const signMachine = createMachine({
+  id: "sign",
+  initial: "selection",
+  context: {
+    user: null,
+  },
+  states: {
+    // 인증 유형 선택
+    selection: {
+      on: {
+        REGISTRY: {
+          target: "registry",
+          actions: "updateUser",
+        },
+        OAUTH: {
+          target: "oauth",
+        },
+        EMAIL: {
+          target: "email",
+        },
+      },
+    },
+    // 이메일 인증
+    email: {
+      on: {
+        CLEAR: {
+          target: "selection",
+        },
+        REGISTRY: {
+          target: "registry",
+          actions: "updateUser",
+        },
+        DONE: {
+          target: "done",
+        },
+      },
+    },
+    // 소셜 로그인
+    oauth: {
+      on: {
+        CLEAR: {
+          target: "selection",
+        },
+      },
+    },
+    // 등록 완료(이메일 등록 후 서버에 사용자 정보가 등록된 상태)
+    registry: {
+      on: {
+        CLEAR: {
+          target: "done",
+        },
+        BACK: {
+          target: "email",
+        },
+      },
+    },
+    // 초기화
+    done: {
+      type: "final",
+    },
+  },
+});
+
+const [state, send, service] = useMachine(signMachine);
+const currentState = state.value;
+const onSend = () => send("REGISTRY");
