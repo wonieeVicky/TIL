820d8efe8c6b205e682dada50baa1b0c0f574d5f
wonieeVicky <hwfongfing@gmail.com>
2024-04-17 23:45:28 +0900

    feat: SignPage 네 signMachineContext 적용

---
+++ React/react-xstate/src/SignPage.jsx
@@ -0,0 +1,15 @@
+﻿import { useMemo } from "react";
+import { SigninModal } from "./SigninModal";
+import { SignupModal } from "./SignupModal";
+import { signMachineContext } from "../machines/signMachineContext";
+
+const SignPage = (props) => {
+  return (
+    <signMachineContext.Provider>
+      <SigninModal />
+      <SignupModal />
+    </signMachineContext.Provider>
+  );
+};
+
+export default SignPage;
