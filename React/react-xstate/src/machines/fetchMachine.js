366b8f4707b9ce47a502666a871ffb9aa2cddb58
wonieeVicky <hwfongfing@gmail.com>
2024-04-19 22:59:12 +0900

    feat: fetchMachine 신규 정의 추가

---
+++ React/react-xstate/src/machines/fetchMachine.js
@@ -0,0 +1,28 @@
+﻿// createMachine 팩토리 함수를 통해 FSM 및 Statechart를 정의
+const fetchMachine = createMachine({
+  id: "fetch",
+  initial: "idle",
+  state: {
+    idle: {
+      on: {
+        FETCHING: {
+          target: "loading",
+        },
+      },
+    },
+    loading: {
+      on: {
+        SUCCESS: {
+          target: "resolved",
+        },
+        FAILURE: {
+          target: "rejected",
+        },
+      },
+    },
+    resolved: {
+      type: "final",
+    },
+    rejected: {},
+  },
+});
