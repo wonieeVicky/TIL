e3d3ea0129e6f1f20470aa8ac97c7faf184a2fe7
wonieeVicky <hwfongfing@gmail.com>
2024-04-21 23:40:14 +0900

    feat: xstate-playground(Join 페이지 내 useMachine으로 상태 관리 구현)

---
+++ React/react-xstate/src/Join.jsx
@@ -0,0 +1,54 @@
+﻿import { useMachine } from "@xstate/react";
+import { useState } from "react";
+import { fetchMachine } from "./machines/fetchMachine";
+
+export default Join = () => {
+  const [state, send] = useMachine(fetchMachine);
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+  const [invalidPassword, setInvalidPassword] = useState(false);
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    send("FETCHING");
+    if (error) {
+      setError(null);
+    }
+    try {
+      await signUp({ id, password });
+      send("SUCCESS");
+    } catch (e) {
+      setError(e);
+      send("FAILURE");
+    }
+  };
+
+  const handleChangeId = (e) => setId(e.target.value);
+
+  const handleChangePassword = (e) => {
+    const pwd = e.target.value;
+    setInvalidPassword(pwd.length < 8);
+    setPassword(pwd);
+  };
+
+  const isDisabled = state.matches("loading") || invalidPassword;
+
+  return (
+    <div className="app">
+      <h1>회원가입</h1>
+      <form onSubmit={handleSubmit}>
+        <label>id</label>
+        <input id="id" onChange={handleChangeId} />
+        <label htmlFor={"password"}>password</label>
+        <input id="password" type="password" onChange={handleChangePassword} />
+        <button disabled={isDisabled} type="submit">
+          OK
+        </button>
+      </form>
+      {state.matches("loading") && <p>Loading...</p>}
+      {state.matches("resolved") && <p>회원가입에 성공했습니다.</p>}
+      {state.matches("rejected") && <p>{error}</p>}
+    </div>
+  );
+};
