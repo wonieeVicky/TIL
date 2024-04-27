02156f6eb426218976f9e68691048e74f889dd63
wonieeVicky <hwfongfing@gmail.com>
2024-04-27 23:58:43 +0900

    feat: xstate-playground(Orthogonal(병렬, 직렬) states 를 위한 parallel type 테스트 machine 구현)

---
+++ React/react-xstate/src/machines/fileMachine.js
@@ -0,0 +1,13 @@
+﻿const fileMachine = createMachine({
+  // ..
+  type: "parallel",
+  // initial: ?? // parallel state는 initial state를 가질 수 없다.
+  states: {
+    upload: {
+      initial: "idle",
+    },
+    download: {
+      initial: "idle",
+    },
+  },
+});
