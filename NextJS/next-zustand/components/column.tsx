ec5ad3c4cfbe35643b062a5a9e794f79d3a8de18
wonieeVicky <hwfongfing@gmail.com>
2024-05-04 23:42:17 +0900

    feat: next-zustand - column 내 useTaskStore 로 tasks 정보 호출

--- NextJS/next-zustand/components/column.tsx
+++ NextJS/next-zustand/components/column.tsx
@@ -1,34 +1,31 @@
-import Task from './task'
-
-const tasks = [
-  {
-    id: '1234',
-    title: 'Our first task',
-    description: 'Some description',
-    status: 'DONE'
-  }
-]
+import { useTaskStore } from '@/lib/store';
+import Task from './task';
+import { useMemo } from 'react';
 
 export default function Column({
   title,
   status
 }: {
-  title: string
-  status: string
+  title: string;
+  status: string;
 }) {
-  const filteredTasks = tasks.filter(task => task.status === status)
+  const tasks = useTaskStore((state) => state.tasks);
+  const filteredTasks = useMemo(
+    () => tasks.filter((task) => task.status === status),
+    [tasks, status]
+  );
 
   return (
-    <section className='h-[600px] flex-1'>
-      <h2 className='ml-1 font-serif text-2xl font-semibold'>{title}</h2>
+    <section className="h-[600px] flex-1">
+      <h2 className="ml-1 font-serif text-2xl font-semibold">{title}</h2>
 
-      <div className='mt-3.5 h-full w-full flex-1 rounded-xl bg-gray-700/50 p-4'>
-        <div className='flex flex-col gap-4'>
-          {filteredTasks.map(task => (
+      <div className="mt-3.5 h-full w-full flex-1 rounded-xl bg-gray-700/50 p-4">
+        <div className="flex flex-col gap-4">
+          {filteredTasks.map((task) => (
             <Task key={task.id} {...task} />
           ))}
         </div>
       </div>
     </section>
-  )
+  );
 }
