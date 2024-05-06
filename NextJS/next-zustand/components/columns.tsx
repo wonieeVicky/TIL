e7cdce3da882e57f85dc7526dbe96656a1147332
wonieeVicky <hwfongfing@gmail.com>
2024-05-07 00:07:21 +0900

    feat: next-zustand - NewTodoDialog Form 컴포넌트 구현

--- NextJS/next-zustand/components/columns.tsx
+++ NextJS/next-zustand/components/columns.tsx
@@ -1,11 +1,15 @@
-import Column from './column'
+import Column from './column';
+import NewTodoDialog from './new-todo-dialog';
 
 export default function Columns() {
   return (
-    <section className='mt-10 flex gap-6 lg:gap-12'>
-      <Column title='Todo' status='TODO' />
-      <Column title='In Progress' status='IN_PROGRESS' />
-      <Column title='Done' status='DONE' />
-    </section>
-  )
+    <div>
+      <NewTodoDialog />
+      <section className="mt-10 flex gap-6 lg:gap-12">
+        <Column title="Todo" status="TODO" />
+        <Column title="In Progress" status="IN_PROGRESS" />
+        <Column title="Done" status="DONE" />
+      </section>
+    </div>
+  );
 }
