83228153f157df9194f6a0d4dcdfcc6d47a92357
wonieeVicky <hwfongfing@gmail.com>
2024-05-07 23:31:21 +0900

    feat: next-zustand - column, task 내 드래그앤드롭 event 반영

--- NextJS/next-zustand/components/column.tsx
+++ NextJS/next-zustand/components/column.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useTaskStore } from '@/lib/store';
+import { Status, useTaskStore } from '@/lib/store';
 import Task from './task';
 import { useMemo } from 'react';
 
@@ -8,7 +8,7 @@ export default function Column({
   status
 }: {
   title: string;
-  status: string;
+  status: Status;
 }) {
   const tasks = useTaskStore((state) => state.tasks);
   const filteredTasks = useMemo(
@@ -16,11 +16,26 @@ export default function Column({
     [tasks, status]
   );
 
+  const updateTask = useTaskStore((state) => state.updateTask);
+  const draggedTask = useTaskStore((state) => state.draggedTask);
+  const dragTask = useTaskStore((state) => state.dragTask);
+
+  const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    if (!draggedTask) return;
+    updateTask(draggedTask, status);
+    dragTask(null);
+  };
+
   return (
     <section className="h-[600px] flex-1">
       <h2 className="ml-1 font-serif text-2xl font-semibold">{title}</h2>
-
-      <div className="mt-3.5 h-full w-full flex-1 rounded-xl bg-gray-700/50 p-4">
+      <div
+        className="mt-3.5 h-full w-full flex-1 rounded-xl bg-gray-700/50 p-4"
+        // drag drop 시점에 진행
+        onDrop={handleDrop}
+        // drag over 인 경우 모두 호출되므로 prevent
+        onDragOver={(e) => e.preventDefault()}
+      >
         <div className="flex flex-col gap-4">
           {filteredTasks.map((task) => (
             <Task key={task.id} {...task} />
--- NextJS/next-zustand/components/task.tsx
+++ NextJS/next-zustand/components/task.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { cn } from '@/lib/utils';
+import { useTaskStore } from '@/lib/store';
 
 export default function Task({
+  id,
   title,
   description,
   status
 }: {
+  id: string;
   title: string;
   description?: string;
   status: string;
 }) {
+  const dragTask = useTaskStore((state) => state.dragTask);
   return (
     <div
       className={cn(
@@ -20,6 +24,8 @@ export default function Task({
           'border-2 border-emerald-500': status === 'DONE'
         }
       )}
+      draggable
+      onDrag={() => dragTask(id)}
     >
       <div>
         <h3 className="font-medium text-gray-700">{title}</h3>
