6d6b91a64315e2ce439885ae95e7091185d328e5
wonieeVicky <hwfongfing@gmail.com>
2024-05-08 23:43:04 +0900

    feat: next-zustand - store 내 persist middleware 적용(localstorage 내 저장)

--- NextJS/next-zustand/lib/store.ts
+++ NextJS/next-zustand/lib/store.ts
@@ -1,5 +1,6 @@
 ﻿import { create } from 'zustand';
 import { v4 as uuid } from 'uuid';
+import { persist } from 'zustand/middleware';
 
 export type Status = 'TODO' | 'IN_PROGRESS' | 'DONE';
 
@@ -22,30 +23,35 @@ export type Actions = {
   updateTask: (id: string, status: Status) => void;
 };
 
-export const useTaskStore = create<State & Actions>()((set) => ({
-  tasks: [],
-  draggedTask: null,
-  addTask: (title: string, description?: string) =>
-    set((state) => ({
-      tasks: [
-        ...state.tasks,
-        {
-          id: uuid(),
-          title,
-          description,
-          status: 'TODO'
-        }
-      ]
-    })),
-  dragTask: (id: string | null) => set({ draggedTask: id }),
-  removeTask: (id: string) =>
-    set((state) => ({
-      tasks: state.tasks.filter((task) => task.id !== id)
-    })),
-  updateTask: (id: string, status: Status) =>
-    set((state) => ({
-      tasks: state.tasks.map((task) =>
-        task.id === id ? { ...task, status } : task
-      )
-    }))
-}));
+export const useTaskStore = create<State & Actions>()(
+  persist(
+    (set) => ({
+      tasks: [],
+      draggedTask: null,
+      addTask: (title: string, description?: string) =>
+        set((state) => ({
+          tasks: [
+            ...state.tasks,
+            {
+              id: uuid(),
+              title,
+              description,
+              status: 'TODO'
+            }
+          ]
+        })),
+      dragTask: (id: string | null) => set({ draggedTask: id }),
+      removeTask: (id: string) =>
+        set((state) => ({
+          tasks: state.tasks.filter((task) => task.id !== id)
+        })),
+      updateTask: (id: string, status: Status) =>
+        set((state) => ({
+          tasks: state.tasks.map((task) =>
+            task.id === id ? { ...task, status } : task
+          )
+        }))
+    }),
+    { name: 'task-store' }
+  )
+);
