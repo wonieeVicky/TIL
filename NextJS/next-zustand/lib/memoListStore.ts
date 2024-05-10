02b2e1bd9e825caf776a7d26b3b599c865af3cdd
wonieeVicky <hwfongfing@gmail.com>
2024-05-10 23:42:32 +0900

    feat: next-zustand - create store(memoList)

---
+++ NextJS/next-zustand/lib/memoListStore.ts
@@ -0,0 +1,16 @@
+﻿import { create } from 'zustand';
+
+export const useMemoStore = create((set) => ({
+  memoList: [],
+  addMemo: (val) =>
+    set((prev) => ({
+      memoList: [
+        ...prev.memoList,
+        { content: val, id: new Date().getMilliseconds() + val }
+      ]
+    })),
+  removeMemo: (id) =>
+    set((prev) => ({
+      memoList: prev.memoList.filter((e) => e.id !== id)
+    }))
+}));
